cdf6f7677010cb72a02db1a07ee83ff39cc71da7
kgisme170 <kgisme170@163.com>
2018-05-20 00:48:28 +0800

    app.py

---
+++ lang/py/usewsgi.py
@@ -0,0 +1,9 @@
+import wsgiref
+from wsgiref.simple_server import make_server
+def application(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    return '<h1>Hello, %s!</h1>' % (environ['PATH_INFO'][1:] or 'web')
+httpd = make_server('', 8000, application)
+print "Serving HTTP on port 8000..."
+# 开始监听HTTP请求:
+httpd.serve_forever()
