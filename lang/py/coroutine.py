435b8b1b2243d0ee6fb89fa3dacf8bb1796cf96d
kgisme170 <kgisme170@163.com>
2018-05-24 20:40:34 +0800

    ok

---
+++ lang/py/coroutine.py
@@ -0,0 +1,22 @@
+import time
+def consumer():
+    r=''
+    while True:
+        n=yield r
+        if not n:
+            return
+        print 'Consumer gets ',n
+        time.sleep(1)
+        r='ok'
+
+def producer(c):
+    c.next()
+    n=0
+    while n<5:
+        n=n+1
+        print 'Producer=',n
+        r=c.send(n)
+        print 'Consumer returns ',r
+    c.close()
+c=consumer()
+producer(c)
