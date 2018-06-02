d236eb46da70fb14ae256e82e4e212ab97e0acca
kgisme170 <kgisme170@163.com>
2018-06-02 14:17:40 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I623cc1b5ca8d8e83332a5237c7170fcf864ea99f

--- lang/scala/ch07.scala
+++ lang/scala/ch07.scala
@@ -2,3 +2,12 @@ package ch0607
 package object people{
     val defaultName="John Q.Public"
 }
+package people{
+    class Person{
+        var name=defaultName
+    }
+}
+package my.you.he{}
+import java.awt.{Color,Font}
+import java.util.{HashMap=>JavaHashMap}
+import scala.collectionmutable._
