be901f4aca5a1a31c3dea99a6c95a34fc21dcb79
kgisme170 <kgisme170@163.com>
2018-06-18 14:45:27 +0800

    ok

--- lang/scala/ch17.scala
+++ lang/scala/ch17.scala
@@ -50,6 +50,10 @@ class Pair3[T:Ordering](val first:T,val second:T){
     def smaller(implicit ord:Ordering[T]) =
         if(ord.compare(first, second) < 0) first else second
 }
+class Pair4[T](val first:T,val second:T){
+    def smaller(implicit ev:T<:<Ordered[T]) = 
+        if (first < second) first else second
+}
 val p=new Pair(a,b)
 println(p.smaller)
 val p1=new Pair1(a,b)
@@ -57,7 +61,7 @@ p1.replaceFirst("ok")
 val p2=new Pair2(2,3)
 println(p2.smaller)
 //5 times println("HI")
-def makePair[T : Manifest](first: T,second: T){
+def makePair[T : Manifest](first: T,second: T)={
     val r=new Array[T](2)
     r(0)=first
     r(1)=second
@@ -65,3 +69,19 @@ def makePair[T : Manifest](first: T,second: T){
 }
 val a1=makePair(1,2)
 println(a1(1))
+val p4=new Pair4(3,4)
+val bo=Array("Hello", "Fred").corresponds(Array(5,4))(_.length==_)
+println(bo)
+
+class Animal{}
+class Bird extends Animal{}
+class Consumer[-S, +T]{
+    def m1[U>:T](u:U)={}
+    def m2[U<:S](u:U)={}
+}
+object o extends App{
+    val c1:Consumer[Animal,Bird]=new Consumer[Animal,Bird]()
+    val c2:Consumer[Bird,Animal]=c1
+    c2.m1(new Animal);
+    c1.m2(new Bird);
+}
