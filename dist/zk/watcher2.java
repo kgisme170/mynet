a7efdb8ae7ff9096575b53ac9d53428514afab60
kgisme170 <kgisme170@163.com>
2018-06-23 13:34:26 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: Iec1bca0d59ce1d11a3ddcfd895ae656984b8b3e0

---
+++ dist/zk/watcher2.java
@@ -0,0 +1,65 @@
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+import java.lang.Exception.*;
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.*;
+import org.apache.zookeeper.KeeperException.*;
+import org.apache.zookeeper.AsyncCallback.*;
+public class watcher2 implements Watcher{
+    private ZooKeeper zk;
+    private String hostPort;
+    private static final int SESSION_TIME_OUT = 15000;
+    watcher2(String _hostPort){
+        hostPort=_hostPort;
+    }
+    void startZK() throws Exception{
+        zk=new ZooKeeper(hostPort, SESSION_TIME_OUT, this);
+    }
+    void stopZK() throws Exception {zk.close();}
+    void register(){
+        try{
+            zk.create("/workers/worker-" + serverId,
+                "Idle".getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                CreateMode.EPHEMERAL,
+                createWorkerCallback, null);
+        }catch(Exception e){}
+    }
+    StringCallback createWorkerCallback = new StringCallback(){
+        public void processResult(int rc, String path, Object ctx, String name){
+            switch(Code.get(rc)){
+                case CONNECTIONLOSS:
+                    register();
+                    break;
+                case OK:
+                    System.out.println("Register OK:" + serverId);
+                    break;
+                case NODEEXISTS:
+                    System.out.println("Already exists:" + serverId);
+                    break;
+                default:
+                    System.out.println("Not OK:" + serverId);
+            }
+        }
+    };
+    public void process(WatchedEvent event){
+        String msg = "##########====>";
+        msg += event;
+        System.out.println("Enter the process method,the event is :" + msg);
+    }
+    Random rand = new Random();
+    String serverId = Long.toString(rand.nextLong());
+    String path = "/master";
+    public static void main(String args[])throws Exception{
+        if (args.length == 0){
+            System.out.println("需要zk server的ip地址和端口作为参数");
+            System.exit(1);
+        }
+        watcher2 m = new watcher2(args[0]);
+        m.startZK();
+        m.register();
+        Thread.sleep(30000);
+        m.stopZK();
+    }
+}
