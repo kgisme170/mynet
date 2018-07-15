ee47805946031688bc6a82c57f996a15b32b80b0
kgisme170 <kgisme170@163.com>
2018-07-16 05:15:31 +0800

    ok

--- lang/ds/kmerge.cpp
+++ lang/ds/kmerge.cpp
@@ -7,12 +7,66 @@ using namespace std;
  * 构造败者树, 败者树一共n个节点，构造n个叶子节点后才算完成，n个非叶子节点会有n+1个叶子节点
  * 败者树构造和调整的算法
  */
-
+struct DevNull:ostream{}dev;
+//#define DEBUG
+#ifdef DEBUG
+#define COUT cout
+#else
+#define COUT dev
+#endif // define
 const size_t testLen = 5;
-const static int buf[testLen][3] = {{10,15,16}, {9,18,20}, {20,22,40}, {6,15,25}, {12,37,48}};
+const static int buf[testLen][3] = {
+    {10,15,16},
+    {9,18,21},
+    {20,22,40},
+    {6,17,25},
+    {12,37,48}
+};
 size_t bufIndex[testLen] = {0};
 struct testWorker{//败者树的数据输入输出
     size_t length(){return testLen;}
+    void input(int& k, size_t arrayIdx){
+        if(arrayIdx>=testLen){
+            cout<<"编程错误, arrayIdx="<<arrayIdx<<'\n';
+            exit(1);
+        }
+        size_t& idx = bufIndex[arrayIdx];
+        //cout<<"idx="<<idx<<",size="<<sizeof(buf[0])/sizeof(int)<<'\n';
+        //if(idx==sizeof(buf[0])/sizeof(int))cout<<"到达结尾\n";
+        k = (idx>=sizeof(buf[0])/sizeof(int)) ? INT_MAX : buf[arrayIdx][idx];
+        ++idx;
+    }
+    void output(int i){cout<<"i="<<i<<'\n';}
+    void init(){}//初始化归并段
+};
+const size_t dataBufSize = 6;
+const static int data[] = {
+    51, 49, 39, 46, 38,
+    29, 14, 61, 15, 30,
+    1,  48, 52, 3,  63,
+    27, 4,  13, 89, 24,
+    46, 58, 33, 76
+};
+class mergedData{
+    size_t bufSize;
+    size_t numData;
+    vector<vector<int>> initVector;
+public:
+    mergedData():bufSize(dataBufSize){
+        numData = sizeof(data)/sizeof(data[0]);
+        size_t numVector = numData / bufSize + numData % bufSize;//如果不整除就要加1
+        cout<<"vector的数量="<<numVector<<'\n';
+        initVector.resize(numVector);
+        for(size_t i=0;i<numData;++i){
+            size_t vIdx = i / bufSize;//第几个vector
+            initVector[vIdx].push_back(data[i]);
+        }
+        for(size_t i=0;i<numVector;++i){
+            cout<<"第"<<i<<"个vector的元素个数="<<initVector[i].size()<<',';
+        }
+        cout<<'\n';
+    }
+    size_t length(){return numData;}
     void input(int& k, size_t arrayIdx){
         if(arrayIdx>=testLen){
             cout<<"编程错误, arrayIdx="<<arrayIdx<<'\n';
@@ -33,18 +87,19 @@ class K_Merge: public IoWorker{
     size_t k;//归并段的数量
 
     void Adjust(size_t s){
-        cout<<"->Adjust(s)="<<s<<'\n';
+        COUT<<"->Adjust(s)="<<s<<'\n';
         size_t t = (s+k)/2; // t是双亲节点
         while(t>0){
-            cout<<"t="<<t<<',';
+            COUT<<"t="<<t<<',';
             if(b[s]>b[ls[t]]){//ls[t]指向败者, s用于比较，指向胜者
-                cout<<"交换b["<<s<<"]("<<b[s]<<")和ls["<<t<<"]("<<ls[t]<<"),";
+                COUT<<"交换b["<<s<<"]("<<b[s]<<")和ls["<<t<<"]("<<ls[t]<<"),";
                 size_t tmpS = s;
                 s = ls[t];
+                COUT<<"s的新值="<<s<<'\n';
                 ls[t] = tmpS;
             }
             t/=2;
-            cout<<'\n';
+            COUT<<'\n';
         }
         ls[0]=s;
         printLoserTree();
@@ -71,21 +126,44 @@ public:
         cout<<"CreateLoserTree() ends========================\n";
         while(b[ls[1]]!=INT_MAX){
             int q = ls[0];//ls[0];
-            IoWorker::output(q);
+            COUT<<"输出位置="<<q<<",值=";
+            IoWorker::output(b[q]);
             IoWorker::input(b[q], q);
+            COUT<<"补充的新值="<<b[q]<<'\n';
             Adjust(q);
         }
     }
     void printLoserTree(){
         for(size_t i=0;i<k;++i){
-            cout<<ls[i]<<',';
+            COUT<<ls[i]<<',';
         }
-        cout<<'\n';
+        COUT<<'\n';
+    }
+
+    void drawLoserTree(){
+        struct IntervalYield{
+            size_t n;
+            size_t count = 0;
+            IntervalYield(size_t _n):n(_n){
+                size_t height = 1;
+                while(n){
+                    ++height;
+                    n/=2;
+                }
+                cout<<height<<'\n';
+            }
+            void drawNext(const string& msg){
+
+            }
+        }y(ls.size());
+        // 计算树的层数
     }
 };
 int main(){
     K_Merge<int, testWorker> mTest;
     mTest.merge();
     mTest.printLoserTree();
+    mTest.drawLoserTree();
+    mergedData d;
     return 0;
 }
