90ba8523770867c502a092f5a8ece4840273cb72
kgisme170 <kgisme170@163.com>
2018-07-16 18:03:37 -0700

    ok

--- linux/module/hellomod.c
+++ linux/module/hellomod.c
@@ -1,15 +1,17 @@
 #include <linux/init.h>
 #include <linux/module.h>
 MODULE_LICENSE("GPL");
-
+int doublelist_init(void);
 static int __init hello_init(void)
 {
     printk(KERN_ALERT "Hello, world\n");
+    doublelist_init();
     return 0;
 }
-
+void doublelist_exit(void);
 static void __exit hello_exit(void)
 {
+    doublelist_exit();
     printk(KERN_ALERT "Goodbye, cruel world\n");
 }
 
---
+++ linux/module/mylist.c
@@ -0,0 +1,47 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/slab.h>
+#include <linux/list.h>
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("GLM");
+
+#define N 10
+struct numlist{
+    int num;
+    struct list_head list;
+};
+struct numlist numhead;//
+int doublelist_init(void){
+    struct numlist *listnode;
+    struct list_head * pos;
+    struct numlist *p;
+    int i;
+    printk("doublelist is starting...\n");
+    INIT_LIST_HEAD(&numhead.list);
+    for(i=0;i<N;++i){
+        listnode=(struct numlist*)kmalloc(sizeof(struct numlist),GFP_KERNEL);
+        listnode->num=i+1;
+        list_add_tail(&listnode->list, &numhead.list);
+        printk("Node %d has added to the doublelist...\n", i+1);
+    }
+    i=1;
+    list_for_each(pos, &numhead.list){
+        p = list_entry(pos, struct numlist, list);
+        printk("Node %d's data: %d\n", i, p->num);
+        ++i;
+    }
+    return 0;
+}
+void doublelist_exit(void){
+    struct list_head *pos, *n;
+    struct numlist *p;
+    int i;
+    i = 1;
+    list_for_each_safe(pos, n, &numhead.list){
+        list_del(pos);
+        p = list_entry(pos, struct numlist, list);
+        kfree(p);
+        printk("Node %d has been removed from the doublelist...\n", i++);
+    }
+    printk("doublelist is exiting...\n");
+}
