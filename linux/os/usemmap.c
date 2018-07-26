f3ad6bb2bd966c8432ac53fd2a1cd895cb61e184
kgisme170@163.com <kgisme170@163.com>
2018-07-25 21:01:47 -0700

    ok

---
+++ linux/os/usemmap.c
@@ -0,0 +1,17 @@
+#include<sys/mman.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include<wait.h>
+#include<stdio.h>
+void main(){
+    int i,sum,fd;
+    int*result_ptr=mmap(0,4,PROT_READ|PROT_WRITE,MAP_SHARED|MAP_ANONYMOUS,0,0);
+    int pid=fork();
+    if(pid==0){//child
+        for(sum=0,i=1;i<=10;++i){sum+=i;}
+        *result_ptr=sum;
+    }else{
+        wait(0);
+        printf("result=%d\n",*result_ptr);
+    }
+}
