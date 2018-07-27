fc339bd2257f5769584cd8caeeb87a09c7c118e9
kgisme170@163.com <kgisme170@163.com>
2018-07-27 13:15:01 -0700

    ok

--- linux/module/hellomod.c
+++ linux/module/hellomod.c
@@ -1,11 +1,13 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/init_task.h>
 MODULE_LICENSE("GPL");
 int doublelist_init(void);
 static int __init hello_init(void)
 {
     printk(KERN_ALERT "Hello, world\n");
     doublelist_init();
+    printk(KERN_ALERT "The process is %s, pid=%d\n",current->comm,current->pid);
     return 0;
 }
 void doublelist_exit(void);
