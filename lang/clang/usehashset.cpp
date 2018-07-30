33352ced81e596652cd495b0a07582a579c4f38c
kgisme170 <kgisme170@163.com>
2018-07-30 12:50:40 +0800

    ok

--- lang/clang/usehashset.cpp
+++ lang/clang/usehashset.cpp
@@ -1,24 +1,33 @@
 #include<unordered_set>
+#include<iostream>
+#include<functional>
 using namespace std;
 struct node{
-     size_t value;
-     node* next;
-     node():value(-1),next(NULL){}
+    size_t value;
 };
 struct myhash{
-     size_t operator()(const node& n)const{
-         return (size_t)n.value;
-     }
+    size_t operator()(const node& n)const{
+        return (size_t)n.value;
+    }
 };
 size_t h(const node& n){
-     return (size_t)n.value;
+    return (size_t)n.value;
 }
 int main(){
-     unordered_set<node, myhash> s1;
-     auto f=[](const node& n){
-         return (size_t)n.value;
-     };
-     unordered_set<node, decltype(f)> s2(f);
-     //unordered_set<node, [](const node& e){return n.value;}> s;
-     return 0;
+    unordered_set<node, myhash> s;
+    unordered_set<node, std::function<size_t(const node&)>> s2(3,h);
+    unordered_set<node, std::function<size_t(const node&)>> s3(4,[](const node& e){return e.value;});
+    cout<<s3.size()<<','<<s3.bucket_count()<<'\n';
+    unordered_set<int> s1;
+    s1.insert(1);
+    s1.insert(9);
+    s1.insert(0);
+    s1.emplace(2);
+    s1.emplace(3);
+    s1.emplace(4);
+    cout<<s1.bucket_count()<<'\n';
+    cout<<s1.max_bucket_count()<<'\n';
+    cout<<s1.load_factor()<<'\n';
+    cout<<s1.max_load_factor()<<'\n';
+    return 0;
 }
