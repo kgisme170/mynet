5cfb8e6a66e12ef8238122c8fedf4122788462bb
kgisme170 <kgisme170@163.com>
2018-07-30 16:52:01 +0800

    ok

---
+++ lang/clang/usepromise.cpp
@@ -0,0 +1,17 @@
+#include<chrono>
+#include<future>
+#include<iostream>
+#include<thread>
+#include<type_traits>
+using namespace std;
+int main(){
+    promise<int> p;
+    thread t([](promise<int>& p){
+        this_thread::sleep_for(chrono::microseconds(1000));
+        p.set_value_at_thread_exit(5);
+    },ref(p));
+    future<int> f = p.get_future();
+    cout<<f.get()<<'\n';
+    t.join();
+    return 0;
+}
