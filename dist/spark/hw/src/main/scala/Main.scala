fdac1e84151745e71b35b4c6f89d683f302947ff
kgisme170 <kgisme170@163.com>
2018-08-09 16:33:22 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I498f2b14181ece247e1c2767619aaabcb7a08464

--- dist/spark/hw/src/main/scala/Main.scala
+++ dist/spark/hw/src/main/scala/Main.scala
@@ -1,3 +1,7 @@
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.regression.LinearRegressionWithSGD
+import org.apache.spark.mllib.stat.Statistics
 object Main extends App {
   println("Hello, cn!")
 }
