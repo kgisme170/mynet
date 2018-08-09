d26d8d6273a9bd9dd069d7ebef78bddf93aa14bc
kgisme170 <kgisme170@163.com>
2018-08-09 17:55:37 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: Iba619ea8d798e430881d3b4573d3a32e75949a1a

---
+++ dist/spark/myspark1/src/main/scala/my.scala
@@ -0,0 +1,26 @@
+/*
+object my {
+  def main(args: Array[String]):Unit = {
+    println("Hello World!")
+  }
+}
+*/
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by wuke on 2016/7/5.
+  */
+object LoadLibSVMFile extends App {
+
+  import org.apache.spark.mllib.regression.LabeledPoint
+  import org.apache.spark.mllib.util.MLUtils
+  import org.apache.spark.rdd.RDD
+
+  val conf = new SparkConf().setAppName("LogisticRegressionMail").setMaster("local")
+
+  val sc = new SparkContext(conf)
+  val examples: RDD[LabeledPoint] = MLUtils.loadLibSVMFile(sc, "sample_libsvm_data.txt")
+
+  println(examples.first)
+}
