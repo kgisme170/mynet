6325256255f495c3c83dfba33239efbcb5b92ffa
kgisme170 <kgisme170@163.com>
2018-08-12 07:09:12 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: Id3ac8c77b5b396e8b4f6aa32a9c212b2da5294f0

--- dist/spark/myjava1/src/main/java/myjava1.java
+++ dist/spark/myjava1/src/main/java/myjava1.java
@@ -1,4 +1,64 @@
-package PACKAGE_NAME;
+import java.io.File;
+import java.util.Arrays;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
+import org.apache.spark.SparkContext.*;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import scala.Tuple2;
 
 public class myjava1 {
+    static void deleteDir(File dir) {
+        for (File f : dir.listFiles()) {
+            if (f.isDirectory()) {
+                deleteDir(f);
+            } else {
+                f.delete();
+            }
+        }
+        dir.delete();
+    }
+
+    static void checkExistenceAndDelete(String dir) {
+        File file = new File(dir);
+        if (file.exists()) {
+            deleteDir(file);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hw");
+        SparkConf conf = new SparkConf().setAppName("wordCount");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaRDD<String> input = sc.textFile("pom.xml");
+        JavaRDD<String> words = input.flatMap(
+                new FlatMapFunction<String, String>() {
+                    @Override
+                    public Iterable<String> call(String s) throws Exception {
+                        return Arrays.asList(s.split((" ")));
+                    }
+                }
+        );
+        JavaPairRDD<String, Integer> counts = words.mapToPair(
+                new PairFunction<String, String, Integer>() {
+                    @Override
+                    public Tuple2<String, Integer> call(String s) throws Exception {
+                        return new Tuple2(s, 1);
+                    }
+                }
+        ).reduceByKey(new Function2<Integer, Integer, Integer>() {
+            @Override
+            public Integer call(Integer integer, Integer integer2) throws Exception {
+                return integer + integer2;
+            }
+        });
+        String dir = "myResult";
+        checkExistenceAndDelete(dir);
+        counts.saveAsTextFile(dir);
+    }
 }
