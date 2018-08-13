d7435603ed583b8452c41845e0fd1e480255d8ed
kgisme170 <kgisme170@163.com>
2018-08-13 11:27:01 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I3c39d33ffed5d7a7e35b38abed93bce31e09090a

--- dist/spark/myspark1/src/main/scala/my.scala
+++ dist/spark/myspark1/src/main/scala/my.scala
@@ -27,12 +27,29 @@ object ch02 extends App {
     }
   }
 
+  def test01() {
+    val input = sc.textFile("pom.xml");
+    input.persist()
+    val words = input.flatMap(line => line.split(" "));
+    val counts = words.map(word => (word, 1)).reduceByKey { case (x, y) => x + y }
+    val dir = "myResultDir";
+    checkExistenceAndDelete(dir);
+    counts.saveAsTextFile(dir);
+  }
+
+  val divide1:PartialFunction[Int, Int]= {
+    case d: Int if d != 0 => 100 / d
+  }
   val conf = new SparkConf().setMaster("local").setAppName("My App")
   val sc = new SparkContext(conf)
-  val input = sc.textFile("pom.xml");
-  val words = input.flatMap(line => line.split(" "));
-  val counts = words.map(word => (word, 1)).reduceByKey { case (x, y) => x + y }
-  val dir = "myResultDir";
-  checkExistenceAndDelete(dir);
-  counts.saveAsTextFile(dir);
+
+  val lines = sc.parallelize((List("pandas", "i like pandas", "I have", "my pandas")))
+  println(lines.count())
+  val p = lines.filter(l=>l.contains("panda"))
+  val q = lines.filter(l=>l.contains("like"))
+  val u = q.union(q)
+  p.take(1).foreach(println)
+  p.collect()
+  println(p.count())
+  println(divide1.isDefinedAt(0))
 }
