f8e6ed863dae4b0ef8eeafdeb094a37f4cfefa79
kgisme170@163.com <kgisme170@163.com>
2018-08-16 07:05:04 +0800

    ok

--- dist/spark/mypython1/ch01.py
+++ dist/spark/mypython1/ch01.py
@@ -3,3 +3,9 @@ import findspark
 findspark.init()
 from pyspark import SparkContext
 from pyspark import SparkConf
+conf = SparkConf().setMaster("local").setAppName("My app")
+sc = SparkContext(conf = conf)
+lines = sc.textFile("ch01.py")
+inputRDD = lines.filter(lambda x:"sc" in x)
+for line in inputRDD.take(10):
+    print line
