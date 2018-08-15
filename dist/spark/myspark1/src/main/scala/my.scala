c2ad03f0dfe3b82fb3f816428259dab18be13059
kgisme170 <kgisme170@163.com>
2018-08-15 17:51:28 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: Idd9272fcd53986f8335e6b0a2b1ae17e7b71b37a

--- dist/spark/myspark1/src/main/scala/my.scala
+++ dist/spark/myspark1/src/main/scala/my.scala
@@ -9,32 +9,34 @@ import org.apache.spark.SparkContext._
  */
 object ch02 extends App {
   def deleteDir(dir: File): Unit = {
-    val files = dir.listFiles();
+    val files = dir.listFiles()
     files.foreach(f => {
       if (f.isDirectory()) {
-        deleteDir(f);
+        deleteDir(f)
       } else {
-        f.delete();
+        f.delete()
       }
-    });
-    dir.delete();
+    })
+    dir.delete()
   }
 
   def checkExistenceAndDelete(dir: String): Unit = {
     val file = new File(dir);
     if (file.exists()) {
-      deleteDir(file);
+      deleteDir(file)
     }
   }
 
   def test01() {
-    val input = sc.textFile("pom.xml");
+    val input = sc.textFile("/Users/x/Documents/spark-2.3.1-bin-hadoop2.7/README.md");
     input.persist()
     val words = input.flatMap(line => line.split(" "));
     val counts = words.map(word => (word, 1)).reduceByKey { case (x, y) => x + y }
-    val dir = "myResultDir";
-    checkExistenceAndDelete(dir);
-    counts.saveAsTextFile(dir);
+    val dir = "myResultDir"
+    checkExistenceAndDelete(dir)
+    counts.saveAsTextFile(dir)
+    val a = counts.collect()
+    a.foreach(println)
   }
 
   def test02(): Unit = {
@@ -44,7 +46,6 @@ object ch02 extends App {
     val q = lines.filter(l => l.contains("like"))
     val u = q.union(q)
     p.take(1).foreach(println)
-    p.collect()
     println(p.count())
     println(divide1.isDefinedAt(0))
   }
@@ -54,5 +55,5 @@ object ch02 extends App {
   }
   val conf = new SparkConf().setMaster("local").setAppName("My App")
   val sc = new SparkContext(conf)
-
+  test01()
 }
