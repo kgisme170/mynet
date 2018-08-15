00a668fdab7307d77fe0685e09d22aae2fb49fee
kgisme170@163.com <kgisme170@163.com>
2018-08-16 05:49:53 +0800

    ok

--- dist/spark/ch02.java
+++
@@ -1,7 +0,0 @@
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaSparkConf;
-public class ch02{
-    public static void main(String [] args){
-
-    }
-}
--- dist/spark/ch02.scala
+++
@@ -1,8 +0,0 @@
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
-
-object ch02 extends App{
-  val conf = new SparkConf().setMaster("local").setAppName("My App")
-  val sc = new SparkContext(conf)
-}
