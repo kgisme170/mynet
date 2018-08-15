8c0f0826b7697b0a224f90538d0ce29a6d3a88b6
kgisme170 <kgisme170@163.com>
2018-08-15 19:51:10 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: Ibbf05126c6bbe00d57b698e05bcb9962057973e9

---
+++ lang/maven/mybank/src/main/java/com/my/App.java
@@ -0,0 +1,13 @@
+package com.my;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Util.printMessage( "Hello World!" + args[0] );
+    }
+}
---
+++ lang/maven/mybank/src/main/java/com/my/Util.java
@@ -0,0 +1,8 @@
+package com.my;
+
+public class Util
+{
+   public static void printMessage(String message){
+       System.out.println(message);
+   }
+}
