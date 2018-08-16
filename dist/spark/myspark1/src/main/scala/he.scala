8724115b757e1e2263582e556280ec2f6da09f2e
kgisme170 <kgisme170@163.com>
2018-08-16 15:41:18 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I62dd990b7928e212d5005272abba45cb1a4e8504

--- dist/spark/myspark1/src/main/scala/he.scala
+++ dist/spark/myspark1/src/main/scala/he.scala
@@ -1,5 +1,3 @@
-import java.io.File
-
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 object he extends App{
@@ -9,4 +7,9 @@ object he extends App{
   val result = input.map(x=>x*x)
   println(result.collect().mkString(","))
   println("test")
+
+  val words = Array("one", "two", "three", "four", "two", "two", "one")
+  val wordsRDD = sc.parallelize(words).map(word=>(word, 1))
+  val wordsReduce = wordsRDD.reduceByKey(_+_).collect().foreach(println)
+  val wordsGroup = wordsRDD.groupByKey().map(w=>(w._1,w._2.sum)).collect().foreach(println)
 }
