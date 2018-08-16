59580d9d420afdf9f8b75b1ffad8bc0b1a8b7f0f
kgisme170@163.com <kgisme170@163.com>
2018-08-17 05:54:03 +0800

    ok

--- dist/spark/myjava1/src/main/java/aggregate.java
+++ dist/spark/myjava1/src/main/java/aggregate.java
@@ -2,9 +2,15 @@ import java.io.Serializable;
 import java.util.*;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.api.java.function.VoidFunction;
+import scala.Tuple2;
+
 class AvgCount implements Serializable{
     public AvgCount(int total, int num){
         this.total = total;
@@ -33,9 +39,31 @@ public class aggregate {
         SparkConf conf = new SparkConf().setAppName("wordCount").setMaster("local");
         JavaSparkContext sc = new JavaSparkContext(conf);
         AvgCount initial = new AvgCount(0,0);
-        List<Integer> a = Arrays.asList(11,23,4,5);
+        List<Integer> a = Arrays.asList(11,23,4,5,7);
         JavaRDD<Integer> rdd = sc.parallelize(a);
+        rdd.collect();
+        Integer sum = rdd.reduce(new Function2<Integer, Integer, Integer>() {
+            public Integer call(Integer integer, Integer integer2) throws Exception {
+                return integer + integer2;
+            }
+        });
+        System.out.println(sum);
         AvgCount result = rdd.aggregate(initial, addAndCount, combine);
         System.out.println(result.avg());
+        JavaPairRDD<Integer, Integer> pairs = rdd.mapToPair(new PairFunction<Integer, Integer, Integer>() {
+            public Tuple2<Integer, Integer> call(Integer x) throws Exception {
+                return new Tuple2(x, x*x);
+            }
+        });
+        JavaPairRDD<Integer, Integer> big = pairs.filter(new Function<Tuple2<Integer, Integer>, Boolean>() {
+            public Boolean call(Tuple2<Integer, Integer> integerIntegerTuple2) throws Exception {
+                return integerIntegerTuple2._2() > 20;
+            }
+        });
+        big.foreach(new VoidFunction<Tuple2<Integer, Integer>>() {
+            public void call(Tuple2<Integer, Integer> integerIntegerTuple2) throws Exception {
+                System.out.println(integerIntegerTuple2._2);
+            }
+        });
     }
 }
