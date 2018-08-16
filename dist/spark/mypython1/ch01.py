e2ddea3a5e742c0091516df8731663495676d1dd
kgisme170@163.com <kgisme170@163.com>
2018-08-16 22:53:35 +0800

    ok

--- dist/spark/mypython1/ch01.py
+++ dist/spark/mypython1/ch01.py
@@ -9,3 +9,15 @@ lines = sc.textFile("ch01.py")
 inputRDD = lines.filter(lambda x:"sc" in x)
 for line in inputRDD.take(10):
     print line
+
+lines = sc.parallelize(["hello world", "hi"])
+words = lines.flatMap(lambda line:line.split(" "))
+print words.first()
+
+data = sc.parallelize([1,2,3,4,1,3])
+print data.reduce(lambda x,y: x+y)
+
+def printall(rdd):
+    for r in rdd:
+        print r
+printall(data.distinct().take(10))
