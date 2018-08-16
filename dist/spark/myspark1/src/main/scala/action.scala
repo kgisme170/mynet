2cd470002ade3331c7bcab2737e6d7afd45a1e4c
kgisme170@163.com <kgisme170@163.com>
2018-08-17 00:20:18 +0800

    ok

--- dist/spark/myspark1/src/main/scala/action.scala
+++ dist/spark/myspark1/src/main/scala/action.scala
@@ -1,8 +1,19 @@
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.storage.StorageLevel
 object action extends App {
   val conf = new SparkConf().setMaster("local").setAppName("My App")
   val sc = new SparkContext(conf)
   val data = sc.parallelize(List(1,2,1,3,1,4))
   val sum = data.reduce(_+_)
   println(sum)
+  data.persist(StorageLevel.DISK_ONLY)
+
+  val data2 = List((1,3),(1,2),(1,4),(2,3),(3,6),(3,8))
+  val result2 = sc.parallelize(data2)
+  result2.collect()
+  result2.keys.foreach(println)
+  result2.values.foreach(println)
+  result2.flatMapValues(x=>(x to 5)).foreach(println)
+  result2.sortByKey().foreach(println)
+  result2.filter{case (k,v)=>v<=3}.foreach(println)
 }
