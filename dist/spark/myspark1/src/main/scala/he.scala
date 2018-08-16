5265e8cd9492fc3104e520aaeef6667113911827
kgisme170 <kgisme170@163.com>
2018-08-16 19:23:46 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I53752c2227156c819a400952b4d8733304e57d24

--- dist/spark/myspark1/src/main/scala/he.scala
+++ dist/spark/myspark1/src/main/scala/he.scala
@@ -1,19 +1,38 @@
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
-object he extends App{
+import org.apache.spark.{HashPartitioner, SparkConf, SparkContext}
+object he extends App {
   val conf = new SparkConf().setMaster("local").setAppName("My App")
   val sc = new SparkContext(conf)
-  val input = sc.parallelize(List(1,2,3,4))
-  val result = input.map(x=>x*x)
+  println("test00==================================")
+  val input = sc.parallelize(List(1, 2, 3, 4))
+  val result = input.map(x => x * x)
   println(result.collect().mkString(","))
-  println("test")
+  println("test01==================================")
 
   val words = Array("one", "two", "three", "four", "two", "two", "one")
-  val wordsRDD = sc.parallelize(words).map(word=>(word, 1))
-  val wordsReduce = wordsRDD.reduceByKey(_+_).collect().foreach(println)
-  val wordsReduceLocal = wordsRDD.reduceByKeyLocally(_+_).foreach(println)
-  val wordsGroup = wordsRDD.groupByKey().map(w=>(w._1,w._2.sum)).foreach(println)
+  val wordsRDD = sc.parallelize(words).map(word => (word, 1))
+  val wordsReduce = wordsRDD.reduceByKey(_ + _).collect().foreach(println)
+  println("test02==================================")
+  val wordsReduceLocal = wordsRDD.reduceByKeyLocally(_ + _).foreach(println)
+  println("test03==================================")
+  val wordsGroup = wordsRDD.groupByKey().map(w => (w._1, w._2.sum)).foreach(println)
+  println("test04==================================")
   wordsRDD.groupByKey().foreach(println)
-  var rdd1 = sc.makeRDD(Array(("A",0),("A",2),("B",1),("B",2),("C",1)))
-  println(rdd1.partitions.size)
+  println("test05==================================")
+  var rdd1 = sc.parallelize(Array(("A", 9.0), ("A", 2.0), ("B", 1.0), ("B", 2.0), ("C", 1.0))).partitionBy(new HashPartitioner(3))
+  rdd1.foreachPartition(p=>println(p.length))
+  println("test06==================================")
+  rdd1.foreachPartition(p=>p.foreach(i=>println(i._2)))
+  println("test07==================================")
+
+  type cType = (Int, Double)
+  val cb = rdd1.combineByKey(
+    createCombiner =  c => (1, c),
+    mergeValue = (c: cType, v) => (c._1 + 1, c._2 + v),
+    mergeCombiners = (c: cType, c2: cType) => (c._1 + c2._1, c._2 + c2._2),
+    numPartitions = rdd1.partitions.size
+  )
+  cb.foreach(println)
+  println("test08==================================")
+  cb.map{case (name, (num, score)) => (name, score / num)}.foreach(println)
+  println("test09==================================")
 }
--- dist/spark/myspark1/src/main/scala/my.scala
+++ dist/spark/myspark1/src/main/scala/my.scala
@@ -5,7 +5,7 @@ import org.apache.spark.SparkContext
 /*
  * 读取一个文件并分解到目标目录下
  */
-object ch02 extends App {
+object my extends App {
   def deleteDir(dir: File): Unit = {
     val files = dir.listFiles()
     files.foreach(f => {
