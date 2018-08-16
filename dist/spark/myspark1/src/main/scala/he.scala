c3640bf0949e806b0ee491b97195fa7330fb44d2
kgisme170 <kgisme170@163.com>
2018-08-16 18:47:31 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I9188f83fbdb208e8a2916fca10c8fca2bd9649a0

--- dist/spark/myspark1/src/main/scala/he.scala
+++ dist/spark/myspark1/src/main/scala/he.scala
@@ -11,5 +11,9 @@ object he extends App{
   val words = Array("one", "two", "three", "four", "two", "two", "one")
   val wordsRDD = sc.parallelize(words).map(word=>(word, 1))
   val wordsReduce = wordsRDD.reduceByKey(_+_).collect().foreach(println)
-  val wordsGroup = wordsRDD.groupByKey().map(w=>(w._1,w._2.sum)).collect().foreach(println)
+  val wordsReduceLocal = wordsRDD.reduceByKeyLocally(_+_).foreach(println)
+  val wordsGroup = wordsRDD.groupByKey().map(w=>(w._1,w._2.sum)).foreach(println)
+  wordsRDD.groupByKey().foreach(println)
+  var rdd1 = sc.makeRDD(Array(("A",0),("A",2),("B",1),("B",2),("C",1)))
+  println(rdd1.partitions.size)
 }
