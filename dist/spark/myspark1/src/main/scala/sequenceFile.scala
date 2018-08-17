5f6456f37a779ee811862c123dabab571a66aba7
kgisme170 <kgisme170@163.com>
2018-08-17 23:25:28 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I092c5991437407224d9129d01e490264f2ba9c75

--- dist/spark/myspark1/src/main/scala/sequenceFile.scala
+++ dist/spark/myspark1/src/main/scala/sequenceFile.scala
@@ -1,4 +1,5 @@
 import org.apache.hadoop.io.{IntWritable, Text}
+import org.apache.hadoop.mapred.SequenceFileOutputFormat
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 
@@ -12,11 +13,11 @@ object sequenceFile {
     rdd.saveAsSequenceFile("output")
 
     //读sequenceFile
-    val output = sc.sequenceFile("output", classOf[Text], classOf[IntWritable]).
-      map { case (x, y) => (x.toString, y.get()) }
-    output.foreach(println)
+    val output = sc.sequenceFile("output", classOf[Text], classOf[IntWritable])
+    output.map{case(x,y)=>(x.toString, y.get)}.foreach(println)
+    output.saveAsHadoopFile("output1", classOf[Text], classOf[IntWritable], classOf[SequenceFileOutputFormat[Text, IntWritable]])
 
-    //写objectFile，
+    //写objectFile
     val ro = sc.parallelize(List("scala", "java", "python"))
     ro.saveAsObjectFile("output2")
 
