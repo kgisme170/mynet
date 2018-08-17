f4cf844fcc6f225adf8ffe75a443de2ea830eb00
kgisme170 <kgisme170@163.com>
2018-08-17 10:06:54 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I761b0ed023ded89e71262bbe48c3c6a0fa3613b7

--- dist/spark/myspark1/src/main/scala/jsoncsv.scala
+++ dist/spark/myspark1/src/main/scala/jsoncsv.scala
@@ -1,5 +1,25 @@
-package jsoncsv
+import org.apache.spark.{SparkConf, SparkContext}
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
+import org.json4s.jackson.Serialization
+import org.json4s.jackson.Serialization.{read,write}
+case class Address(line1: String, city: String, state: String, zip: String) {
+  override def toString = s"Address(line1=$line1, city=$city, state=$state, zip=$zip)"
+}
+case class Person(firstName:String, lastName:String, address:List[Address]){
+  override def toString = s"Person(firstName=$firstName, lastName=$lastName, address=$address)"
+}
+object jsoncsv extends App {
+  val conf = new SparkConf().setMaster("local").setAppName("My App")
+  val sc = new SparkContext(conf)
 
-class scala {
-
+  val inputJson = "test.json"
+  val inputFile = sc.textFile(inputJson)
+  val inputRDD = inputFile.map { record =>
+    implicit val formats = DefaultFormats
+    parse(record).extract[Person]
+  }
+  inputRDD.collect()
+  inputRDD.foreach(println)
+  inputRDD.saveAsTextFile("myResult")
 }
