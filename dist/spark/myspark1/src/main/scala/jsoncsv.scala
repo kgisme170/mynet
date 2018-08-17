32a6d0995c475a11354354cbe3794be298198666
kgisme170 <kgisme170@163.com>
2018-08-17 10:30:40 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I1ff25d9b93dd0325fbbac9f8652fd8b8e1a6bcdf

--- dist/spark/myspark1/src/main/scala/jsoncsv.scala
+++ dist/spark/myspark1/src/main/scala/jsoncsv.scala
@@ -1,25 +1,43 @@
 import org.apache.spark.{SparkConf, SparkContext}
-import org.json4s._
-import org.json4s.jackson.JsonMethods._
-import org.json4s.jackson.Serialization
-import org.json4s.jackson.Serialization.{read,write}
-case class Address(line1: String, city: String, state: String, zip: String) {
-  override def toString = s"Address(line1=$line1, city=$city, state=$state, zip=$zip)"
-}
-case class Person(firstName:String, lastName:String, address:List[Address]){
-  override def toString = s"Person(firstName=$firstName, lastName=$lastName, address=$address)"
-}
+import shapeless.Data
 object jsoncsv extends App {
+  def testjson: Unit = {
+    case class Address(line1: String, city: String, state: String, zip: String) {
+      override def toString = s"Address(line1=$line1, city=$city, state=$state, zip=$zip)"
+    }
+    case class Person(firstName: String, lastName: String, address: List[Address]) {
+      override def toString = s"Person(firstName=$firstName, lastName=$lastName, address=$address)"
+    }
+    import org.json4s._
+    import org.json4s.jackson.JsonMethods._
+    val inputJson = "test.json"
+    val inputFile = sc.textFile(inputJson)
+    val inputRDD = inputFile.map { record =>
+      implicit val formats = DefaultFormats
+      parse(record).extract[Person]
+    }
+    inputRDD.collect()
+    inputRDD.foreach(println)
+    inputRDD.saveAsTextFile("myResult")
+  }
+  def testcsv: Unit = {
+    import java.io.StringReader
+    import au.com.bytecode.opencsv.CSVReader
+    case class myData(index: String, title: String, content: String){
+      override def toString = s"Data(index=$index, title=$title, content=$content)"
+    }
+    val input = sc.textFile("sample.csv")
+    val result = input.map { line =>
+      val reader = new CSVReader(new StringReader(line))
+      reader.readNext()
+    }
+    for(res <- result){
+      for(re <- res)
+        println(re)
+    }
+
+  }
   val conf = new SparkConf().setMaster("local").setAppName("My App")
   val sc = new SparkContext(conf)
-
-  val inputJson = "test.json"
-  val inputFile = sc.textFile(inputJson)
-  val inputRDD = inputFile.map { record =>
-    implicit val formats = DefaultFormats
-    parse(record).extract[Person]
-  }
-  inputRDD.collect()
-  inputRDD.foreach(println)
-  inputRDD.saveAsTextFile("myResult")
+  testcsv
 }
