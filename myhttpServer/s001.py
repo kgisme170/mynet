418a058533ea5c23b636656a4c192fb7029681ab
kgisme170@163.com <kgisme170@163.com>
2018-08-19 10:18:04 +0800

    ok

--- myhttpServer/s001.py
+++
@@ -1,8 +0,0 @@
-import SimpleHTTPServer
-import SocketServer
-PORT=8081
-Handler=SimpleHTTPServer.SimpleHTTPRequestHandler
-print type(Handler)
-
-httpd=SocketServer.TCPServer(("",PORT),Handler)
-httpd.serve_forever()
