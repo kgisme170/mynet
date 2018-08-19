2a4e4f54e1b549a00a89ca5a232e0d440cee16ef
kgisme170@163.com <kgisme170@163.com>
2018-08-19 12:05:53 +0800

    ok

--- mytest/myspark1/src/main/scala/hiveCtx.scala
+++ mytest/myspark1/src/main/scala/hiveCtx.scala
@@ -1,13 +1,18 @@
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.hive.HiveContext
 object hiveCtx extends App {
+  def fromJson(): Unit ={
+    val lp = ctx.jsonFile("test2.json")
+    lp.registerTempTable("user");
+    val schemaRDD = ctx.sql("select name, lovePandas from user");
+    schemaRDD.columns.foreach(println)
+    schemaRDD.show()
+    schemaRDD.printSchema()
+  }
   val conf = new SparkConf().setMaster("local").setAppName("My App")
   val context = new SparkContext(conf)
   val ctx = new HiveContext(context)
-  val lp = ctx.jsonFile("test2.json")
-  lp.registerTempTable("user");
-  val schemaRDD = ctx.sql("select name, lovePandas from user");
-  schemaRDD.columns.foreach(println)
-  schemaRDD.show()
-  schemaRDD.printSchema()
+  val rows = ctx.sql("select * from z")
+  rows.show()
+  rows.printSchema()
 }
