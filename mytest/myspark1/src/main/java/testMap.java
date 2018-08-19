adb050d1e4cd2207a1d5f617a22a0555bc868880
kgisme170@163.com <kgisme170@163.com>
2018-08-19 10:06:47 +0800

    ok

--- mytest/myspark1/src/main/java/testMap.java
+++ mytest/myspark1/src/main/java/testMap.java
@@ -4,7 +4,6 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.Function2;
 import scala.Tuple2;
 
 public class testMap {
@@ -15,12 +14,7 @@ public class testMap {
         JavaRDD<String> input = sc.textFile("pom.xml");
         JavaRDD<String> words = input.flatMap((String s)-> Arrays.asList(s.split((" "))).iterator());
         JavaPairRDD<String, Integer> counts = words.mapToPair((String s) -> new Tuple2(s, 1)).
-                reduceByKey(new Function2<Integer, Integer, Integer>() {
-            @Override
-            public Integer call(Integer integer, Integer integer2) throws Exception {
-                return integer + integer2;
-            }
-        });
+                reduceByKey((Object integer, Object integer2)-> (Integer)integer + (Integer)integer2);
         String dir = "myResult";
         checkDeleteDir.checkExistenceAndDelete(dir);
         counts.saveAsTextFile(dir);
