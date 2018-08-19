03419baf56037748bd468165269e2727f983b30f
kgisme170@163.com <kgisme170@163.com>
2018-08-19 14:51:59 +0800

    ok

--- mytest/myspark1/src/main/scala/jdbcMysql.scala
+++ mytest/myspark1/src/main/scala/jdbcMysql.scala
@@ -5,55 +5,56 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.{SparkConf, SparkContext}
 
 object jdbcMysql extends App {
-  /*val spark = SparkSession.builder().appName("sqlSession").getOrCreate()
-  val ctx = spark.sqlContext
-  val sql = "select * from db1.t1"
-  val df = ctx.read.format("jdbc").options(Map(
-    "driver"->"com.mysql.jdbc.Driver",
-    "url"->"jdbc:mysql://localhost:14340",
-    "dbtable"->"db1.t1",
-    "user"-> "root",
-    "password"->"mypassword",
-    "lowerBound"->"0",
-    "upperBound"-> "1000",
-    "fetchSize"->"100"
-  )).load()*/
-
-  val conf = new SparkConf().setAppName("mysql").setMaster("local[4]")
-  val sc = new SparkContext(conf)
-
-  def createConnection() = {
-    Class.forName("com.mysql.jdbc.Driver").newInstance()
-    DriverManager.getConnection("jdbc:mysql://localhost:14340/db1")
+  def useSessionBuilder() {
+    val spark = SparkSession.builder().appName("sqlSession").getOrCreate()
+    val ctx = spark.sqlContext
+    val sql = "select * from t1"
+    val df = ctx.read.format("jdbc").options(Map(
+      "driver" -> "com.mysql.jdbc.Driver",
+      "url" -> "jdbc:mysql://localhost:3306",
+      "dbtable" -> "db1.t1",
+      "user" -> "root",
+      "password" -> "mypassword",
+      "lowerBound" -> "0",
+      "upperBound" -> "1000",
+      "fetchSize" -> "100"
+    )).load()
+    df.show()
   }
+  def useJdbcRDD() {
+    val conf = new SparkConf().setAppName("mysql").setMaster("local[4]")
+    val sc = new SparkContext(conf)
 
-  //createConnection()
-  def extractValues(r: ResultSet) = {
-    (r.getInt(1), r.getString(2))
+    def createConnection() = {
+      Class.forName("com.mysql.jdbc.Driver").newInstance()
+      DriverManager.getConnection("jdbc:mysql://localhost:3306/db1", "root", "mypassword")
+    }
+
+    val data = new JdbcRDD(sc, createConnection,
+      sql = "select * from t1 WHERE ID >= ? AND ID <= ?",
+      lowerBound = 1,
+      upperBound = 100,
+      numPartitions = 2,
+      mapRow = r => (r.getInt(1), r.getString(2)))
+
+    data.foreach(println)
   }
-
-  val data = new JdbcRDD(sc, createConnection, "select * from t1",
-    lowerBound = 1,
-    upperBound = 3,
-    numPartitions = 2,
-    mapRow = extractValues)
-
-  println(data.collect().toList)
-  /*
-  val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-
-  //val sql="select id,region,city,company,name from tb_user_imei"
-  //定义mysql信息
-  val jdbcDF = sqlContext.read.format("jdbc").options(Map(
-      "driver"->"com.mysql.jdbc.Driver",
-      "url"->"jdbc:mysql://localhost:14340",
-      "dbtable"->"db1.t1",
-      "user"-> "root",
-      "password"->"mypassword",
-      "lowerBound"->"0",
-      "upperBound"-> "1000",
-      "fetchSize"->"100"
-  )).load()
-  jdbcDF.show
-  jdbcDF.collect().take(20).foreach(println) //终端打印DF中的数据。*/
+  def useSparkConf() {
+    val conf = new SparkConf().setAppName("mysql").setMaster("local[4]")
+    val sc = new SparkContext(conf)
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+    val jdbcDF = sqlContext.read.format("jdbc").options(Map(
+      "driver" -> "com.mysql.jdbc.Driver",
+      "url" -> "jdbc:mysql://localhost:3306/db1",
+      "dbtable" -> "t1",
+      "user" -> "root",
+      "password" -> "mypassword",
+      "lowerBound" -> "0",
+      "upperBound" -> "1000",
+      "fetchSize" -> "100"
+    )).load()
+    jdbcDF.show
+    jdbcDF.collect().take(20).foreach(println) //终端打印DF中的数据。*/
+  }
+  useJdbcRDD()
 }
