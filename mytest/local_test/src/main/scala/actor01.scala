8091efc1ea7d6de592ff64ecba5158c0f5d2e3f2
kgisme170@163.com <kgisme170@163.com>
2018-08-22 05:26:06 +0800

    ok

--- mytest/local_test/src/main/scala/actor01.scala
+++ mytest/local_test/src/main/scala/actor01.scala
@@ -1,6 +1,17 @@
 import akka.actor.Actor
+import akka.actor.ActorSystem
+import akka.actor.Props
 
+class HelloActor extends Actor{
+  override def receive = {
+    case "hello" => println("hello received")
+    case _ => println("other messages")
+  }
+}
 
 object actor01 {
-
+  val system = ActorSystem("actor01")
+  val helloActor = system.actorOf(Props[HelloActor],name="myActor")
+  helloActor!"msg"
+  helloActor!"hello"
 }
