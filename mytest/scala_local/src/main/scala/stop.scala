c0c0b1023564244ca83eb16ade2d50f42adba110
kgisme170@163.com <kgisme170@163.com>
2018-08-22 06:42:02 +0800

    ok

--- mytest/scala_local/src/main/scala/stop.scala
+++ mytest/scala_local/src/main/scala/stop.scala
@@ -1,3 +1,31 @@
-class stop {
+import akka.actor._
+import akka.pattern.gracefulStop
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.duration._
+import scala.language.postfixOps
 
+case object TestActorStop
+
+class TestActor extends Actor {
+  def receive = {
+    case TestActorStop =>
+      context.stop(self)
+    case _ => println("TestActor got message")
+  }
+  override def postStop {println("TestActor: postStop")}
+}
+
+object stop extends App{
+  val system = ActorSystem("GracefulStopTest")
+  val testActor = system.actorOf(Props[TestActor], name="TestActor")
+  // try to stop the actor graceful
+  try {
+    val stopped: Future[Boolean] = gracefulStop(testActor, 2 seconds, TestActorStop)
+    Await.result(stopped, 3 seconds)
+    println("testActor was stopped")
+  } catch {
+    case e: akka.pattern.AskTimeoutException => e.printStackTrace
+  } finally {
+    system.shutdown
+  }
 }
