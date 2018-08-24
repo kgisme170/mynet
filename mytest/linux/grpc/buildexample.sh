d49fa26b29c1ac53f11cfc2b32e4c3fa0d51d713
kgisme170 <kgisme170@163.com>
2018-08-25 07:54:49 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: Ieabe14bf30866edc3b3cd2ee2dd5302b9ae45c47

--- mytest/linux/grpc/buildexample.sh
+++ mytest/linux/grpc/buildexample.sh
@@ -4,5 +4,4 @@ protoc --cpp_out=./ hello.proto
 protoc --cpp_out=./ embed.proto
 protoc --cpp_out=./ serialize.proto
 protoc --python_out=./ pytest.proto
-protoc --grpc_out=./ --plugin=protoc-gen-grpc=/usr/local/bin/grpc_cpp_plugin examples.proto
-#make -f ExamplesMakefile
+# protoc --grpc_out=./ --plugin=protoc-gen-grpc=/usr/local/bin/grpc_cpp_plugin examples.proto
