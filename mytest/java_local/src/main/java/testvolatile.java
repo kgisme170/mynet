ceeea19d261b8048e260b4c9d63604d655572b96
kgisme170 <kgisme170@163.com>
2018-08-24 11:39:41 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: Idb175db902d4454171f57e921ff3a8f333743f8a

--- mytest/java_local/src/main/java/testvolatile.java
+++ mytest/java_local/src/main/java/testvolatile.java
@@ -3,7 +3,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class testvolatile{
     public static CountDownLatch l = new CountDownLatch(10);
     public static void main(String[] args)throws InterruptedException{
-        AtomicInteger count = new AtomicInteger(0);
+        final AtomicInteger count = new AtomicInteger(0);
         for(int i=0;i<10;++i){
             new Thread(){
                 public void run(){
