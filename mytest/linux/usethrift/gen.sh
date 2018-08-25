7e374c69b304c4bc7f7b510d3ee91a825d8a0b7c
kgisme170 <kgisme170@163.com>
2018-08-25 20:17:57 +0800

    thrift

--- mytest/linux/usethrift/gen.sh
+++ mytest/linux/usethrift/gen.sh
@@ -4,12 +4,11 @@ thrift -r --gen cpp -o build worker.thrift
 thrift -r --gen cpp -o build student.thrift
 
 GEN="build/gen-cpp"
-INCLUDE="-I$ODPS_SRC/thirdparty/compatible/include/thrift -I$GEN"
-LINKPATH="-L$ODPS_SRC/thirdpaty/compatible/lib"
-LIB="-lthrift"
+INCLUDE="-I/usr/local/include/thrift -I$GEN"
+LIB="-L /usr/local/lib -lthrift -lthriftnb -levent"
+CXX="g++"
+$CXX workerclient.cpp $GEN/MyManager.cpp -o build/MyManager_client $INCLUDE $LINKPATH $LIB
+$CXX $GEN/MyManager_server.skeleton.cpp $GEN/MyManager.cpp -o build/MyManager_server $INCLUDE $LIB
 
-g++ workerclient.cpp $GEN/MyManager.cpp -o build/MyManager_client $INCLUDE $LINKPATH $LIB
-g++ $GEN/MyManager_server.skeleton.cpp $GEN/MyManager.cpp -o build/MyManager_server $INCLUDE $LINKPATH $LIB
-
-g++ studentclient.cpp $GEN/Serv.cpp $GEN/student_types.cpp -o build/Student_client $INCLUDE $LINKPATH $LIB
-g++ studentserver.cpp $GEN/Serv.cpp $GEN/student_types.cpp -o build/Student_server $INCLUDE $LINKPATH $LIB
+$CXX studentclient.cpp $GEN/Serv.cpp $GEN/student_types.cpp -o build/Student_client $INCLUDE $LIB
+$CXX studentserver.cpp $GEN/Serv.cpp $GEN/student_types.cpp -o build/Student_server $INCLUDE $LIB
