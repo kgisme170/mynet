8b84c21b105b4a7671a00763aaa61c8979e9ba09
kgisme170@163.com <kgisme170@163.com>
2018-08-26 15:39:40 +0800

    ok

--- mytest/java_mr/src/main/java/w1_args.java
+++ mytest/java_mr/src/main/java/w1_args.java
@@ -14,52 +14,22 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class WordCount2 extends Configured implements Tool {
-    private final static LongWritable ONE = new LongWritable(1L);
-
-    // Mapper Class, Counts words in each line. For each line, break the line into words and emits them as (word, 1)
-
-    public static class MapClass extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
-        private final static IntWritable one = new IntWritable(1);
-        private Text word = new Text();
-
-        public void map(LongWritable key, Text value,
-                        OutputCollector<Text, IntWritable> output,
-                        Reporter reporter) throws IOException {
-
-            String line = value.toString();
-            StringTokenizer itr = new StringTokenizer(line);
-            while (itr.hasMoreTokens()) {
-                word.set(itr.nextToken());
-                output.collect(word, one);
-            }
-        }
-    }
-
-    // Reducer class that just emits the sum of the input values.
-
-    public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
-        public void reduce(Text key, Iterator<IntWritable> values,
-                           OutputCollector<Text, IntWritable> output,
-                           Reporter reporter) throws IOException {
-            int sum = 0;
-            while (values.hasNext()) {
-                sum += values.next().get();
-            }
-            output.collect(key, new IntWritable(sum));
-        }
-    }
-
+public class w1_args extends Configured implements Tool {
     static int printUsage() {
-        System.out.println("wordcount [-m #mappers ] [-r #reducers] input_file output_file");
+        System.out.println("w2 [-m #mappers ] [-r #reducers] input_file output_file");
         ToolRunner.printGenericCommandUsage(System.out);
         return -1;
     }
 
+    public static void main(String[] args) throws Exception {
+        int res = ToolRunner.run(new Configuration(), new w1_args(), args);
+        System.exit(res);
+    }
+
     public int run(String[] args) throws Exception {
 
-        JobConf conf = new JobConf(getConf(), WordCount2.class);
-        conf.setJobName("wordcount");
+        JobConf conf = new JobConf(getConf(), w1_args.class);
+        conf.setJobName("-----------w2----------");
 
         // the keys are words (strings)
         conf.setOutputKeyClass(Text.class);
@@ -96,15 +66,38 @@ public class WordCount2 extends Configured implements Tool {
                     other_args.size() + " instead of 2.");
             return printUsage();
         }
-        FileInputFormat.setInputPaths(conf, new Path((String)other_args.get(0)));
-        FileOutputFormat.setOutputPath(conf, new Path((String)other_args.get(1)));
+        FileInputFormat.setInputPaths(conf, new Path((String) other_args.get(0)));
+        FileOutputFormat.setOutputPath(conf, new Path((String) other_args.get(1)));
 
         JobClient.runJob(conf);
         return 0;
     }
 
-    public static void main(String[] args) throws Exception {
-        int res = ToolRunner.run(new Configuration(), new WordCount2(), args);
-        System.exit(res);
+    public static class MapClass extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(LongWritable key, Text value,
+                        OutputCollector<Text, IntWritable> output,
+                        Reporter reporter) throws IOException {
+            String line = value.toString();
+            StringTokenizer itr = new StringTokenizer(line);
+            while (itr.hasMoreTokens()) {
+                word.set(itr.nextToken());
+                output.collect(word, one);
+            }
+        }
+    }
+
+    public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+        public void reduce(Text key, Iterator<IntWritable> values,
+                           OutputCollector<Text, IntWritable> output,
+                           Reporter reporter) throws IOException {
+            int sum = 0;
+            while (values.hasNext()) {
+                sum += values.next().get();
+            }
+            output.collect(key, new IntWritable(sum));
+        }
     }
 }
