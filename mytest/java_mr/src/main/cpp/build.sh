68f5c207c4dd74b7180fce4453342f42b19aa4b3
kgisme170@163.com <kgisme170@163.com>
2018-09-01 21:34:07 +0800

    ok

--- mytest/java_mr/src/main/cpp/build.sh
+++ mytest/java_mr/src/main/cpp/build.sh
@@ -5,7 +5,7 @@
 export CLASSPATH=`hadoop classpath`
 export LD_LIBRARY_PATH=$HADOOP_LIB/native:$JAVA_HOME/jre/lib/amd64/server:$LD_LIBRARY_PATH
 
-gcc dfsio.c -I$HADOOP_LIB/include -I$HADOOP_HOME/include -I$JAVA_HOME/include -L$HADOOP_LIB/native -L$JAVA_HOME/jre/lib/amd64/server -ljvm -lhdfs -o dfsio
-g++ w.cpp -I$HADOOP_LIB/include -I$HADOOP_HOME/include -I$JAVA_HOME/include -L$HADOOP_LIB/native -lhadooppipes -lhadooputils -lpthread -lcrypto -o w -fPIC
-#./dfsio "/tmp/test01"
-mapred pipes -D hadoop.pipes.java.recordreader=true hadoop.pipes.java.recordwriter=true -input war-and-peace-input -output war-and-peace-output -program bin/w
+#gcc dfsio.c -I$HADOOP_LIB/include -I$HADOOP_HOME/include -I$JAVA_HOME/include -L$HADOOP_LIB/native -L$JAVA_HOME/jre/lib/amd64/server -ljvm -lhdfs -o dfsio
+#g++ w.cpp -I$HADOOP_LIB/include -I$HADOOP_HOME/include -I$JAVA_HOME/include -L$HADOOP_LIB/native -lhadooppipes -lhadooputils -lpthread -lcrypto -o w -fPIC
+./dfsio "/tmp/test01"
+#mapred pipes -D hadoop.pipes.java.recordreader=true hadoop.pipes.java.recordwriter=true -input war-and-peace-input -output war-and-peace-output -program bin/w
