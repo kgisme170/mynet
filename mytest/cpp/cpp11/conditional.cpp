75a383f4ad2d9335e98641fb8cb7f942cd163032
kgisme170@163.com <kgisme170@163.com>
2018-09-13 00:36:53 +0800

    ok

--- mytest/cpp/cpp11/conditional.cpp
+++ mytest/cpp/cpp11/conditional.cpp
@@ -2,9 +2,8 @@
 #include <thread>
 #include <condition_variable>
 #include <mutex>
+#include <chrono>
 using namespace std;
-/*功能: 测试条件变量的功能*/
-
 mutex m;
 condition_variable c;
 bool ok=false;
@@ -13,18 +12,16 @@ int main(){
         unique_lock<mutex> _(m);
         c.wait(_, [](){return ok;});
         cout<<"等待主线程完成\n";
-        this_thread::sleep_for(10s);
+        this_thread::sleep_for(std::chrono::seconds(10));
         cout<<"辅助线程睡眠完成\n";
         c.notify_one();
     });
-
-    this_thread::sleep_for(1s);
+    this_thread::sleep_for(std::chrono::seconds(1));
     ok=true;
     c.notify_one();
-
     cout<<"主线程等待\n";
     unique_lock<mutex> _(m);
-    this_thread::sleep_for(1s);
+    this_thread::sleep_for(std::chrono::seconds(1));
     c.wait(_, []{return true;});
     t.join();
     return 0;
