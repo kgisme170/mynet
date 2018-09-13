c0af34d4fbc9dff7615f1a98a31a10b390a1c5d1
kgisme170 <kgisme170@163.com>
2018-09-13 17:25:13 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I91b23f85de7a2a20e7c1a6d4c7898e8a84382b70

---
+++ mytest/cpp/cpp11/noexcept.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+struct A{
+    A(){}
+    A(const A&){ cout << "A copy ctor\n"; }
+    A(A&&){ cout << "A move ctor\n"; }
+};
+struct B{
+    B(){}
+    B(const B&){ cout << "A copy ctor\n"; }
+    B(B&&)noexcept{ cout << "B move ctor\n"; }
+};
+template<class T>void f(){
+    vector<T> vi(2);
+    size_t c = 0;
+    cout<<"========\n";
+    for (size_t i = 0; i<4; ++i)
+    {
+        cout<<"begin emplace_back...........\n";
+        vi.emplace_back(T());
+        cout<<"end emplace_back...........\n";
+    }
+    vi.resize(0);
+}
+int main(){
+    f<A>();
+    f<B>();
+    return 0;
+}
