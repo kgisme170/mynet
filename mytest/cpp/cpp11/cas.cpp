ad79ade48b2dc85c332a82106ae62f3ef3c34bd6
kgisme170@163.com <kgisme170@163.com>
2018-09-14 02:55:00 +0800

    ok

---
+++ mytest/cpp/cpp11/cas.cpp
@@ -0,0 +1,16 @@
+#include <atomic>
+#include <iostream>
+using namespace std;
+
+struct Big {
+  int i;
+  int j;
+  int k[100];
+};
+
+int main() {
+  Big value, expect;
+  atomic<Big> ab;
+  ab.compare_exchange_weak(expect, value);
+  return 0;
+}
