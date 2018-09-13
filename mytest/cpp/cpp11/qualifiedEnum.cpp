5af62e4170f77ba2e4bd118b1747ee241c8fb455
kgisme170 <kgisme170@163.com>
2018-09-13 20:27:49 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I6cddf44b70d67dad8c92bf0e3aa4f16f70064378

---
+++ mytest/cpp/cpp11/qualifiedEnum.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+namespace m{
+class my{
+public:
+    enum A{
+        u=1,
+        v=2,
+        w=3
+    };
+    static A f(A a){
+        return (A)(a + A::u);
+    }
+};
+int main(){
+    using namespace m;
+    my::A r=my::f(my::u);
+    return 0;
+}
