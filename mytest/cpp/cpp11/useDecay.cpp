6b337a271f4451c39239dc4ff4d36e4b7a39fb66
kgisme170 <kgisme170@163.com>
2018-09-13 20:58:41 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: Id86030cb22cb89b45d8426d77b2689aeeae3ab19

---
+++ mytest/cpp/cpp11/useDecay.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<map>
+#include<string>
+using namespace std;
+template<class T>
+void func1(T&& param) {
+    if (std::is_same<T,int>::value)
+        cout << "int\n";
+    else
+        cout << "not int\n";
+}
+template<class T>
+void func2(T&& param) {
+    if (std::is_same<typename std::decay<T>::type,int>::value)
+        cout << "int\n";
+    else
+        cout << "not int\n";
+}
+int main(){
+    std::pair<std::string, int> p = make_pair("bar", 0);
+    int v = 3;
+    func1(v);  //prints "param is not an int"!!!!
+    func2(v);
+    return 0;
+}
