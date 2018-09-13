84ff5f0873378bca2cbac0ff1d37322276af710b
kgisme170 <kgisme170@163.com>
2018-09-13 21:07:01 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: Ia5c23b5c66e1e9f621a78390b356f17b5f009301

---
+++ mytest/cpp/cpp14/decltypeAuto.cpp
@@ -0,0 +1,21 @@
+#include<utility>
+//foward类型作为返回，带有value category，简单用auto会丢失
+template<class Fun, class... Args>
+decltype(auto) Example(Fun fun, Args&&... args)
+{
+    return fun(std::forward<Args>(args)...);
+}
+
+template<int i>
+struct Int {};
+
+constexpr auto iter(Int<0>) -> Int<0>;
+
+template<int i>
+constexpr auto iter(Int<i>) -> decltype(auto)
+{ return iter(Int<i-1>{}); }
+
+int main() {
+    decltype(iter(Int<10>{})) a;
+    return 0;
+}
