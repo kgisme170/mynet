60037e0f5577e6d4a8199d313dcb2c6e2ba41dc5
kgisme170 <kgisme170@163.com>
2018-09-14 10:28:26 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I6374b42774dc43c959a01a42eb5b33e7582a5d6e

---
+++ mytest/cpp/cpp14/traits.cpp
@@ -0,0 +1,19 @@
+#include<type_traits>
+using namespace std;
+template<int i>
+struct place_holder{};
+using _1 = place_holder<1>;
+using _2 = place_holder<2>;
+
+template<typename T>
+struct is_placeholder:public integral_constant<int,0>{};
+template<int i>
+struct is_placeholder<place_holder<i> >:public integral_constant<int,i>{};
+template<typename T>
+auto check(){
+    return is_placeholder<T>::value;
+}
+int main(){
+    auto i=check<_1>();
+    return 0;
+}
