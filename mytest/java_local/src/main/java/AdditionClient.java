32ea30dd7892b05f7f5b461e2ef059b97ae7dfe2
kgisme170 <kgisme170@163.com>
2018-09-17 16:02:33 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: Ic28ef10a5f2e521218d2556c639debc07fed53fd

---
+++ mytest/java_local/src/main/java/AdditionClient.java
@@ -0,0 +1,22 @@
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+import org.apache.thrift.transport.TTransportException;
+
+public class AdditionClient {
+    public static void main(String[] args) {
+        try {
+            TTransport transport = new TSocket("localhost", 9090);
+            transport.open();
+            AdditionService.Client client = new AdditionService.Client(new TBinaryProtocol(transport));
+            System.out.println(client.add(100, 200));
+            transport.close();
+        } catch (TTransportException e) {
+            e.printStackTrace();
+        } catch (TException x) {
+            x.printStackTrace();
+        }
+    }
+}
