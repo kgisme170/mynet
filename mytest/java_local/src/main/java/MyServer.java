ea6256d41d8d08c98fe09bb3601a5eb14a7e16b1
kgisme170 <kgisme170@163.com>
2018-09-17 16:19:39 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I76ae352a716d32847d43cee53133d560bd291261

--- mytest/java_local/src/main/java/MyServer.java
+++ mytest/java_local/src/main/java/MyServer.java
@@ -3,19 +3,18 @@ import org.apache.thrift.transport.TServerTransport;
 import org.apache.thrift.server.TServer;
 import org.apache.thrift.server.TServer.Args;
 import org.apache.thrift.server.TSimpleServer;
-
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TBinaryProtocol.Factory;
 public class MyServer {
     public static void main(String[] args) {
         AdditionService.Processor<AdditionServiceHandler> processor =
             new AdditionService.Processor<AdditionServiceHandler>(new AdditionServiceHandler());
         try {
             TServerTransport serverTransport = new TServerSocket(9090);
-            TServer server = new TSimpleServer(
-            new Args(serverTransport).processor(processor));
+            TServer server = new TSimpleServer(new Args(serverTransport).processor(processor).protocolFactory(new TBinaryProtocol.Factory(true, true)));
 
             // Use this for a multithreaded server
-            // TServer server = new TThreadPoolServer(new
-            // TThreadPoolServer.Args(serverTransport).processor(processor));
+            // TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(serverTransport).processor(processor));
 
             System.out.println("Starting the simple server...");
             server.serve();
