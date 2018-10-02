dd24bedd4bfd5afbec557e165f217123b5bdd2d1
kgisme170@163.com <kgisme170@163.com>
2018-10-02 17:23:03 +0800

    ok

--- mytest/cpp/gtest/gtest01.cpp
+++ mytest/cpp/gtest/gtest01.cpp
@@ -1,4 +1,7 @@
 #include <gtest/gtest.h>
+#include <iostream>
+#include <typeinfo>
+using namespace std;
 int Foo(int a, int b)
 {
     if (a == 0 || b == 0)
@@ -10,9 +13,37 @@ int Foo(int a, int b)
         return b;
     return Foo(b, c);
 }
-
-TEST(FooTest, HandleNoneZeroInput)
+class FooTest : public testing::Test {
+protected:
+    static void SetUpTestCase() {
+        cout<<"FooTest SetUp\n";
+    }
+    static void TearDownTestCase() {
+        cout<<"FooTest TearDown\n";
+    }
+};
+class FooTest2: public FooTest{
+protected:
+    static void SetUpTestCase() {
+        cout<<"FooTest2 SetUp\n";
+    }
+    static void TearDownTestCase() {
+        cout<<"FooTest2 TearDown\n";
+    }
+    virtual void SetUp(){cout<<"case setup\n";}
+    virtual void TearDown(){cout<<"case teardown\n";}
+};
+TEST_F(FooTest, HandleNoneZeroInput)
 {
     EXPECT_EQ(2, Foo(4, 10));
     EXPECT_EQ(6, Foo(30, 18));
+}
+TEST_F(FooTest, Case2)
+{
+    EXPECT_EQ(2, Foo(4, 10));
+    EXPECT_EQ(6, Foo(30, 18));
+    cout<<typeid(*this).name()<<endl;
+}
+TEST_F(FooTest2, Success){
+    ASSERT_TRUE(true);
 }
