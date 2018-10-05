8db13d2f77a192e1205e6166a6242f8569128ed9
kgisme170@163.com <kgisme170@163.com>
2018-10-05 09:10:47 +0800

    ok

--- mytest/cpp/testgmock.sh
+++ mytest/cpp/testgmock.sh
@@ -3,8 +3,9 @@ TESTFILE=/tmp/testgmock.cpp
 cat << EOF > $TESTFILE
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
-int main(){
-    return 0;
+int main(int argc, char* argv[]){
+    testing::InitGoogleTest(&argc,argv);
+    return RUN_ALL_TESTS();
 }
 EOF
-g++ $TESTFILE -c -o /tmp/testgmock.o > /dev/null 2>&1
+g++ $TESTFILE -c -o /tmp/testgmock.o -lgmock -lgtest -lpthread> /dev/null 2>&1
