c8024715bd2208353f49fb86fed62cf6f0e6a00e
kgisme170@163.com <kgisme170@163.com>
2018-10-19 00:02:19 +0800

    ok

---
+++ mytest/streaming/src/main/java/clientApp.java
@@ -0,0 +1,25 @@
+import java.net.*;
+import java.io.*;
+public class clientApp{
+    public static void main(String[] args){
+        try{
+            System.out.println("Defining new Socket");
+            ServerSocket soc=new ServerSocket(9087);
+            System.out.println("Waiting for Incoming Connection");
+            Socket clientSocket=soc.accept();
+            System.out.println("Connection Received");
+            OutputStream os=clientSocket.getOutputStream();
+            BufferedReader read=new BufferedReader(new InputStreamReader(System.in));
+            while(true){
+                PrintWriter out=new PrintWriter(os,true);
+                System.out.println(">");
+                String data=read.readLine();
+                System.out.println("Write to socket");
+                out.println(data);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
+
