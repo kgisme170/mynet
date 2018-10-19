b3ab2105ece15f766666d865615202a5ca072d39
kgisme170@163.com <kgisme170@163.com>
2018-10-20 07:58:38 +0800

    ok

---
+++ mytest/streaming/src/main/scala/useFlume.scala
@@ -0,0 +1,33 @@
+import java.net.InetSocketAddress
+
+import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.flume._
+object useFlume {
+  def main(args:Array[String]): Unit ={
+    println("创建配置")
+    val conf = new SparkConf()
+    conf.setAppName("流数据加载").setMaster("local[4]")
+    println("接收流context")
+    val ctx = new StreamingContext(conf,Seconds(2))
+    var addresses = new Array[InetSocketAddress](2)
+    addresses(0) = new InetSocketAddress("localhost", 4949)
+    addresses(1) = new InetSocketAddress("localhost", 4950)
+    val flumeStream = FlumeUtils.createPollingStream(ctx,addresses,StorageLevel.MEMORY_AND_DISK_SER_2,1000,1)
+    //val os = new ObjectOutputStream(Console.out)
+    flumeStream.foreachRDD((rdd:RDD[SparkFlumeEvent])=>{
+      val array = rdd.collect()
+      println("开始打印-----")
+      println("Event size="+array.size)
+      for(e<-array){
+        val payLoad = e.event.getBody()
+        println(new String(payLoad.array()))
+      }
+      println("结束打印=====")
+    })
+    ctx.start()
+    ctx.awaitTermination()
+  }
+}
