d4dfa7b07c8b78880b1bea9dc09ae472a8da87d4
kgisme170@163.com <kgisme170@163.com>
2018-10-20 08:14:43 +0800

    ok

--- mytest/streaming/src/main/scala/useFlume.scala
+++ mytest/streaming/src/main/scala/useFlume.scala
@@ -12,7 +12,7 @@ object useFlume {
     conf.setAppName("流数据加载").setMaster("local[4]")
     println("接收流context")
     val ctx = new StreamingContext(conf,Seconds(2))
-    var addresses = new Array[InetSocketAddress](2)
+    val addresses = new Array[InetSocketAddress](2)
     addresses(0) = new InetSocketAddress("localhost", 4949)
     addresses(1) = new InetSocketAddress("localhost", 4950)
     val flumeStream = FlumeUtils.createPollingStream(ctx,addresses,StorageLevel.MEMORY_AND_DISK_SER_2,1000,1)
