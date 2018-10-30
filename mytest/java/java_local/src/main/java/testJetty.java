f912945d11830351a375b41e06f8689aa11e27fe
kgisme170@163.com <kgisme170@163.com>
2018-10-30 03:58:33 -0700

    ok

---
+++ mytest/java/java_local/src/main/java/testJetty.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+public class testJetty {
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+        server.setHandler(new AbstractHandler() {
+            @Override
+            public void handle(String s, Request request, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException, ServletException {
+                httpServletResponse.setContentType("text/html;charset=utf-8");
+                httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+                request.setHandled(true);
+                httpServletResponse.getWriter().println("<h1>testJetty</h1>");
+            }
+        });
+        server.start();
+        server.join();
+    }
+}
