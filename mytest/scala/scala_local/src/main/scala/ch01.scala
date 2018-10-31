cc6d09562394d1336b6453623e771d9d04e31c6b
kgisme170@163.com <kgisme170@163.com>
2018-10-31 09:44:17 -0700

    ok

--- mytest/scala/scala_local/src/main/scala/ch01.scala
+++ mytest/scala/scala_local/src/main/scala/ch01.scala
@@ -1,15 +1,33 @@
-object ch01 extends App{
-  for(i<-1 to 10)println(i)
-  import scala.util.control.Breaks._
+import java.io.FileNotFoundException
+
+object ch01 extends App {
+  for (i <- 1 to 10) println(i)
+
   import scala.util.Random
-  breakable{
-    while(true){
-      val r= new Random()
+  import scala.util.control.Breaks._
+
+  breakable {
+    while (true) {
+      val r = new Random()
       val i = r.nextInt(10)
       println(i)
-      if(i>5){
+      if (i > 5) {
         break
       }
     }
+  }
+
+  try {
+    import scala.io.Source
+    val file = Source.fromFile("/tmp/file01")
+    val lines = file.getLines()
+    for (l <- lines) {
+      println(l)
+    }
+  } catch {
+    case ex: FileNotFoundException => println("找不到文件")
+    case ex: Exception => println(ex)
+  } finally {
+    println("結束");
   }
 }
