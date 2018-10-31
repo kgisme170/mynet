a95f57f4217b12f9091932b29881ef4854f297a4
liming.glm <liming.glm@alibaba-inc.com>
2018-10-31 21:47:59 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I754b5bd9227cb1c21ec6161ed582981f2fd65d58

--- mytest/java/java_local/src/main/java/testPwd.java
+++ mytest/java/java_local/src/main/java/testPwd.java
@@ -3,7 +3,7 @@ import java.io.File;
 public class testPwd {
     public static void main(String [] args) {
         //touch ~/a.txt
-        File f = new File("~/a.txt");
+        File f = new File(System.getProperty("user.home") + "/a.txt");
         System.out.println(f.exists());
     }
 }
---
+++ mytest/scala/useSbt/project/assembly.sbt
@@ -0,0 +1,3 @@
+addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.14.7")
+addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.9.0")
+addSbtPlugin("com.typesafe.sbt" % "sbt-git" % "1.0.0")
