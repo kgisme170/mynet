b3195386e40cc9a0ba5043eaea770fcc54885b3a
liming.glm <liming.glm@alibaba-inc.com>
2018-11-01 16:08:36 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I84f0e4ffc681e120edd66e7b4a11e7c4008a12f7

---
+++ mytest/cloud/testMeta/src/main/java/RamOpenApi.java
@@ -0,0 +1,32 @@
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.profile.DefaultProfile;
+import com.aliyuncs.profile.IClientProfile;
+import com.aliyuncs.ram.model.v20150501.*;
+//https://help.aliyun.com/document_detail/62184.html 是openapi的sdk文档，不包含innerapi
+public class RamOpenApi {
+
+    public static void main(String[] args) {
+        if (args.length != 4) {
+            System.out.println("运行参数有3个: [regionId], [accessKeyId], [secret]");
+        }
+        String regionId = args[1];
+        String accessKeyId = args[2];
+        String secret = args[3];
+        IClientProfile profile = DefaultProfile.getProfile(regionId, accessKeyId, secret);
+        DefaultAcsClient client = new DefaultAcsClient(profile);
+        final CreateUserRequest request = new CreateUserRequest();
+        request.setUserName("ramtest001");
+        try {
+            final CreateUserResponse response = client.getAcsResponse(request);
+
+            System.out.println("UserName: " + response.getUser().getUserName());
+            System.out.println("CreateTime: " + response.getUser().getCreateDate());
+        } catch (ClientException e) {
+            System.out.println("Failed.");
+            System.out.println("Error code: " + e.getErrCode());
+            System.out.println("Error message: " + e.getErrMsg());
+        }
+
+    }
+}
