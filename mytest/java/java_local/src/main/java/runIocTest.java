3cde8a2bd0b4f8787a80a1753ba62c0a09ce5654
liming.glm <liming.glm@alibaba-inc.com>
2018-11-01 20:18:53 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: Ied2de8ae3b5feb903dd3fa3fca019635aadbf74c

--- mytest/java/java_local/src/main/java/runIocTest.java
+++ mytest/java/java_local/src/main/java/runIocTest.java
@@ -1,9 +1,13 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
+import static java.lang.System.getProperty;
+
 public class runIocTest {
     public static void main(String[] args) {
-        ApplicationContext factory = new FileSystemXmlApplicationContext("applicationContext.xml");
+        String fileName = "../../src/applicationContext.xml";
+        String s = testFastJsonComplex.class.getResource(".") + fileName;
+        ApplicationContext factory = new FileSystemXmlApplicationContext(s);
         ArrayIocTest test = (ArrayIocTest) factory.getBean("arrayIocTest");
         test.testData();
     }
