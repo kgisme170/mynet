d4060c4c93aa873f985bd8f57c376148c8cb912c
liming.glm <liming.glm@alibaba-inc.com>
2018-11-02 19:20:08 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I9cea4f20fca79bc3663c2102d79916422c1e85a5

--- mytest/java/gs-spring-boot/src/main/java/hello/HelloController.java
+++ mytest/java/gs-spring-boot/src/main/java/hello/HelloController.java
@@ -1,5 +1,6 @@
 package hello;
 
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,5 +11,17 @@ public class HelloController {
     public String index() {
         return "Greetings from Spring Boot!";
     }
-    
+
+    @RequestMapping("/another")
+    public String another() {
+        return "another!";
+    }
+
+    @RequestMapping("/index")
+    public String index(ModelMap map) {
+        // 加入一个属性，用来在模板中读取
+        map.addAttribute("host", "http://my.aliyun.com");
+        // return模板文件的名称，对应src/main/resources/templates/index.html
+        return "index";
+    }
 }
