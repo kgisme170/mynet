84337064d0af3f62cf2a537d158a4e568c7d0477
liming.glm <liming.glm@alibaba-inc.com>
2018-11-03 00:54:57 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I653214f4ed37a0857867cb0f43ec458040812f9b

--- mytest/java/java_local/src/main/java/testDerby.java
+++ mytest/java/java_local/src/main/java/testDerby.java
@@ -13,28 +13,25 @@ public class testDerby {
             Properties props = new Properties();
             props.put("user", "用户1");
             props.put("password", "用户11");
-            //create and connect the database named helloDB
+
             Connection conn = DriverManager.getConnection("jdbc:derby:hello;create=true", props);
             System.out.println("创建数据库hello");
             conn.setAutoCommit(false);
 
-            // create a table and insert two records
             Statement s = conn.createStatement();
             s.execute("create table t(name varchar(40), age int)");
             System.out.println("创建表 t");
             s.execute("insert into t values('张三', 26)");
             s.execute("insert into t values ('李四', 32)");
-            // list the two records
             ResultSet rs = s.executeQuery(
                     "SELECT name, age FROM t ORDER BY age");
             System.out.println("----------------");
             while (rs.next()) {
                 StringBuilder builder = new StringBuilder(rs.getString(1));
-                builder.append("t");
+                builder.append("\t");
                 builder.append(rs.getInt(2));
                 System.out.println(builder.toString());
             }
-            // delete the table
             s.execute("drop table t");
             System.out.println("删除表 t");
 
@@ -45,15 +42,13 @@ public class testDerby {
             conn.close();
             System.out.println("提交 transaction 并关闭连接");
 
-            try { // perform. a clean shutdown
+            try {
                 DriverManager.getConnection("jdbc:derby:;shutdown=true");
             } catch (SQLException se) {
                 System.out.println("Database 正常关闭");
             }
         } catch (Throwable e) {
-            // handle the exception
             e.printStackTrace();
         }
-        System.out.println("SimpleApp finished");
     }
 }
---
+++ mytest/java/java_local/src/main/java/testPg.java
@@ -0,0 +1,37 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class testPg {
+    public static void main(String args[]) {
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection conn = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5432/postgres",
+                            "postgres", "password");
+            conn.setAutoCommit(false);
+
+            Statement s = conn.createStatement();
+            ResultSet rs = s.executeQuery(
+                    "SELECT name, age FROM company ORDER BY age");
+            System.out.println("----------------");
+            while (rs.next()) {
+                StringBuilder builder = new StringBuilder(rs.getString(1));
+                builder.append("\t");
+                builder.append(rs.getInt(2));
+                System.out.println(builder.toString());
+            }
+            rs.close();
+            s.close();
+            System.out.println("关闭返回的 set 和 statement");
+            conn.commit();
+            conn.close();
+            System.out.println("提交 transaction 并关闭连接");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(0);
+        }
+    }
+}
