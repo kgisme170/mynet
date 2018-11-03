fe6a3e8b8165e8cbd5a298ade9e4b4c81a084169
liming.glm <liming.glm@alibaba-inc.com>
2018-11-03 19:50:47 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: Ic691a72f2bd8819a3e8b2bdcf920135fdc99d42e

---
+++ mytest/java/useSlf4j/src/main/java/useSlf.java
@@ -0,0 +1,9 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class useSlf {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(useSlf.class);
+        logger.info("Hello World");
+    }
+}
