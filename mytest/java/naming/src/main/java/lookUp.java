528ddc20a5d5aa48d393abcc9b0d18243afc144b
liming.glm <liming.glm@alibaba-inc.com>
2018-11-05 19:53:03 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: Ia1cf8241deaeb1ac028f10f817caa1f007ca7fe4

---
+++ mytest/java/naming/src/main/java/lookUp.java
@@ -0,0 +1,22 @@
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.Hashtable;
+
+public class lookUp {
+    public static void main(String[] args) {
+        Hashtable<String, String> env = new Hashtable<>();
+        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.fscontext.RefFSContextFactory");
+        env.put(Context.PROVIDER_URL, "file:///");
+
+        String name = "/tmp";
+        try {
+            Context ctx = new InitialContext(env);
+
+            Object obj = ctx.lookup(name);
+            System.out.println(name + " is bound to: " + obj + "\t[" + obj.getClass() + ']');
+        } catch (NamingException e) {
+            System.err.println("Problem looking up " + name + ": " + e);
+        }
+    }
+}
