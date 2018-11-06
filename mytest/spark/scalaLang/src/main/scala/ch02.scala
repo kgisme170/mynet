5ecaefaab6448bad02b40d04e1701e62b3e9ebac
kgisme170@163.com <kgisme170@163.com>
2018-11-06 11:15:19 -0800

    scala

--- mytest/spark/scalaLang/src/main/scala/ch02.scala
+++ mytest/spark/scalaLang/src/main/scala/ch02.scala
@@ -16,4 +16,14 @@ object ch02 extends App {
   println("\n===")
   for (_ <- O) print(_)
   println("\n===")
+
+  val bigData = Map("a1" -> 1, "a2" -> 2, "a3" -> 3)
+  println(bigData.drop(1))
+  val bigData2 = scala.collection.mutable.Map("a1" -> 1, "a2" -> 2, "a3" -> 3)
+  bigData2 += ("a4" -> 4)
+  bigData2 += ("a4" -> 4)
+  for(d<-bigData2)println(d)
+  println
+  val t1 = (1,2,"hello")
+  println(t1._1)
 }
