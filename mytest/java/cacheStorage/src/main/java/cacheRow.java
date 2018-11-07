86c96458a2c19461bc48b463d0e3f35cab7cdee1
liming.glm <liming.glm@alibaba-inc.com>
2018-11-07 20:32:58 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I0a9ae861d0c63f983b87ebd6d2e1edce95eb3e33

---
+++ mytest/java/cacheStorage/src/main/java/cacheRow.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public class cacheRow {
+    String projectName;
+    String accountId;
+    List<String> quotaGroup;
+}
--- mytest/java/useKafka/src/main/java/testKafka.java
+++ mytest/java/useKafka/src/main/java/testKafka.java
@@ -3,10 +3,43 @@ import kafka.utils.ZKStringSerializer$;
 import kafka.utils.ZkUtils;
 import org.I0Itec.zkclient.ZkClient;
 import org.I0Itec.zkclient.ZkConnection;
+import org.apache.kafka.clients.consumer.*;
+import org.apache.log4j.BasicConfigurator;
+
+import java.util.Arrays;
 import java.util.Properties;
+
+import static org.apache.kafka.common.resource.ResourceType.TOPIC;
 
 public class testKafka {
     public static void main(String[] args) {
+        BasicConfigurator.configure();
+        String group = "group-1";
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, group);
+        //props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true"); // 自动commit
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000"); // 自动commit的间隔
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        Consumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+        consumer.subscribe(Arrays.asList("test")); // 可消费多个topic,组成一个list
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(1000);
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.printf("offset = %d, key = %s, value = %s \n", record.offset(), record.key(), record.value());
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    public static void createTopic(String topicName){
         ZkClient zkClient = null;
         try {
             String zookeeperHosts = "localhost:2181"; // 如有多个zk，用逗号隔开 String zookeeperHosts = "192.168.20.1:2181,192.168.20.2:2181";
@@ -16,13 +49,11 @@ public class testKafka {
             zkClient = new ZkClient(zookeeperHosts, sessionTimeOutInMs, connectionTimeOutInMs, ZKStringSerializer$.MODULE$);
             ZkUtils zkUtils = new ZkUtils(zkClient, new ZkConnection(zookeeperHosts), false);
 
-            String topicName = "test01";
             int noOfPartitions = 2;
             int noOfReplication = 1;
             Properties topicConfiguration = new Properties();
 
             AdminUtils.createTopic(zkUtils, topicName, noOfPartitions, noOfReplication, topicConfiguration, null);
-
         } catch (Exception ex) {
             ex.printStackTrace();
         } finally {
