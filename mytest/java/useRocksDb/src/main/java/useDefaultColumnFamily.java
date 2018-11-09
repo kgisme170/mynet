a71e82fa63c624280f4e83f93ad0f827f9dbe546
liming.glm <liming.glm@alibaba-inc.com>
2018-11-09 20:01:27 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I440b31651f85573221b411c7921159d2b4406117

---
+++ mytest/java/useRocksDb/src/main/java/useDefaultColumnFamily.java
@@ -0,0 +1,81 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+
+import org.rocksdb.Options;
+import org.rocksdb.RocksDB;
+import org.rocksdb.RocksDBException;
+import org.rocksdb.RocksIterator;
+public class useDefaultColumnFamily {
+    static {
+        RocksDB.loadLibrary();
+    }
+
+    private RocksDB rocksDB;
+
+    private void openDb(String dbPath) throws RocksDBException {
+        final Options options = new Options();
+        options.setCreateIfMissing(true);
+        rocksDB = RocksDB.open(options, dbPath);
+        listColumnFamilies(options, dbPath);
+    }
+
+    private void listColumnFamilies(Options options, String path) throws RocksDBException {
+        System.out.println("开始打印ColumnFamilies");
+        List<byte[]> cfs = RocksDB.listColumnFamilies(options, path);
+        for (byte[] cf : cfs) {
+            System.out.println(new String(cf));
+        }
+        System.out.println("结束打印ColumnFamilies");
+    }
+
+    private void putData(String key, String value) throws RocksDBException {
+        rocksDB.put(key.getBytes(), value.getBytes());
+    }
+
+    private void removeData(String key) throws RocksDBException {
+        rocksDB.remove(key.getBytes());
+    }
+
+    private void printData(String key) throws RocksDBException {
+        byte[] getValue = rocksDB.get(key.getBytes());
+        System.out.println(new String(getValue));
+    }
+
+    private void printData(String[] allKeys) throws RocksDBException {
+        List<byte[]> keys = new ArrayList<>();
+        for (String k : allKeys) {
+            keys.add(k.getBytes());
+        }
+        Map<byte[], byte[]> valueMap = rocksDB.multiGet(keys);
+        for (Map.Entry<byte[], byte[]> entry : valueMap.entrySet()) {
+            System.out.println(new String(entry.getKey() + ":" + new String(entry.getValue())));
+        }
+    }
+
+    private void printAllData() {
+        System.out.println("开始打印所有数据");
+        RocksIterator it = rocksDB.newIterator();
+        for (it.seekToFirst(); it.isValid(); it.next()) {
+            System.out.println("Key:" + new String(it.key()) + ", Value:" + new String(it.value()));
+        }
+        System.out.println("结束打印所有数据");
+    }
+
+    public void testDefaultColumnFamily() throws RocksDBException {
+        final String dbPath = "/tmp/data02/";
+        openDb(dbPath);
+        putData("Hello", "World");
+        printData("Hello");
+        putData("SecondKey", "SecondValue");
+        printData(new String[]{"Hello", "SecondKey"});
+
+        printAllData();
+        removeData("Hello");
+        printAllData();
+    }
+
+    public static void main(String[] args) throws RocksDBException {
+        new useDefaultColumnFamily().testDefaultColumnFamily();
+    }
+}
