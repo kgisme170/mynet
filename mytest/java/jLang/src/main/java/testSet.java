6a12df9e8468ad36752b83358ad285597a9d46c2
liming.glm <liming.glm@alibaba-inc.com>
2018-11-10 11:26:08 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: Ie78fe28aad073e4844cb1895772499941f8b0c2c

---
+++ mytest/java/jLang/src/main/java/testSet.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class testSet {
+    static class My implements Comparable {
+        private int mI;
+
+        public My(int i) {
+            mI = i;
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            return mI - ((My) o).mI;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeSet tree = new TreeSet();
+        tree.add(new My(4));
+        tree.add(new My(2));
+        tree.add(new My(5));
+
+        Iterator it = tree.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next() + " ");
+        }
+
+        HashSet<My> m = new HashSet<>();
+        m.add(new My(6));
+        m.add(new My(2));
+        LinkedHashSet<My> s = new LinkedHashSet<>();
+        s.add(new My(7));
+    }
+}
