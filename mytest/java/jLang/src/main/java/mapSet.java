137841a14eab9cea8236d90deb431ffec58a7185
liming.glm <liming.glm@alibaba-inc.com>
2018-11-16 11:00:59 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I433bc080bf84d965d98bc4d20ba33bd88c13884d

---
+++ mytest/java/jLang/src/main/java/mapSet.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class mapSet {
+    public static void main(String[] args) {
+        Map<String, Set<String>> map = new HashMap<>();
+        Set<String> set = new HashSet<>();
+        set.add("user1");
+        set.add("user2");
+        map.put("key1", set);
+
+        Map<String, Set<String>> map2 = new HashMap<>();
+        Map<String, Set<String>> map3 = new HashMap<>();
+        map2.putAll(map);// 只拷贝了reference
+        map3.putAll(map);
+        map3.replaceAll((k, v) -> new HashSet<>(v));
+        Map<String, Set<String>> map4 = map.entrySet()
+                .stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, e -> new HashSet<>(e.getValue())));
+        map.get("key1").add("user3");
+        System.out.println(map2.get("key1").size()); // "map2"受到了影响
+        System.out.println(map3.get("key1").size()); // "map3"没有受到影响
+        System.out.println(map4.get("key1").size()); // "map4"没有受到影响
+    }
+}
