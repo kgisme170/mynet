978891a0d9f96f28b876e8716d57325413c2f6cc
liming.glm <liming.glm@alibaba-inc.com>
2018-12-01 01:08:24 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: Ie0fe3ac442697ed512beee7d83a4a50f7ae66e6c

--- mytest/java/jLang/src/main/java/useTimerTask.java
+++ mytest/java/jLang/src/main/java/useTimerTask.java
@@ -2,7 +2,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class useTimerTask {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Timer timer = new Timer(true);
         TimerTask task1 = new TimerTask() {
             @Override
@@ -11,24 +11,24 @@ public class useTimerTask {
                     System.out.println("begin");
                     Thread.sleep(500);
                     System.out.println("end");
-                }catch(InterruptedException e){
+                } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         };
         TimerTask task2 = new TimerTask() {
             @Override
-            public void run(){
+            public void run() {
                 int i = 0;
                 int j = 2;
-                System.out.println(j/i);
+                System.out.println(j / i);
             }
         };
         timer.schedule(task1, 1000, 3000);
         timer.schedule(task2, 1000, 3000);
-        try{
+        try {
             Thread.sleep(10000);
-        }catch(Exception ex){
+        } catch (Exception ex) {
             timer.cancel(); // 只要有一个task失败了，所有task都中止
         }
     }
