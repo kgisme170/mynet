5e0ecf93974f86717863d58faed11ae97abe5b33
liming.glm <liming.glm@alibaba-inc.com>
2018-12-01 14:19:54 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: Ic64ff686899ca598d4f3d0d6f9e4a5d6b830b644

---
+++ mytest/java/jLang/src/main/java/useEnum.java
@@ -0,0 +1,41 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class useEnum {
+    enum Status {
+        SCUUESS("1", "成功"), FAILED("2", "失败");
+
+        public String value;
+        public String desc;
+
+        Status(String value, String desc) {
+            this.value = value;
+            this.desc = desc;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+
+        public String getDesc() {
+            return desc;
+        }
+
+        public void setDesc(String desc) {
+            this.desc = desc;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Status.SCUUESS.getValue());
+        System.out.println(Status.SCUUESS.getDesc());
+        System.out.println(Status.FAILED.getValue());
+        System.out.println(Status.FAILED.getDesc());
+        Map<Status, String> m = new HashMap<Status, String>();
+        System.out.println(m);
+    }
+}
