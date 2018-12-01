115a555a5a84d08ab2270992c120564d67ee7728
liming.glm <liming.glm@alibaba-inc.com>
2018-12-01 16:00:59 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I5a194c7c7ad9d91f93a56092886af4e5e3c0327d

--- mytest/java/jLang/src/main/java/timeAndCurrency.java
+++ mytest/java/jLang/src/main/java/timeAndCurrency.java
@@ -9,40 +9,43 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-public class ch6ch7 {
-    public static void main(String[] args){
+public class timeAndCurrency {
+    public static void main(String[] args) {
         Instant start = Instant.now();
         Instant end = Instant.now();
 
         Duration d = Duration.between(start, end);
         long mils = d.toMillis();
-
+        System.out.println(mils);
 
         d.multipliedBy(10).minus(Duration.between(Instant.now(), Instant.now())).isNegative();
         LocalDate today = LocalDate.now();
         LocalDate birth = LocalDate.of(1999, 3, 3).plusDays(3);
-        //.with(TemporalAdjuster.nextOrSame(DayOfWeek.TUESDAY));
-        //.until(2, ChronoUnit.DAYS);
-        ZonedDateTime zdt = ZonedDateTime.of(1999,9,9,9,9,9,0,ZoneId.of("America/New_York"));
+        System.out.println(today);
+        System.out.println(birth);
+        ZonedDateTime zdt = ZonedDateTime.of(1999, 9, 9, 9, 9, 9, 0, ZoneId.of("America/New_York"));
         ZonedDateTime zdt2 = ZonedDateTime.of(
-                LocalDate.of(1999,9,9),
-                LocalTime.of(9,9,9),
+                LocalDate.of(1999, 9, 9),
+                LocalTime.of(9, 9, 9),
                 ZoneId.of("America/New_York")).plus(Period.ofDays(7));//day-light saving works. Don't use Duration.ofDays
-
+        System.out.println(zdt2);
         DateTimeFormatter fmt = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG);
         String sf = fmt.format(zdt);
-
-        for(DayOfWeek w : DayOfWeek.values()){
+        System.out.println(sf);
+        for (DayOfWeek w : DayOfWeek.values()) {
             System.out.println(w.getDisplayName(TextStyle.SHORT, Locale.ENGLISH) + " ");
         }
         fmt = DateTimeFormatter.ofPattern("E yyyy-MM-dd HH:mm");
+        sf = fmt.format(zdt);
+        System.out.println(sf);
 
         LocalDate church = LocalDate.parse("1999-09-09");
-
+        System.out.println(church);
         LocalDateTime zdt3 = LocalDateTime.parse("1999-09-09 09:09:09.999",
                 DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"));
-
+        System.out.println(zdt3);
         Locale[] supportedLocals = NumberFormat.getAvailableLocales();
+        System.out.println(supportedLocals);
         Locale loc = Locale.GERMAN;
         NumberFormat curFmt = NumberFormat.getCurrencyInstance(loc);
         System.out.println(curFmt.format(1234567.8));
@@ -56,6 +59,5 @@ public class ch6ch7 {
         System.out.println(words);
         String normalized = Normalizer.normalize("abc", Normalizer.Form.NFD);
         System.out.println(normalized);
-
     }
 }
