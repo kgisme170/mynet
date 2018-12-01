bf3fae978cff321dee8f624c18d4ed47a8658084
liming.glm <liming.glm@alibaba-inc.com>
2018-12-01 18:23:33 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I680bcbb964b5b236974f82e4c27071d1da7704dc

--- mytest/java/jLang/src/main/java/useScript.java
+++ mytest/java/jLang/src/main/java/useScript.java
@@ -1,26 +1,12 @@
-import javax.annotation.processing.Processor;
 import javax.script.ScriptEngine;
 
 import javax.script.*;
-import javax.tools.JavaCompiler;
-import com.sun.tools.javac.file.*;
-import javax.tools.StandardJavaFileManager;
-import javax.tools.ToolProvider;
 import java.io.*;
 import java.util.*;
-import java.util.concurrent.Callable;
 
 import static java.lang.System.*;
 public class useScript {
-    static String fn = System.getProperty("user.dir") + "/java/jLang/src/main/java/useReflect.java";
-    static JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-
     public static void main(String args[]) {
-        f1();
-        f2();
-    }
-
-    public static void f1() {
         ScriptEngineManager manager = new ScriptEngineManager();
 
         List<ScriptEngineFactory> factories = manager.getEngineFactories();
@@ -77,50 +63,6 @@ public class useScript {
             } catch (ScriptException e) {
                 e.printStackTrace();
             }
-        }
-    }
-
-    public static void f2() {
-        System.out.println("f2");
-        try {
-            OutputStream outStream = new FileOutputStream("core3.class");
-            OutputStream errStream = new BufferedOutputStream(System.err);
-            int result = compiler.run(null, outStream, errStream, "-sourcepath", "src", fn);
-            System.out.println(result);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
-        String[] as = new String[]{fn};
-        List<String> list = Arrays.asList(as);
-        Iterable fileObjects = fileManager.getJavaFileObjectsFromStrings(list);
-        System.out.println("2");
-        for (Iterator it = fileObjects.iterator(); it.hasNext(); it.next()) {
-            System.out.println("for loop");
-        }
-        Callable<Boolean> task = new JavaCompiler.CompilationTask() {
-            @Override
-            public void setProcessors(Iterable<? extends Processor> processors) {
-
-            }
-
-            @Override
-            public void setLocale(Locale locale) {
-
-            }
-
-            @Override
-            public Boolean call() {
-                return true;
-            }
-        };
-        try {
-            if (!task.call()) {
-                System.out.println("call not ok");
-            }
-            System.out.println("call ok");
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 }
