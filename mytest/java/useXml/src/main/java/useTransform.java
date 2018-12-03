ae5ce0164fc3e8a63f0772169e5ca14ebc1b3ede
liming.glm <liming.glm@alibaba-inc.com>
2018-12-03 22:15:18 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I898ca3c8788ef8f4d521db0d21f1dfad209d5434

---
+++ mytest/java/useXml/src/main/java/useTransform.java
@@ -0,0 +1,25 @@
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import java.net.URI;
+
+public class useTransform {
+    static String currentPath = Thread.currentThread().getContextClassLoader().getResource("").toString();
+
+    public static void main(String args[]) throws Exception {
+        String path = currentPath + "phone.xml";
+        String xml = new URI(path).getPath();
+
+        String pathXsl = currentPath + "phone.xsl";
+        String xsl = new URI(pathXsl).getPath();
+
+        StreamSource source = new StreamSource(xml);
+        StreamSource styleSource = new StreamSource(xsl);
+        TransformerFactory factory = TransformerFactory.newInstance();
+        Transformer transformer = factory.newTransformer(styleSource);
+
+        StreamResult result = new StreamResult(System.out);
+        transformer.transform(source, result);
+    }
+}
