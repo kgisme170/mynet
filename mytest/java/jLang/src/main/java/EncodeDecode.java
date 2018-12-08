7498dc34b12b0ef76a1e9371f545fde6ecac1e32
liming.glm <liming.glm@alibaba-inc.com>
2018-12-08 15:29:00 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I64cae1ecf6c0b39dbbb0e6e78c6fc8e33359c062

---
+++ mytest/java/jLang/src/main/java/EncodeDecode.java
@@ -0,0 +1,36 @@
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.*;
+
+public class EncodeDecode {
+    public static void main(String[] args) {
+        Charset charset = StandardCharsets.UTF_8;
+        //注意: StandardCharsets是jdk1.7添加的
+
+        //从字符集中创建相应的编码和解码器
+        CharsetEncoder encoder = charset.newEncoder();
+        CharsetDecoder decoder = charset.newDecoder();
+
+        //构造一个buffer
+        CharBuffer charBuffer = CharBuffer.allocate(64);
+        charBuffer.put('你');
+        charBuffer.put('好');
+        charBuffer.put('!');
+        charBuffer.flip();
+
+        try {
+            //将字符序列转换成字节序列
+            ByteBuffer bb = encoder.encode(charBuffer);
+            for (; bb.hasRemaining(); ) {
+                System.out.print(bb.get() + " ");
+            }
+
+            //将字节序列转换成字符序列
+            bb.flip();
+            CharBuffer cb = decoder.decode(bb);
+            System.out.println("\n" + cb);
+        } catch (CharacterCodingException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- mytest/java/jLang/src/main/java/basicTypes.java
+++ mytest/java/jLang/src/main/java/basicTypes.java
@@ -52,34 +52,8 @@ public class basicTypes {
             e.printStackTrace();
         }
         for (String sp : Pattern.compile("\\PL+").splitAsStream("ab cd ef:gh,xy").collect(Collectors.toSet())) {
-            System.out.println(sp); // PL+代表非字母都当成是分隔符, \\PN+非数字 \\PZ+非分隔符, \\PS+非符号
-        }
-        Charset charset = StandardCharsets.UTF_8; //注意: StandardCharsets是jdk1.7添加的
-
-        //从字符集中创建相应的编码和解码器
-        CharsetEncoder encoder = charset.newEncoder();
-        CharsetDecoder decoder = charset.newDecoder();
-
-        //构造一个buffer
-        CharBuffer charBuffer = CharBuffer.allocate(64);
-        charBuffer.put('你');
-        charBuffer.put('好');
-        charBuffer.put('!');
-        charBuffer.flip();
-
-        try {
-            //将字符序列转换成字节序列
-            ByteBuffer bb = encoder.encode(charBuffer);
-            for (; bb.hasRemaining(); ) {
-                System.out.print(bb.get() + " ");
-            }
-
-            //将字节序列转换成字符序列
-            bb.flip();
-            CharBuffer cb = decoder.decode(bb);
-            System.out.println("\n" + cb);
-        } catch (CharacterCodingException e) {
-            e.printStackTrace();
+            System.out.println(sp);
+            // PL+代表非字母都当成是分隔符, \\PN+非数字 \\PZ+非分隔符, \\PS+非符号
         }
     }
 }
