3fd64eafc03f8aac12a1e5de343654406f4a5056
liming.glm <liming.glm@alibaba-inc.com>
2018-12-09 09:13:32 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I4445af2bef19d594272d27b0dfc4f33eab60cc31

--- mytest/java/concurrent/src/main/java/UseCountDownLatch.java
+++ mytest/java/concurrent/src/main/java/UseCountDownLatch.java
@@ -1,24 +1,41 @@
-import java.util.concurrent.CountDownLatch;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import java.util.concurrent.*;
+
 /**
  * @author liming.glm
  */
 public class UseCountDownLatch {
     public static void main(String [] args) {
-        final CountDownLatch latch = new CountDownLatch(2);
-        new Thread() {
+        final CountDownLatch latch = new CountDownLatch(1);
+        ThreadFactory namedThreadFactory = new ThreadFactoryBuilder().setNameFormat("UseCountDownLatch").build();
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(
+                10,
+                10,
+                100,
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(1),
+                namedThreadFactory);
+
+        executor.execute(new Runnable() {
             @Override
             public void run() {
+                System.out.println("run");
                 latch.countDown();
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("end");
             }
-        }.start();
-        new Thread() {
-            @Override
-            public void run() {
-                latch.countDown();
-            }
-        }.start();
+        });
         try {
             latch.await();
+            System.out.println("Before");
+            executor.shutdown();
+            System.out.println("After");
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
