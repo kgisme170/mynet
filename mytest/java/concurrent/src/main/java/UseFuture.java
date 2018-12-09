382bb68aa5d7fdaafe2523d83aaafe021a8971cc
liming.glm <liming.glm@alibaba-inc.com>
2018-12-09 09:33:32 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I5f0b6d12dcd712b0e9d5d3f075d027d11ccde922

--- mytest/java/concurrent/src/main/java/UseFuture.java
+++ mytest/java/concurrent/src/main/java/UseFuture.java
@@ -1,3 +1,5 @@
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 /**
@@ -10,8 +12,9 @@ public class UseFuture {
         @Override
         public Integer call() {
             try {
-                System.out.println("开始");
+                System.out.println("开始UseFuture");
                 Thread.sleep(2000);
+                System.out.println("结束UseFuture");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -20,13 +23,24 @@ public class UseFuture {
     }
 
     public static void main(String[] args) {
+        ThreadFactory namedThreadFactory = new ThreadFactoryBuilder().setNameFormat("UseFuture").build();
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(
+                10,
+                10,
+                100,
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(1),
+                namedThreadFactory);
+
         // FutureTask继承了Runnable和Future接口
         FutureTask<Integer> futureTask1 = new FutureTask<>(new MyThread());
         FutureTask<Integer> futureTask2 = new FutureTask<>(new MyThread());
-        new Thread(futureTask1, "有返回值的thread").start();
+        executor.execute(futureTask1);
         //主线程会等待所有start的子线程，除非子线程setDaemon
 
-        new Thread(futureTask2, "有返回值的thread").start();
+        executor.execute(futureTask2);
+        executor.shutdown();
+
         //主线程会等待所有start的子线程，除非子线程setDaemon
         try {
             System.out.println(futureTask1.get());
@@ -48,5 +62,6 @@ public class UseFuture {
         }catch(Exception e){
             e.printStackTrace();
         }
+        pool.shutdown();
     }
 }
