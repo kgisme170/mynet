f46d2be9d50ef2239bca5ca27f5b5682ca435a2f
kgisme170 <kgisme170@163.com>
2018-12-12 00:48:09 +0800

    Default Changelist

---
+++ mytest/java/security/src/main/java/TestPolicy.java
@@ -0,0 +1,15 @@
+import java.io.FileWriter;
+import java.io.IOException;
+// java -classpath . -Djava.security.manager -Djava.security.policy=../../src/myPolicy.txt TestPolicy
+public class TestPolicy {
+    public static void main(String[] args) {
+        FileWriter writer;
+        try {
+            writer = new FileWriter("testPolicy.txt");
+            writer.write("hello1");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
