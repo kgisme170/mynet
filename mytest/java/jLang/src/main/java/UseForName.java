cee730e40dac02156ff8eb10ef104718950dd141
liming.glm <liming.glm@alibaba-inc.com>
2018-12-12 10:56:49 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I0f46037e236a7a7d2957b241337d92d9d26e2c18

--- mytest/java/jLang/src/main/java/UseForName.java
+++ mytest/java/jLang/src/main/java/UseForName.java
@@ -12,13 +12,13 @@ class C1{
 public class UseForName {
     public static void main(String[] args) {
         try {
-            Class c = Class.forName("C");
+            Class c = Class.forName("C1");
             //执行static
             Constructor ctor = c.getConstructor();
             C1 obj = (C1) ctor.newInstance();
 
             ClassLoader l = ClassLoader.getSystemClassLoader();
-            Class c2 = l.loadClass("C");
+            Class c2 = l.loadClass("C1");
             Constructor ctor2 = c2.getConstructor();
             C1 obj2 = (C1) ctor2.newInstance();
             //第一次创建对象，才执行static
