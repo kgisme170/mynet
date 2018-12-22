4de3fc74c261626ff0a4893630dadb2c19f4519f
kgisme170 <kgisme170@163.com>
2018-12-22 23:26:09 +0800

    Java

---
+++ mytest/java/network/src/main/java/SocketScanner.java
@@ -0,0 +1,23 @@
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * @author liming.gong
+ */
+public class SocketScanner {
+    public static void main(String[] args) {
+        String host = "time-a.nist.gov";
+        try (Socket s = new Socket(host, 13);
+             Scanner in = new Scanner(s.getInputStream(), "UTF-8")) {
+            s.setSoTimeout(1000);
+            while (in.hasNextLine()) {
+                System.out.println(in.nextLine());
+            }
+            byte[] addressBytes = InetAddress.getByName(host).getAddress();
+            System.out.println(addressBytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
