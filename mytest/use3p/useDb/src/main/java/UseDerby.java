7fedf99498be5460180aded41f20c890aa9b66bb
liming.glm <liming.glm@alibaba-inc.com>
2019-01-04 17:17:33 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: Icc1feae46f1f1d45e596b9a36009a87eb9c96979

--- mytest/use3p/useDb/src/main/java/UseDerby.java
+++ mytest/use3p/useDb/src/main/java/UseDerby.java
@@ -1,39 +1,79 @@
+import com.sun.rowset.CachedRowSetImpl;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
+
 /**
  * @author liming.gong
  */
-public class UseDerby {
-    public static void main(String[] args) {
+
+class ConnectDerby {
+    private ResultSet rs = null;
+    private Statement s = null;
+    private Properties props = null;
+    private Connection conn = null;
+
+    public ConnectDerby() {
+    }
+
+    public void iterateRs() {
+        System.out.println("----------------");
         try {
-            Class.forName("org.apache.derby.jdbc.EmbeddedDriver").newInstance();
-            System.out.println("加载嵌入式驱动");
-            Properties props = new Properties();
-            props.put("user", "用户1");
-            props.put("password", "用户11");
-
-            Connection conn = DriverManager.getConnection("jdbc:derby:hello;create=true", props);
-            System.out.println("创建数据库hello");
-            conn.setAutoCommit(false);
-
-            Statement s = conn.createStatement();
-            s.execute("create table t(name varchar(40), age int)");
-            System.out.println("创建表 t");
-            s.execute("insert into t values('张三', 26)");
-            s.execute("insert into t values ('李四', 32)");
-            ResultSet rs = s.executeQuery(
-                    "SELECT name, age FROM t ORDER BY age");
-            System.out.println("----------------");
             while (rs.next()) {
                 StringBuilder builder = new StringBuilder(rs.getString(1));
                 builder.append("\t");
                 builder.append(rs.getInt(2));
                 System.out.println(builder.toString());
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("----------------");
+    }
+
+    public void iterateCachedRs() {
+        try {
+            CachedRowSetImpl rowSet = new CachedRowSetImpl();
+            rowSet.populate(rs);
+            System.out.println("----------------");
+            while (rowSet.next()) {
+                System.out.println("id:" + rowSet.getString(1));
+            }
+            System.out.println("----------------");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void init() {
+        try {
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver").newInstance();
+            System.out.println("加载嵌入式驱动");
+            props = new Properties();
+            props.put("user", "用户1");
+            props.put("password", "用户11");
+
+            conn = DriverManager.getConnection("jdbc:derby:hello;create=true", props);
+            System.out.println("创建数据库hello");
+            conn.setAutoCommit(false);
+
+            s = conn.createStatement();
+            s.execute("create table t(name varchar(40), age int)");
+            System.out.println("创建表 t");
+            s.execute("insert into t values('张三', 26)");
+            s.execute("insert into t values ('李四', 32)");
+            rs = s.executeQuery("SELECT name, age FROM t ORDER BY age");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void destroy() {
+        try {
             s.execute("drop table t");
             System.out.println("删除表 t");
 
@@ -43,7 +83,6 @@ public class UseDerby {
             conn.commit();
             conn.close();
             System.out.println("提交 transaction 并关闭连接");
-
             try {
                 DriverManager.getConnection("jdbc:derby:;shutdown=true");
             } catch (SQLException se) {
@@ -52,5 +91,14 @@ public class UseDerby {
         } catch (Throwable e) {
             e.printStackTrace();
         }
+    }
+}
+
+public class UseDerby {
+    public static void main(String[] args) {
+        ConnectDerby connectDerby = new ConnectDerby();
+        connectDerby.init();
+        connectDerby.iterateCachedRs();
+        connectDerby.destroy();
     }
 }
