6b9a904c6a599152eeee5af1ee499b33e812b567
liming.glm <liming.glm@alibaba-inc.com>
2019-01-05 23:11:44 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: Ic26ca101496b13746f4daca2bdf9b13acb731542

---
+++ mytest/java/useJunit/src/main/java/UseLog.java
@@ -0,0 +1,30 @@
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author liming.gong
+ */
+public class UseLog {
+    public static final Logger logger = Logger.getLogger("my logger name");
+    void f(T ... s) {
+        System.out.println(s.length);
+    }
+    void g(String [] s){
+        System.out.println(s.length);
+    }
+    public static void main(String[] args) throws Exception {
+        //Logger.getGlobal().log(Level.FINE, "msg");
+        //Logger.getGlobal().info("msg1");
+        //Logger.getGlobal().setLevel(Level.OFF);
+        //Logger.getGlobal().warning("msg2");
+        //Logger.getGlobal().setLevel(Level.INFO);
+        //Logger.getGlobal().info("msg3");
+        logger.addHandler(new FileHandler("%h/myapp.log", 0, 3));
+        logger.info("my4");
+        //logger的配置文件
+        //java.util.logging.
+
+        new UseLog().f<String>("abc", "xyz");
+    }
+}
