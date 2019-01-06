3050d4206888f1ea3097896249987b40b217ddc5
liming.glm <liming.glm@alibaba-inc.com>
2019-01-06 19:37:37 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I0f362e10e0f239356df4edb01e64290f5e7e8660

---
+++ mytest/use3p/useDb/src/main/java/DefaultMethod.java
@@ -0,0 +1,28 @@
+interface IMy {
+    default void f1() {}
+    default void f2() {}
+    default void f3() {
+        System.out.println("f3");
+        sf();
+    }
+    static void sf() {
+        System.out.println("f1, f2");
+    }
+}
+
+/**
+ * @author liming.gong
+ */
+class DefaultMethod implements IMy {
+    @Override
+    public void f3() {
+        f1();
+        f2();
+        IMy.super.f3();
+        System.out.println("override f3");
+    }
+
+    public static void main(String[] args) {
+        new DefaultMethod().f3();
+    }
+}
