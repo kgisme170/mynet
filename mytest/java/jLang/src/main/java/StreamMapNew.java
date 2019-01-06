4480af03114713970763a806107c5986b3b5db07
liming.glm <liming.glm@alibaba-inc.com>
2019-01-06 22:33:01 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I09af38578a1ab3ce8c2968a6dbdbd8f0c248ab39

--- mytest/java/jLang/src/main/java/StreamMapNew.java
+++ mytest/java/jLang/src/main/java/StreamMapNew.java
@@ -1,9 +1,9 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-class Person {
+class MyPerson {
     private String name;
-    public Person(String name) {
+    public MyPerson(String name) {
         this.name = name;
     }
     public String getName() { return name; }
@@ -15,7 +15,7 @@ public class StreamMapNew {
     public static void main(String[] args) {
         String[] array = new String[]{"xyz1", "abc", "1234"};
         List<String> stringList = Arrays.asList(array);
-        List<Person> personList = stringList.stream().map(Person::new).collect(Collectors.toList());
+        List<MyPerson> personList = stringList.stream().map(MyPerson::new).collect(Collectors.toList());
         personList.forEach(x -> System.out.println(x.getName()));
     }
 }
