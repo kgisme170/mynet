d23af95b3b945e07ce94ba55acaa8a3934e16783
liming.glm <liming.glm@alibaba-inc.com>
2019-01-06 23:04:23 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I913a5e165e856a749a5f1f8e7afcb89e51d02613

---
+++ mytest/java/useJunit/src/test/java/TestReentrantLock.java
@@ -0,0 +1,82 @@
+import org.junit.*;
+
+import java.util.Arrays;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class TestReentrantLock {
+    @Test
+    public void TestLockWithException() {
+        Lock bankLock = new ReentrantLock();
+        try {
+            bankLock.lock();
+            Thread main = Thread.currentThread();
+            Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(500);
+                        main.interrupt();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            Thread t = new Thread(runnable);
+            t.start();
+            Thread.sleep(1000);
+            t.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("finally unlock");
+            bankLock.unlock();
+        }
+    }
+
+    class Bank {
+        private Lock bankLock = new ReentrantLock();
+        private Condition sufficientFunds = bankLock.newCondition();
+        private final double[] accounts;
+
+        public Bank(int n, double initialBalance) {
+            accounts = new double[n];
+            Arrays.fill(accounts, initialBalance);
+        }
+
+        public void transfer(int from, int to, double amount) throws InterruptedException {
+            bankLock.lock();
+            try {
+                while(accounts[from] < amount) {
+                    sufficientFunds.await();
+                }
+                System.out.println(Thread.currentThread());
+                accounts[from] -= amount;
+                accounts[to] += amount;
+                sufficientFunds.signalAll();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                bankLock.unlock();
+            }
+        }
+
+        public double getTotal() {
+            bankLock.lock();
+            try {
+                double sum = 0;
+                for (double a: accounts) {
+                    sum += a;
+                }
+                return sum;
+            } finally {
+                bankLock.unlock();
+            }
+        }
+    }
+
+    @Test
+    public void TestAccountTransfer() {
+    }
+}
