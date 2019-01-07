90f88d168fef69ab2df7fafc98c76a8040825f12
liming.glm <liming.glm@alibaba-inc.com>
2019-01-07 22:34:51 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I2e58d68ba423dcbcbb19f9b9d61c6ef8fa5ab531

---
+++ mytest/cpp/cpp98/templateTemplate.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+template<class T>
+class My{
+public:
+    void f(){}
+    template<class G>
+    void g()const {}
+};
+template<class T>
+void h(const My<T>& m){
+    m.template g<int>();
+}
+int main(){
+    My<char> m;
+    m.g<int>();
+    h(m);
+    return 0;
+}
