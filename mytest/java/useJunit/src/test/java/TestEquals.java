a0445894373150cb087e62c869c18ce951e0847b
liming.glm <liming.glm@alibaba-inc.com>
2019-01-07 21:28:06 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: Ia8b69c9c808fcb90a426f67cc55a37d31c8a51b2

--- mytest/java/useJunit/src/test/java/TestEquals.java
+++ mytest/java/useJunit/src/test/java/TestEquals.java
@@ -1,6 +1,9 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class TestEquals {
     class My {
         int i = 1;
@@ -32,5 +35,76 @@ public class TestEquals {
         You m1 = new You();
         You m2 = new You();
         Assert.assertNotEquals(m1, m2);
+    }
+
+    @Test
+    public void TestOverrideEqual() {
+        My m1 = new My();
+        My m2 = new My();
+        Set<My> mySet = new HashSet<>();
+        mySet.add(m1);
+        mySet.add(m2);
+        Assert.assertEquals(2, mySet.size());
+    }
+
+    class MyHash extends My {
+        @Override
+        public int hashCode() {
+            return i;
+        }
+    }
+
+    @Test
+    public void TestOverrideAll() {
+        MyHash m1 = new MyHash();
+        MyHash m2 = new MyHash();
+        Set<MyHash> mySet = new HashSet<>();
+        mySet.add(m1);
+        mySet.add(m2);
+        Assert.assertEquals(1, mySet.size());
+    }
+
+    class He {
+        int i = 1;
+
+        @Override
+        public int hashCode() {
+            return i;
+        }
+    }
+
+    @Test
+    public void TestOverrideHashCode() {
+        He he = new He();
+        He he2 = new He();
+        Set<He> heSet = new HashSet<>();
+        heSet.add(he);
+        Assert.assertFalse(heSet.contains(he2));
+    }
+
+    class HeEqual extends He {
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null) {
+                return false;
+            }
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+            final HeEqual heEqual = (HeEqual) obj;
+            return heEqual.i == i;
+        }
+    }
+
+    @Test
+    public void TestOverrideAll2() {
+        HeEqual he = new HeEqual();
+        HeEqual he2 = new HeEqual();
+        Set<HeEqual> heSet = new HashSet<>();
+        heSet.add(he);
+        Assert.assertTrue(heSet.contains(he2));
     }
 }
