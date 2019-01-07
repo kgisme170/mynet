eba0bc5a2efa468c30855fcaec012bbb12841aa3
liming.glm <liming.glm@alibaba-inc.com>
2019-01-07 17:59:10 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I9fff9bee8b2c88af7fb96d8be96dcd70143d6d92

--- mytest/java/useJunit/src/main/java/UseLog.java
+++ mytest/java/useJunit/src/main/java/UseLog.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -6,25 +7,30 @@ import java.util.logging.Logger;
  * @author liming.gong
  */
 public class UseLog {
-    public static final Logger logger = Logger.getLogger("my logger name");
-    // f(T ... s) {
-    //    System.out.println(s.length);
-    //}
-    void g(String [] s){
-        System.out.println(s.length);
-    }
-    public static void main(String[] args) throws Exception {
-        //Logger.getGlobal().log(Level.FINE, "msg");
-        //Logger.getGlobal().info("msg1");
-        //Logger.getGlobal().setLevel(Level.OFF);
-        //Logger.getGlobal().warning("msg2");
-        //Logger.getGlobal().setLevel(Level.INFO);
-        //Logger.getGlobal().info("msg3");
+    public static final Logger logger = Logger.getLogger("UseLog");
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("arg count: " + args.length);
+        logger.info("hello from info");
+        logger.warning("hello from warning");
+
+        Logger.getGlobal().log(Level.FINE, "msg");
+        Logger.getGlobal().info("msg3");
         logger.addHandler(new FileHandler("%h/myapp.log", 0, 3));
         logger.info("my4");
-        //logger的配置文件
-        //java.util.logging.
-
-        //new UseLog().f<String>("abc", "xyz");
+        Logger.getGlobal().setLevel(Level.INFO);
     }
 }
+
+/**
+ * 要使得log的配置文件有效，必须采用jvm选项，-D需要出现在class name之前。
+ * 出现在class name后面的是程序运行时参数args的部分
+ * 例如以下用法：
+ *
+ *
+ $java -Djava.util.logging.config.file=myConfig.txt UseLog
+ arg count: 0
+ 一月 07, 2019 5:50:30 下午 UseLog main
+ 警告: hello from warning
+ *
+ */
