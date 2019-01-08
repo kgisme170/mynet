4f991f8481579c2e9719c5466d270beb025779e2
liming.glm <liming.glm@alibaba-inc.com>
2019-01-08 16:39:05 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: Iba8606ea5a98da64d54063fd214514ba94fd40ea

--- mytest/java/useJunit/src/test/java/UseHalfGenericType.java
+++ mytest/java/useJunit/src/test/java/UseHalfGenericType.java
@@ -1,11 +1,21 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 public class UseHalfGenericType {
+    static <T> void reAdd(Map<String, T> map) {
+        T value = map.get("123");
+        map.put("456", value);
+    }
+
     public static void main(String[] args) {
-        Map<String, ?> map = new HashMap<>(3);
-        //map.put("abc", Optional.of(5));
-        //map.put("kk", "xyz");
+        Map<String, ?> m = new HashMap<>(3);
+        reAdd(m);
+        Map<String, ? super Object> map = new HashMap<>(3);//OK
+        map.put("abc", Optional.of(5));
+        map.put("kk", "xyz");
+        map.forEach((k, v) -> System.out.println(k + "\t" + v));
+
+        List<?> l = new ArrayList<String>();
+        List<? extends Exception> l2 = new ArrayList<RuntimeException>();
+        List<? super Exception> l3 = new ArrayList<Object>();
     }
 }
