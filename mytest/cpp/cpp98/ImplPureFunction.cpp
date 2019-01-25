a5681b52c18f59a6d67a2644d8ed2a6a4ff1e931
liming.glm <liming.glm@alibaba-inc.com>
2019-01-25 21:23:15 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I63c59ea44d61480a207e8183437ebba2f5a587a8

---
+++ mytest/cpp/cpp98/ImplPureFunction.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+struct S {
+    virtual void f() = 0;
+    virtual ~S() {}
+};
+void S::f() { cout<<"Impl pure function\n"; }
+struct Impl : S {
+    void f() { S::f(); }
+};
+
+int main(){
+    S* s = new Impl();
+    s->f();
+    delete s;
+    return 0;
+}
