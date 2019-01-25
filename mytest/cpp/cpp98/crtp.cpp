eb3fc7f9d3e03d64bf4f99a7883b5c5a2c9bff11
liming.glm <liming.glm@alibaba-inc.com>
2019-01-25 19:35:29 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: Id7ad615ccfbf6e4ed6c22747c55f6ea9e2e9bce0

---
+++ mytest/cpp/cpp98/crtp.cpp
@@ -0,0 +1,23 @@
+#include <cstring>
+#include <iostream>
+using namespace std;
+template <class T>
+struct Base
+{
+    void f()
+    {
+        // ...
+        static_cast<T*>(this)->cmp();
+        // ...
+    }
+};
+
+struct Derived : Base<Derived>
+{
+    void cmp(){cout<<"cmp function\n";}
+};
+int main(){
+    Derived d;
+    d.f();
+    return 0;
+}
--- mytest/java/useJunit/src/test/java/UseCompletableFuture.java
+++ mytest/java/useJunit/src/test/java/UseCompletableFuture.java
@@ -1,4 +1,7 @@
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
  * @author liming.gong
@@ -7,5 +10,17 @@ public class UseCompletableFuture {
     public static void main(String[] args) throws Exception {
         CompletableFuture<String> contents = new CompletableFuture<>();
         // contents.thenApply();
+
+        ExecutorService service = Executors.newSingleThreadExecutor();
+        Future<Integer> future = service.submit(()->{
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return 2;
+        });
+        System.out.println(future.get());
+        service.shutdown();
     }
 }
