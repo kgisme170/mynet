cc90114281bd85d24d5caeeeda9d1315d3d67b20
liming.glm <liming.glm@alibaba-inc.com>
2019-01-31 16:03:01 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I5c5545d5799db73cab337f6d1d836c3996a8feaa

--- mytest/cpp/genSconscript.py
+++ mytest/cpp/genSconscript.py
@@ -15,24 +15,32 @@ def find_source_files(dir_name):
                     file_list.append(file_path)
     return file_list
 
-for dir in ["cpp11", "cpp14", "cpp17"]:
-    absDir = os.path.join(cwd, dir)
-    version = dir.replace("cpp", "c++")
-    print(absDir)
-    os.chdir(absDir)
+def generate_sconscript(dir_list, libs_flag = ""):
+    for dir in dir_list:
+        absDir = os.path.join(cwd, dir)
+        if dir.startswith("cpp"):
+            version = dir.replace("cpp", "c++")
+        else:
+            version = "c++11"
+        print(absDir)
+        os.chdir(absDir)
 
-    f = open('SConscript', 'w')
-    f.write('#1. Env setttings' + os.linesep)
-    f.write('import os' + os.linesep)
-    f.write('env=Environment(ENV=os.environ)' + os.linesep)
-    f.write('env.Append(CXXFLAGS=\"--std=' + version + '\"' + os.linesep)
-    f.write('#2. Targets' + os.linesep)
-    file_list = find_source_files(absDir)
-    file_list.sort()
-    for file_path in file_list:
-        basename = os.path.basename(file_path)
-        p = os.path.splitext(basename)[0]
-        LIBS = "\', LIBS=[\'atomic\', \'pthread\'])"
-        cmd = "env.Program(target=\'" + p + "\', source=\'" + basename + LIBS + os.linesep
-        f.write(cmd)
-    f.close()
+        f = open('SConscript', 'w')
+        f.write('#1. Env setttings' + os.linesep)
+        f.write('import os' + os.linesep)
+        f.write('env=Environment(ENV=os.environ)' + os.linesep)
+        f.write('env.Append(CXXFLAGS=\"--std=' + version + '\")' + os.linesep)
+        f.write('#2. Targets' + os.linesep)
+        file_list = find_source_files(absDir)
+        file_list.sort()
+        for file_path in file_list:
+            basename = os.path.basename(file_path)
+            p = os.path.splitext(basename)[0]
+            if libs_flag == "":
+                f.write("env.Program(target=\'" + p + "\', source=\'" + basename + "\')" + os.linesep)
+            else:
+                f.write("env.Program(target=\'" + p + "\', source=\'" + basename + libs_flag + os.linesep)
+        f.close()
+
+generate_sconscript(["cpp11", "cpp14", "cpp17"], "\', LIBS=[\'atomic\', \'pthread\'])")
+generate_sconscript(["cpp98", "datastructure"])
