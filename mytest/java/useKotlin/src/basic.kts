3de32af3b56bcadce4dc02520506037d3f022041
liming.glm <liming.glm@alibaba-inc.com>
2019-02-03 13:11:59 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: Ie05092b89e78a9c4ecdf4fe88c6829883f12e51f

--- mytest/java/useKotlin/src/basic.kts
+++ mytest/java/useKotlin/src/basic.kts
@@ -3,3 +3,41 @@ fun main(args: Array<String>) {
     println("Hello World!" + args[0])
 }
 println("hello")
+var a1 = 1
+val s = "string"
+val temp:String = "finish"
+a1 = 2
+class Test {
+    companion object {
+        var Global = "hello"
+    }
+}
+println(Test.Global)
+
+val name get() = 2
+println(name.javaClass)
+
+fun add(x:Int, y:Int):Int{return x+y}
+fun minus(x:Int, y:Int)=x-y
+fun addPrint(x:Int, y:Int){println(x+y)}
+fun toString(x:Int, y:Int):String?{
+    if(x>y){return x.toString()}
+    else{return null}
+}
+val a = "A"
+val b = "B"
+println("a的值是" + a + ",b的值是" + b)//传统写法
+println("a的值是$a,b的值是$b")//Kotlin替代传统写法
+println("a的值是${a.replace("A", "C")},b的值是$b")//Kotlin写法，在模板中写代码
+
+class TestA {
+    init {}
+    open fun testOpen() {
+        println(this)
+    }
+}
+class TestB : TestA {
+    override fun testOpen() {
+        super.testOpen()
+    }
+}
