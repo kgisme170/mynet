ab0479c80196a8383c5440809bcf2f5dbafb5221
liming.glm <liming.glm@alibaba-inc.com>
2019-02-05 07:45:56 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I3b01ed617e5beb5258f25bffeb3dc8fa33ae0747

--- mytest/linux/genSConscript.py
+++ mytest/linux/genSConscript.py
@@ -7,4 +7,4 @@ from findFiles import *
 generate_sconscript("multiplex/linux", "--std=c++11", "['pthread']")
 generate_sconscript("multiplex/posix", "--std=c++11", "['pthread']")
 generate_sconscript("multiplex/pthread", "--std=c++11", "['pthread']")
-
+generate_sconscript("sysv", "--std=c++98")
