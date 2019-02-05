28f1db90194b5f69c9badf0d5e8867e966e42ea5
liming.glm <liming.glm@alibaba-inc.com>
2019-02-05 08:37:55 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I29110b2f8023052fb1fe46194b074147726b74c6

---
+++ mytest/build/useBazel/demo-project/src/main/java/com/demo/DemoRunner.java
@@ -0,0 +1,6 @@
+package com.demo;
+public class DemoRunner {
+    public static void main(String args[]) {
+        Hello.hello();
+    }
+}
---
+++ mytest/build/useBazel/demo-project/src/main/java/com/demo/Hello.java
@@ -0,0 +1,7 @@
+package com.demo;
+
+public class Hello {
+    public static void hello() {
+        System.out.println("hello,world");
+    }
+}
