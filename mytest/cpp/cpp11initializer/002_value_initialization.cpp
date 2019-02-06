689b9564795076c656c8a00bb27b29de534187c9
liming.glm <liming.glm@alibaba-inc.com>
2019-02-06 08:20:39 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: I0a00521e61740c36689e816f07c1a1259be62b28

--- mytest/cpp/cpp11initializer/002_value_initialization.cpp
+++ mytest/cpp/cpp11initializer/002_value_initialization.cpp
@@ -5,8 +5,8 @@ struct B{int i;
 int main(){
     A* a1=new A;//a1->i是个随机值，未初始化
     A* a2=new A();//有了()，a2->i就是0，做了value-initialize
-    int *pi=new i(7);//注意，不是pi指向的不是数组，而是一个单个的int，初始化为7
-    int *qi=new i[8]();//申请8个int，全都初始化为0
+    int *pi=new int(7);//注意，不是pi指向的不是数组，而是一个单个的int，初始化为7
+    int *qi=new int[8]();//申请8个int，全都初始化为0
     B* pb=new B[5]();//申请5个B类型对象，调用B::B()初始化
 
     A* p=new A[5]{{1},{2},{3}};//剩下的两个A初始化为0
