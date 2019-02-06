b3f84cfcb4b24b542951179c111bb46b4f93e047
liming.glm <liming.glm@alibaba-inc.com>
2019-02-06 08:24:19 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: Id26901fdccdfe7be81c516ae6232fe9675ecbb57

--- mytest/cpp/cpp11initializer/009_dereference_initializer_list.cpp
+++ mytest/cpp/cpp11initializer/009_dereference_initializer_list.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 initializer_list<int> f(){return {1,2,3};}
-int main() {}
+int main() {
     for(auto& e in f()){
         cout<<e<<endl;//打印的可能是随机值
     }
--- mytest/cpp/cpp11initializer/012_empty_initializer_list.cpp
+++ mytest/cpp/cpp11initializer/012_empty_initializer_list.cpp
@@ -6,7 +6,7 @@ struct A{
     A(const initializer_list<int> dlist){cout<<"A init\n";}
 };
 int main() {
-    A a{};//打印A int
-    A a{{}};//打印A init
+    A a1{};//打印A int
+    A a2{{}};//打印A init
     return 0;
 }
