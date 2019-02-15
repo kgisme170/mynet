4aa7dec135e2f031420749753f230681bc526baa
kgisme170 <kgisme170@163.com>
2019-02-15 03:42:39 +0000

    cmake cpp

---
+++ mytest/build/useCmake/transmitDeps/dl.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+
+int main(){
+    void * handle = dlopen("./libmya.so", RTLD_LAZY);
+    if (!handle) {
+        fprintf(stderr, "%s\n", dlerror());
+        exit(EXIT_FAILURE);
+    }
+    int (*f)(int) = (int(*)(int)) dlsym(handle, "_Z1ai");
+    char* error = dlerror();
+    if (error != NULL) {
+        fprintf(stderr, "%s\n", error);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("%d\n", f(3));
+    dlclose(handle);
+    exit(EXIT_SUCCESS);
+    return 0;
+}
