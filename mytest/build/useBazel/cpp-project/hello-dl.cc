bb4ccef2e015c5b32f52db2197564f737983e0b5
kgisme170 <kgisme170@163.com>
2019-02-15 06:42:58 +0000

    fix bazel

--- mytest/build/useBazel/cpp-project/hello-dl.cc
+++ mytest/build/useBazel/cpp-project/hello-dl.cc
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 #include <dlfcn.h>
 int main(){
-    void * handle = dlopen("hello-greet", RTLD_LAZY);
+    void * handle = dlopen("libhello-greet.so", RTLD_LAZY);
     if (!handle) {
         fprintf(stderr, "%s\n", dlerror());
         exit(EXIT_FAILURE);
     }
-    void (*f)() = (void (*)()) dlsym(handle, "f");
+    void (*f)() = (void (*)()) dlsym(handle, "_Z1fv");
     char* error = dlerror();
     if (error != NULL) {
         fprintf(stderr, "%s\n", error);
