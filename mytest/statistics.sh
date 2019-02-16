a52e6054e9f7129ff5ba6e522081704f3420bed1
liming.glm <liming.glm@alibaba-inc.com>
2019-02-16 21:45:33 +0800

    *改动描述:
    *Bug 或 Feature: Bug
    *依赖信息:
        无
    *测试相关:
       - 自动测试 - UT: Y
       - UT Case 路径 : odps_src/test/unit_test/xxxx
       - 自动测试 - FT: N
       - FT Case 路径: NA
       - 手动测试描述：
          无
    *影响评估：
       - 性能影响：N
       - 性能影响描述： 无
       - 兼容性影响:  N
       - 兼容性影响描述： 无
       - 配置变更影响:  N
       - 是否已提交并完成部署变更review:  N
       - 部署变更步骤描述:
         无
    
    *其他信息：
         无
    
    Change-Id: Ib936053eb869bbd77d31d9266329b1b7267ecb4d

--- mytest/statistics.sh
+++ mytest/statistics.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 BASEDIR=$(dirname "$0")
 echo "find in $BASEDIR file number:"
-find $BASEDIR -name "*.c" -o -name "*.cpp" -o -name "*.h" -o -name "*java" -o -name "*.scala" -o -name "*.kt" -o -name "*.kts" -o -name "*.md" -o -name "*.py" -o -name "*.groovy" -o -name "*.gradle" -o -name "SConstruct" -o -name "SConscript" -o -name "*.xml" -o -name "*.json" -o -name "*.sh" -o -name "*.txt" -o -name "*.yml" -o -name "*.MF" -o -name "*.csv" -o -name "*conf*" -o -name "*.policy" -o -name "*.rb" -o -name "MANIFEST.MF" -o -name "*.erl" -o -name "*.Command"|wc -l
+find $BASEDIR -name "*.c" -o -name "*.cpp" -o -name "*.h" -o -name "*java" -o -name "*.scala" -o -name "*.kt" -o -name "*.kts" -o -name "*.md" -o -name "*.py" -o -name "*.groovy" -o -name "*.gradle" -o -name "SConstruct" -o -name "SConscript" -o -name "*.xml" -o -name "*.json" -o -name "*.sh" -o -name "*.txt" -o -name "*.yml" -o -name "*.MF" -o -name "*.csv" -o -name "*conf*" -o -name "*.policy" -o -name "*.rb" -o -name "MANIFEST.MF" -o -name "*.erl" -o -name "*.Command" -o -name "*.lua"|wc -l
