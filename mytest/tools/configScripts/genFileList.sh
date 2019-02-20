1493275fbb41ea5a88d59bc74669db0aab740a92
kgisme170 <kgisme170@163.com>
2019-02-20 08:14:33 +0000

    clean

--- mytest/tools/configScripts/genFileList.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-find . -type f -iname '*.cpp' -or -iname '*.h' -or -iname '*.cc' |grep -v third_party|grep -v thirdparty|grep -v "build/release64" |grep -v "\./bin/" |grep -v "\./package"|grep -v base_apsara|grep -v "\./llvm"|grep -v "\./hive"|grep -v "./.odps_build_dependencies"|grep -v "\./apsara/"|sort
