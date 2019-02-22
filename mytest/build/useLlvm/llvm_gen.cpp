22726ce3e35b39d0de34d0867cc88708de2533a2
kgisme170 <kgisme170@163.com>
2019-02-22 02:50:00 -1000

    ok

--- mytest/build/useLlvm/llvm_gen.cpp
+++ mytest/build/useLlvm/llvm_gen.cpp
@@ -1,46 +1,39 @@
-#include <unistd.h>
-#include <cstdio>
-#include <float.h>
-#include <string>
-#include <vector>
-#include <sstream>
-#include <iostream>
-
-#include <llvm/Support/ManagedStatic.h>
-#include <llvm/Support/DynamicLibrary.h>
-#include <llvm/Support/MemoryBuffer.h>
-#include <llvm/Support/TargetSelect.h>
-#include <llvm/Support/raw_ostream.h>
-#include <llvm/Support/PrettyStackTrace.h>
-#include <llvm/Bitcode/BitcodeReader.h>
-#include <llvm/Bitcode/BitcodeWriter.h>
-
-#include <llvm/IR/DataLayout.h>
-#include <llvm/IR/DerivedTypes.h>
-#include <llvm/IR/IRBuilder.h>
-#include <llvm/IR/LegacyPassManager.h>
-#include <llvm/IR/Verifier.h>
-
-#include <llvm/Analysis/Passes.h>
-#include <llvm/Analysis/TargetTransformInfo.h>
-
-#include <llvm/Target/TargetMachine.h>
-
-#include <llvm/Transforms/Scalar.h>
-#include "llvm/Transforms/IPO.h"
-#include <llvm/Transforms/IPO/PassManagerBuilder.h>
-#include <llvm/Transforms/Utils/Cloning.h>
-
-#include <llvm/ExecutionEngine/GenericValue.h>
-#include <llvm/ExecutionEngine/MCJIT.h>
-#include <llvm/ExecutionEngine/ExecutionEngine.h>
 #include "llvm_gen.h"
-
 #define Debug() cerr
 
-using namespace std;
-using namespace llvm;
+bool llvm_gen::init(const string& fileName) {
+    if(!llvm_is_multithreaded()) {
+	printf("Not multi thread\n");
+    }
+    InitializeNativeTarget();
+    InitializeNativeTargetAsmPrinter();
+    InitializeNativeTargetAsmParser();
 
-int main(){
+    LLVMContext mContext;
+    ErrorOr<std::unique_ptr<MemoryBuffer>> buf = MemoryBuffer::getFile(fileName);
+    Expected<std::unique_ptr<Module>> m = parseBitcodeFile(**buf, mContext);
+    if (m) {
+	printf("Fail to parse IR module\n");
+	return false;
+    }
+    unique_ptr<Module> mModule(m->get());
+    mModule->setModuleIdentifier("myId");
+    unique_ptr<EngineBuilder> mEngineBuilder(new EngineBuilder(move(mModule)));
+    string cpuName = sys::getHostCPUName().str();
+    mEngineBuilder->setMCPU(cpuName);
+    mEngineBuilder->setEngineKind(EngineKind::JIT);
+    mEngineBuilder->setOptLevel(CodeGenOpt::Aggressive);
+    string sErr;
+    mEngineBuilder->setErrorStr(&sErr);
+    ExecutionEngine* mMCJit = mEngineBuilder->create();
+    if (!mMCJit) {
+	printf("Cannot create MCJit: %s\n", sErr.c_str());
+	return false;
+    }
+}
+
+int main(int argc, char* argv[]){
+    llvm_gen gen;
+    gen.init(argv[1]);
     return 0;
 }
--- mytest/build/useLlvm/llvm_gen.h
+++ mytest/build/useLlvm/llvm_gen.h
@@ -1,29 +1,57 @@
 #pragma once
-#include <stdint.h>
-#include <set>
-#include <string>
-#include <vector>
-#include <iostream>
-#include <tr1/functional>
-#include <tr1/unordered_set>
-#include <llvm/IR/Constants.h>
-#include <llvm/IR/DerivedTypes.h>
-#include <llvm/IR/DIBuilder.h>
-#include <llvm/IR/IRBuilder.h>
-#include <llvm/IR/Instructions.h>
-#include <llvm/IR/LLVMContext.h>
-#include <llvm/IR/Module.h>
-#include <llvm/ExecutionEngine/GenericValue.h>
-#include <llvm/ExecutionEngine/ExecutionEngine.h>
-
-typedef std::tr1::function<void(void)> llmmCallback;
-
-#include <map>
-#include <string>
-#include <sstream>
-#include <iomanip>
 #include <algorithm>
+#include <cstdio>
+#include <float.h>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <set>
+#include <sstream>
+#include <stdint.h>
+#include <string>
+#include <unistd.h>
+#include <unordered_set>
+#include <vector>
 
-#include "llvm/IR/Instruction.h"
-#include "llvm/IR/Module.h"
-#include "llvm/IR/Function.h"
+#include "llvm/Transforms/IPO.h"
+#include <llvm/Analysis/Passes.h>
+#include <llvm/Analysis/TargetTransformInfo.h>
+#include <llvm/Bitcode/BitcodeReader.h>
+#include <llvm/Bitcode/BitcodeWriter.h>
+#include <llvm/ExecutionEngine/ExecutionEngine.h>
+#include <llvm/ExecutionEngine/GenericValue.h>
+#include <llvm/ExecutionEngine/MCJIT.h>
+#include <llvm/IR/Constants.h>
+#include <llvm/IR/DIBuilder.h>
+#include <llvm/IR/DataLayout.h>
+#include <llvm/IR/DerivedTypes.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/Instruction.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/LegacyPassManager.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Verifier.h>
+#include <llvm/Support/DynamicLibrary.h>
+#include <llvm/Support/ManagedStatic.h>
+#include <llvm/Support/MemoryBuffer.h>
+#include <llvm/Support/PrettyStackTrace.h>
+#include <llvm/Support/TargetSelect.h>
+#include <llvm/Support/raw_ostream.h>
+#include <llvm/Target/TargetMachine.h>
+#include <llvm/Transforms/IPO/PassManagerBuilder.h>
+#include <llvm/Transforms/Scalar.h>
+#include <llvm/Transforms/Utils/Cloning.h>
+
+using namespace std;
+using namespace llvm;
+
+typedef function<void(void)> llmmCallback;
+
+class llvm_gen {
+    LLVMContext mContext;
+public:
+    llvm_gen(){}
+    bool init(const string& fileName);
+};
