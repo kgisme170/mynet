dff9f552a65e70711c4d2f0ab3a1aaa26f786e79
Liming Gong <limgong@microsoft.com>
2019-06-01 10:38:44 +0800

    sampleCode

---
+++ testbed/mytest/ms/usql/downloadFile.ps1
@@ -0,0 +1,7 @@
+$webclient = New-Object System.Net.WebClient
+$mydocs = [environment]::getfolderpath("mydocuments")
+$url = "https://raw.githubusercontent.com/Azure/usql/master/Examples/Samples/Data/Sear
+chLog.tsv"
+$basename = Split-Path $url -Leaf
+$output_filename = Join-Path $mydocs $basename
+$webclient.DownloadFile($url,$output_filename)
