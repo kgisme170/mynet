70a5b27f6211ad8ece1c69b3bdb2e339179e9d08
limgong <limgong@microsoft.com>
2019-06-05 20:28:49 +0800

    convert

--- testbed/mytest/ms/usql/USQLApplication1/ConsoleApp1/Program.cs
+++ testbed/mytest/ms/usql/USQLApplication1/ConsoleApp1/Program.cs
@@ -1,11 +1,35 @@
 ﻿using System;
+using System.Configuration;
+
 namespace ConsoleApp1
 {
     class Program
     {
+        public static readonly double PI = Convert.ToDouble(ConfigurationManager.AppSettings[0]);
         public static void Main(string[] args)
         {
-            AnonymousTypes.Test();
+            //AnonymousTypes.Test();
+            object o1 = "hello";
+            object o2 = new string("hello".ToCharArray());
+            Console.WriteLine(o1 == o2);
+            Console.WriteLine(o1.Equals(o2));
+
+            string s1 = "hello";
+            string s2 = new string("hello".ToCharArray());
+            Console.WriteLine(s1 == s2);
+            Console.WriteLine(s1.Equals(s2));
+
+            int? i = null;
+            Console.WriteLine(i.HasValue);
+            try
+            {
+                Console.WriteLine(i.Value);
+            }
+            catch (Exception e){
+                Console.WriteLine(e.Message);
+            }
+            int x = int.Parse("123");
+            Console.WriteLine(PI);
         }
     }
 }
