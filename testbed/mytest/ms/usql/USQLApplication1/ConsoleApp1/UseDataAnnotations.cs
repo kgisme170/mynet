91d9abbb42f0e024d9ebc1ad1dab17949f6d8b2f
limgong <limgong@microsoft.com>
2019-06-06 21:45:29 +0800

    annotation type

--- testbed/mytest/ms/usql/USQLApplication1/ConsoleApp1/UseDataAnnotations.cs
+++ testbed/mytest/ms/usql/USQLApplication1/ConsoleApp1/UseDataAnnotations.cs
@@ -6,6 +6,18 @@ namespace ConsoleApp1
         [Required(ErrorMessage = "Name is compulsory")]
         [StringLength(20)]
         [RegularExpression(@"^[A-Z]{5, 20}$")]
+        [DataType(DataType.Text)]
         public string Name { get; set; }
+
+        [Range(0,120, ErrorMessage ="Age between 0 to 120")]
+        public int Age { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [Phone]
+        public string Cell { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
     }
 }
