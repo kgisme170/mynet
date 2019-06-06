e40b5108044e6073d673642dab0288b7bb0c581e
limgong <limgong@microsoft.com>
2019-06-06 12:01:00 +0800

    DisposeFinalize

--- testbed/mytest/ms/usql/USQLApplication1/ConsoleApp1/Program.cs
+++ testbed/mytest/ms/usql/USQLApplication1/ConsoleApp1/Program.cs
@@ -1,28 +1,12 @@
 ﻿using System;
-using System.Threading;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        public static volatile int count = 0;
-        public static void Method()
-        {
-            Console.WriteLine("s");
-            while (count < 1000)
-            {
-                Thread.Sleep(10);
-                ++count;
-            }
-            Console.WriteLine("e");
-        }
-
         public static void Main(String [] args)
         {
-            new Thread(Method).Start();
-            Thread.Sleep(4000);
-            count = 1000;
-            Console.ReadLine();
+            Destructor.Test();
         }
     }
 }
---
+++ testbed/mytest/ms/usql/USQLApplication1/ConsoleApp1/UseBond.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class UseBond
+    {
+        public static void Test()
+        {
+            byte[] by = new byte[10];
+            //Bond.BondBlob b = Bond.BondBlob(by);
+        }
+    }
+}
