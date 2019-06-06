7acba4c00a7f5f30657af97ce8f59861aea51b2e
limgong <limgong@microsoft.com>
2019-06-06 10:00:34 +0800

    coalescing

---
+++ testbed/mytest/ms/usql/USQLApplication1/ConsoleApp1/Coalescing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    class Coalescing
+    {
+        public static void Test()
+        {
+            string str1 = null;
+            string str2 = null;
+            string str3 = null;
+            string str4 = "abc";
+            string str5 = str1 ?? str2 ?? str3 ?? str4;
+            Console.WriteLine(str5);
+        }
+    }
+}
