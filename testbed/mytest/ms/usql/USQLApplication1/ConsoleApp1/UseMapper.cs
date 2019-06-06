6e1bf672f58fafe7d1b466e10bf9005261daa2d9
limgong <limgong@microsoft.com>
2019-06-06 09:57:51 +0800

    Add mapper

---
+++ testbed/mytest/ms/usql/USQLApplication1/ConsoleApp1/UseMapper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+namespace ConsoleApp1
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Salary { get; set; }
+    }
+    class UseMapper
+    {
+        public static void Test()
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Person, Employee>());
+            var mapper = config.CreateMapper();
+            Person p = new Person()
+            {
+                FirstName = "j",
+                LastName = "l"
+            };
+            Employee emp = mapper.Map<Employee>(p);
+        }
+    }
+}
--- testbed/mytest/ms/usql/USQLApplication1/OrdersLib/OrdersLib.cs
+++ testbed/mytest/ms/usql/USQLApplication1/OrdersLib/OrdersLib.cs
@@ -8,11 +8,6 @@ using Microsoft.Analytics.Interfaces;
 using Microsoft.Analytics.Interfaces;
 using Microsoft.Analytics.Types.Sql;
 */
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
 namespace MyNs
 {
     public static class HelpersOut
