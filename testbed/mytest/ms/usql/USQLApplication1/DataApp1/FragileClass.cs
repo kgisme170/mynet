3b59563b1341bca2687c17b887d3794c715d9ee4
limgong <limgong@microsoft.com>
2019-06-07 21:40:39 +0800

    C# fragile class

---
+++ testbed/mytest/ms/usql/USQLApplication1/DataApp1/FragileClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataApp1
+{
+    class Base
+    {
+        public virtual void F(int i)
+        {
+            Console.WriteLine("Base F");
+        }
+    }
+    class Derive : Base
+    {
+        public void F(double d)
+        {
+            Console.WriteLine("Derive F");
+        }
+    }
+    class FragileClass
+    {
+        public static void Test()
+        {
+            Derive d = new Derive();
+            d.F(2);
+        }
+    }
+}
--- testbed/mytest/ms/usql/USQLApplication1/DataApp1/Program.cs
+++ testbed/mytest/ms/usql/USQLApplication1/DataApp1/Program.cs
@@ -16,6 +16,7 @@ namespace DataApp1
 
             DateTime dt = DateTime.Now;
             Console.WriteLine(dt == null);
+            FragileClass.Test();
         }
     }
 }
