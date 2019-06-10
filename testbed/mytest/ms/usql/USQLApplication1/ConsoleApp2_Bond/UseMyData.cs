a20b39df37c5d682a3e989cb3872b9650d820cf0
limgong <limgong@microsoft.com>
2019-06-10 22:53:48 +0800

    Use bond ok

--- testbed/mytest/ms/usql/USQLApplication1/ConsoleApp2_Bond/UseMyData.cs
+++ testbed/mytest/ms/usql/USQLApplication1/ConsoleApp2_Bond/UseMyData.cs
@@ -3,11 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Microsoft.Bond;
 namespace ConsoleApp2_Bond
 {
+    class RawData
+    {
+        public string Name { set; get; }
+        public Int64 Id { set; get; }
+        public byte[] Photo { set; get; }
+    }
     class UseMyData
     {
-        
+        public static void Test()
+        {
+            MyData myData = new MyData
+            {
+                Name = "john",
+                Id = 123,
+                Photo = new BondBlob(new byte[10])
+            };
+            RawData rawData = new RawData
+            {
+                Name = "john",
+                Id = 123,
+                Photo = new byte[10]
+            };
+            
+        }
     }
 }
