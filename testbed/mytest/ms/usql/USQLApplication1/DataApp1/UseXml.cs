e48230f7a292c1728d9b201bcb069b70b3695110
limgong <limgong@microsoft.com>
2019-06-10 14:02:22 +0800

    LinqToXml

---
+++ testbed/mytest/ms/usql/USQLApplication1/DataApp1/UseXml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+using System.Collections.Generic;
+namespace DataApp1
+{
+    class UseXml
+    {
+        public static void Test()
+        {
+            var xml = XDocument.Load(@"XmlToText.xml");
+            IEnumerable<XElement> nodes =
+                from item in xml.Descendants("floor")
+                select item;
+            foreach(var n in nodes)
+            {
+                Console.Write(n.Element("name").Value+":");
+                Console.WriteLine(n.Element("rooms").Value);
+            }
+        }
+    }
+}
