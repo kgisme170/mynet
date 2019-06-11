c7026138b44326a50620ff82b98f09f0787455f9
limgong <limgong@microsoft.com>
2019-06-11 17:34:36 +0800

    struct and immutable

--- testbed/mytest/ms/usql/USQLApplication1/ConsoleApp2_Bond/UseMyData.cs
+++ testbed/mytest/ms/usql/USQLApplication1/ConsoleApp2_Bond/UseMyData.cs
@@ -1,10 +1,22 @@
 ﻿using Microsoft.Bond;
 using System;
+using System.ComponentModel;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 
 namespace ConsoleApp2_Bond
 {
+    struct MyStruct
+    {
+        public int mI;
+        public string mS;
+    }
+    [ImmutableObject(true)]
+    struct ComplexStruct
+    {
+        public int mJ;
+        public MyStruct st;
+    }
     [Serializable]
     class RawData
     {
@@ -35,6 +47,40 @@ namespace ConsoleApp2_Bond
             bitFormatter.Serialize(memoryStream, rawData);
             byte [] bytes = memoryStream.ToArray();
             Console.WriteLine(bytes.Length);
+
+            MyStruct s1;
+            s1.mI = 1;
+            s1.mS = "ab";
+
+            MyStruct s2 = new MyStruct
+            {
+                mI = 2,
+                mS = "xy"
+            };
+
+            MyStruct s3 = s1;
+            s3.mI = 4;
+            MyStruct s4 = s2;
+            s4.mI = 5;
+            Console.WriteLine(s1.mI);
+            Console.WriteLine(s2.mI);
+
+            ComplexStruct cs = new ComplexStruct
+            {
+                mJ = 6,
+                st = new MyStruct
+                {
+                    mI = 7,
+                    mS = "immutable"
+                }
+            };
+            cs.st.mS = "Changed";
+            Console.WriteLine(cs.st.mS);
+            cs.st = new MyStruct
+            {
+                mI = 8,
+                mS = "check immutable?"
+            };
         }
     }
 }
