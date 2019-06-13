9274624780637f8cf818c7d0fdfeb3e3a45c7501
limgong <limgong@microsoft.com>
2019-06-13 10:39:21 +0800

    suppress warning

--- testbed/mytest/ms/usql/USQLApplication1/DataApp1/CodeGen/GenerateClass.cs
+++ testbed/mytest/ms/usql/USQLApplication1/DataApp1/CodeGen/GenerateClass.cs
@@ -1,6 +1,6 @@
 ﻿
 // This is generated code:
-#pragma warning disable CS0401 
+#pragma warning disable CS0414
 class MyGeneratedClass {
   // Generated code:
   private int P1 = 0;
--- testbed/mytest/ms/usql/USQLApplication1/DataApp1/Program.cs
+++ testbed/mytest/ms/usql/USQLApplication1/DataApp1/Program.cs
@@ -1,5 +1,11 @@
-﻿namespace DataApp1
+﻿#pragma warning disable CS0414
+namespace DataApp1
 {
+    class NotUsed
+    {
+        private int pi = 1;
+        private int pj = 2;
+    }
     class Program
     {
         static void Main(string[] args)
