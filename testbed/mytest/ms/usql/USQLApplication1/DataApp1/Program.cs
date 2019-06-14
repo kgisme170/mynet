6e933e13f15196c516283d0258c95c5403f99cef
limgong <limgong@microsoft.com>
2019-06-14 21:02:26 +0800

    ObserverObservable

--- testbed/mytest/ms/usql/USQLApplication1/DataApp1/Program.cs
+++ testbed/mytest/ms/usql/USQLApplication1/DataApp1/Program.cs
@@ -3,14 +3,14 @@ namespace DataApp1
 {
     class NotUsed
     {
-        private int pi = 1;
-        private int pj = 2;
+        private readonly int pi = 1;
+        private readonly int pj = 2;
     }
     class Program
     {
         static void Main(string[] args)
         {
-            UseEnumerator.Test();
+            UseObservable.Test();
         }
     }
 }
