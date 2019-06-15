bd78a1a75ea699bc3c0fa5186e95f351bbae8d09
kgisme170 <kgisme170@163.com>
2019-06-15 16:27:56 +0800

    Split program1

--- testbed/mytest/ms/usql/USQLApplication1/ConsoleApp2/Program.cs
+++ testbed/mytest/ms/usql/USQLApplication1/ConsoleApp2/Program.cs
@@ -1,150 +1,48 @@
-﻿using Microsoft.ComplexEventProcessing;
-using Microsoft.ComplexEventProcessing.Linq;
-using System;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading;
+﻿using System;
 
 namespace ConsoleApp2
 {
-    class Program
+    partial class Program
     {
-        readonly private static PayloadOutputConfig outputConfig = new PayloadOutputConfig();
-        private static void RunQuery(CepStream<Payload> cepStream, Application application)
+        public static int GetChoice()
         {
-            // Configure output adapter
-
-            // Create query and bind to the output adapter
-            var query = cepStream.ToQuery(application, Guid.NewGuid().ToString(), "description", typeof(PayloadOutputFactory), outputConfig, EventShape.Point, StreamEventOrder.ChainOrdered);
-
-            // Start query
-            query.Start();
-
-            // Wait until query change state
-            DiagnosticView diagnosticView;
-            do
+            int choice = -1;
+            while(choice != -1)
             {
-                Thread.Sleep(100);
-                diagnosticView = query.Application.Server.GetDiagnosticView(query.Name);
-            } while ((string)diagnosticView[DiagnosticViewProperty.QueryState] == "Running");
+                try
+                {
+                    Console.WriteLine("Enter choice, 0 to quit");
+                    Console.WriteLine("1: Test generated timed data");
+                    Console.WriteLine("2: Test from excel file data");
 
-            // Stop query
-            query.Stop();
+                    string line = Console.ReadLine();
+                    choice = int.Parse(line);
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+            return choice;
         }
 
         static void Main(string[] args)
         {
-            // Create a tracer to output information on the console.
-            TraceListener tracer = new ConsoleTraceListener();
-            using (Server server = Server.Create("MyInstance"))
+            int choice = GetChoice();
+            switch (choice)
             {
-                try
-                {
-                    Application application = server.CreateApplication("application");
-                    var ericSEKConfig = new InputConfig
-                    {
-                        ID = "ERIC-SEK",
-                        Filename = "eric_b_sek_2009.csv",
-                        ColumnNames = new string[] { "Open", "High", "Low", "Close", "Volume", "Adj Close" },
-                        StartDate = new DateTime(2009, 01, 01),
-                        Interval = 0
-                    };
-
-                    // Implicit 方式
-                    var cepStream = CepStream<Payload>.Create("cepStream",
-                                                                   typeof(PayloadInputFactory),
-                                                                   ericSEKConfig,
-                                                                   EventShape.Point);
-
-                    var filtered = from e in cepStream
-                                   where e.Value > 95
-                                   select e;
-                    /*
-                    var query = filtered.ToQuery(application,
-                                                 "filterQuery",
-                                                 "Filter out Values over 95",
-                                                 typeof(PayloadOutputFactory),
-                                                 new PayloadOutputConfig(),
-                                                 EventShape.Point,
-                                                 StreamEventOrder.FullyOrdered);
-                                                 */
-                    RunQuery(filtered, application);
-
-                    var avgCepStream = from w in cepStream.Where(e => e.FieldID == "Close")
-                                                 .HoppingWindow(TimeSpan.FromDays(7), TimeSpan.FromDays(1), HoppingWindowOutputPolicy.ClipToWindowEnd)
-                                       select new Payload()
-                                       {
-                                           StockID = "ERIC",
-                                           FieldID = "7-day avg",
-                                           Value = w.Avg(e => e.Value)
-                                       };
-
-                    RunQuery(avgCepStream, application);
-
-                    var ericUSDGroupCepStream = from e in cepStream
-                                                group e by e.FieldID into eGroup
-                                                from w in eGroup.HoppingWindow(TimeSpan.FromDays(7), TimeSpan.FromDays(1), HoppingWindowOutputPolicy.ClipToWindowEnd)
-                                                select new Payload()
-                                                {
-                                                    StockID = "ERIC 7-day avg",
-                                                    FieldID = eGroup.Key,
-                                                    Value = w.Avg(e => e.Value)
-                                                };
-
-                    RunQuery(ericUSDGroupCepStream, application);
-
-                    var bigLooserCepStream = (from e1 in cepStream
-                                              from e2 in cepStream.ShiftEventTime(e => e.StartTime.AddDays(7))
-                                              where e1.FieldID == "Close" && e2.FieldID == "Close"
-                                              select new Payload()
-                                              {
-                                                  StockID = "ERIC > 10% drop",
-                                                  FieldID = "Close",
-                                                  Value = (e1.Value - e2.Value) / e2.Value * 100
-                                              }).Where(e => e.Value < -10);
-
-
-                    RunQuery(bigLooserCepStream, application);
-
-                    // Explicit 方式
-                    var input = CepStream<Payload>.Create("input");
-                    var stddevCepStream = from w in input.Where(e => e.FieldID == "Close")
-                                                                     .HoppingWindow(TimeSpan.FromDays(7), TimeSpan.FromDays(1), HoppingWindowOutputPolicy.ClipToWindowEnd)
-                                          select new Payload()
-                                          {
-                                              StockID = "ERIC",
-                                              FieldID = "7-day Stddev",
-                                              Value = w.StandardDeviation()
-                                          };
-                    var queryTemplate = application.CreateQueryTemplate("standardDeviationExampleTemplate", "Description...", stddevCepStream);
-
-                    var queryBinder = new QueryBinder(queryTemplate);
-                    var InputAdapterFromFile = application.CreateInputAdapter<PayloadInputFactory>("PayloadInput", "Description...");
-                    var outputAdapter = application.CreateOutputAdapter<PayloadOutputFactory>("PayloadOutput", "Description...");
-                    queryBinder.BindProducer<Payload>("input", InputAdapterFromFile, ericSEKConfig, EventShape.Point);
-                    queryBinder.AddConsumer<Payload>("output", outputAdapter, outputConfig, EventShape.Point, StreamEventOrder.ChainOrdered);
-
-                    var query = application.CreateQuery("standardDeviationExampleQuery", "Description...", queryBinder);
-                    query.Start();
-
-                    // Wait until output adapter signals that it is finished
-                    DiagnosticView diagnosticView;
-                    do
-                    {
-                        Thread.Sleep(100);
-                        diagnosticView = query.Application.Server.GetDiagnosticView(query.Name);
-                    } while ((string)diagnosticView["QueryState"] == "Running");
-
-                    // Stop
-                    query.Stop();
-                    // Release resources
-                    application.Delete();
-                    server.Dispose();
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.ToString());
-                }
+                case 0:
+                    Console.WriteLine("Quit");
+                    break;
+                case 1:
+                    Test1();
+                    break;
+                case 2:
+                    Test2();
+                    break;
+                default:
+                    Console.WriteLine("error");
+                    break;
             }
         }
     }
---
+++ testbed/mytest/ms/usql/USQLApplication1/ConsoleApp2/Program1.cs
@@ -0,0 +1,217 @@
+﻿using Microsoft.ComplexEventProcessing;
+using Microsoft.ComplexEventProcessing.Adapters;
+using Microsoft.ComplexEventProcessing.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace ConsoleApp2
+{
+    public class TollData
+    {
+        public DateTime Timestamp { get; set; }
+        public int Number { get; set; }
+    }
+
+    public class TollConfig
+    {
+        public string TollName { get; set; }
+        public IEnumerator<TollData> TollData { get; set; }
+    }
+
+    public class OutConfig
+    {
+        public string OutConfigName { get; set; }
+    }
+
+    public class MediationData
+    {
+        public int Number { get; set; }
+    }
+
+    public class InAdapter : TypedPointInputAdapter<MediationData>
+    {
+        readonly TollConfig config;
+        public InAdapter(TollConfig config)
+        {
+            this.config = config;
+        }
+
+        private void ProduceEvents()
+        {
+            EnqueueCtiEvent(new DateTimeOffset(DateTime.Now, TimeSpan.Zero));
+            var data = config.TollData;
+
+            while (data.MoveNext())
+            {
+                var e = CreateInsertEvent();
+                TollData d = data.Current;
+                e.StartTime = d.Timestamp;
+                e.Payload = new MediationData
+                {
+                    Number = d.Number
+                };
+                Enqueue(ref e);
+            }
+        }
+
+        // override
+        public override void Resume()
+        {
+            ProduceEvents();
+        }
+
+        public override void Start()
+        {
+            ProduceEvents();
+        }
+    }
+
+    class InputFactory : ITypedInputAdapterFactory<TollConfig>
+    {
+        public InputAdapterBase Create<TPayload>(TollConfig configInfo, EventShape eventShape)
+        {
+            return new InAdapter(configInfo);
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("InputFactory dispose()");
+        }
+    }
+    public class OutAdapter : TypedPointOutputAdapter<MediationData>
+    {
+        private void ConsumeEvents()
+        {
+            while (true)
+            {
+                var result = Dequeue(out PointEvent<MediationData> d);
+                if(result == DequeueOperationResult.Empty)
+                {
+                    return;
+                }
+                if(d.EventKind == EventKind.Insert)
+                {
+                    Console.WriteLine("[" + d.StartTime + "]" + d.Payload.Number);
+                }
+            }
+        }
+
+        // implement
+        public override void Resume()
+        {
+            ConsumeEvents();
+        }
+
+        public override void Start()
+        {
+            ConsumeEvents();
+        }
+    }
+
+    public class OutputFactory : ITypedOutputAdapterFactory<TollConfig>
+    {
+        public OutputAdapterBase Create<TPayload>(TollConfig configInfo, EventShape eventShape)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("OutputFactory dispose()");
+        }
+    }
+
+    partial class Program
+    {
+        static DateTime now = DateTime.Now;
+        static int currentMinute = 1;
+        public static TollData GetTollData(int n)
+        {
+            return new TollData
+            {
+                Timestamp = DateTime.Now + TimeSpan.FromMinutes(currentMinute++),
+                Number = n
+            };
+        }
+        public static IEnumerator<TollData> GetData()
+        {
+            return new List<TollData>()
+            {
+                GetTollData(2),
+                GetTollData(4),
+                GetTollData(6),
+                GetTollData(8),
+                GetTollData(10),
+                GetTollData(12),
+                GetTollData(18),
+                GetTollData(0),
+                GetTollData(0),
+                GetTollData(3),
+                GetTollData(3),
+                GetTollData(3),
+                GetTollData(6),
+                GetTollData(9),
+                GetTollData(0),
+                GetTollData(0),
+                GetTollData(0),
+                GetTollData(15),
+                GetTollData(9),
+            }.GetEnumerator();
+        }
+        public static void Test1()
+        {
+            Server server = Server.Create("MyInstance");
+            Application application = server.CreateApplication("Myapp");
+
+            var tollData = GetData();
+            var tollConfig = new TollConfig()
+            {
+                TollName = "Toll1",
+                TollData = tollData
+            };
+
+            var input1 = CepStream<TollData>.Create("input1");
+            var outConfig = new OutConfig()
+            {
+                OutConfigName = "outConfig1"
+            };
+            var filter = from e in input1
+                         where e.Number > 3
+                         select e;
+            var binder = new QueryBinder(application.CreateQueryTemplate(
+                "template1",
+                "description1",
+                input1));
+            var inputAdaptor = application.CreateInputAdapter<InputFactory>("nameIn", "descIn");
+            var outputAdaptor = application.CreateOutputAdapter<OutputFactory>("nameOut", "descOut");
+
+            binder.BindProducer<TollData>(
+                "input1",
+                inputAdaptor,
+                tollConfig,
+                EventShape.Point);
+            binder.AddConsumer<TollData>(
+                "output1",
+                outputAdaptor,
+                outConfig,
+                EventShape.Point,
+                StreamEventOrder.ChainOrdered);
+
+            var query = application.CreateQuery("queyName", "desc", binder);
+            query.Start();
+            DiagnosticView diagnosticView;
+            do
+            {
+                Thread.Sleep(100);
+                diagnosticView = query.Application.Server.GetDiagnosticView(query.Name);
+            } while ((string)diagnosticView["QueryState"] == "Running");
+
+            query.Stop();
+
+            application.Delete();
+            server.Dispose();
+        }
+    }
+}
