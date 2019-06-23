2f908a73b6c49332edde5a9bdd4b6e2720bf2159
limgong <limgong@microsoft.com>
2019-06-23 11:51:30 +0800

    Add remoting

---
+++ testbed/mytest/ms/TrillApp/HelloServiceClient/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Windows.Forms;
+namespace HelloServiceClient
+{
+    public partial class Form1 : Form
+    {
+        IHelloRemotingService.IHelloRemotingService client;
+        public Form1()
+        {
+            InitializeComponent();
+            TcpChannel tcpChannel = new TcpChannel();
+            ChannelServices.RegisterChannel(tcpChannel, true);
+            client = (IHelloRemotingService.IHelloRemotingService)Activator.GetObject(
+                typeof(IHelloRemotingService.IHelloRemotingService),
+                "tcp://localhost:8080/GetMessage");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = client.GetMessage(textBox1.Text);
+        }
+    }
+}
---
+++ testbed/mytest/ms/TrillApp/RemotingServiceHost/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
+namespace RemotingServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloRemotingService.HelloRemotingService remotingService =
+                new HelloRemotingService.HelloRemotingService();
+            TcpChannel tcpChannel = new TcpChannel(8080);
+            try
+            {
+                ChannelServices.RegisterChannel(tcpChannel, true);
+
+                RemotingConfiguration.RegisterWellKnownServiceType(
+                    typeof(HelloRemotingService.HelloRemotingService),
+                    "GetMessage",
+                    WellKnownObjectMode.Singleton);
+                Console.WriteLine("Service started");
+                Console.ReadLine();
+            }
+            catch(Exception e)
+            {
+
+            }
+        }
+    }
+}
