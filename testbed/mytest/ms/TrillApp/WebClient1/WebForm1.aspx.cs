517c57a23c2b390398c1276194e1754462c623c9
limgong <limgong@microsoft.com>
2019-06-23 11:23:43 +0800

    Add webclient1

---
+++ testbed/mytest/ms/TrillApp/WebClient1/WebForm1.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebClient1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            HelloService.WebService1SoapClient client = new HelloService.WebService1SoapClient();
+            Label1.Text = client.GetMessage(TextBox1.Text);
+        }
+    }
+}
