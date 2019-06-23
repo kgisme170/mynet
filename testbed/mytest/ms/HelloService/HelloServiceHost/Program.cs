29c20c7b42687082f0cffa12caaba244b4cb70a6
limgong <limgong@microsoft.com>
2019-06-23 19:44:44 +0800

    wcf app demo

---
+++ testbed/mytest/ms/HelloService/HelloServiceHost/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ServiceModel;
+namespace HelloServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(ServiceHost serviceHost = new ServiceHost(typeof(HelloService.HelloService)))
+            {
+                serviceHost.Open();
+                Console.WriteLine("service starts");
+                Console.ReadLine();
+            }
+        }
+    }
+}
