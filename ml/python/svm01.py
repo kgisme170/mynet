9710c0137d64570fde4dd89df8acd623cce535be
kgisme170 <kgisme170@163.com>
2019-06-24 23:43:53 +0800

    ok

--- ml/python/svm01.py
+++ ml/python/svm01.py
@@ -10,14 +10,19 @@ clf = svm.SVC(kernel='linear', C=1000)
 clf.fit(X, y)
 
 plt.scatter(X[:, 0], X[:, 1], c=y, s=30, cmap=plt.cm.Paired)
+plt.show()
+newData = [[3,4],[5,6]]
+print(clf.predict(newData))
 
-#plt.show()
-#newData = [[3,4],[5,6]]
-#print(clf.predict(newData))
+#ax=plt.gca()
+#xlim=ax.get_xlim()
+#ylim=ax.get_ylim()
 
-ax=plt.gca()
-xlim=ax.get_xlim()
-ylim=ax.get_ylim()
+#xx=np.linspace(xlim[0], xlim[1], 30)
+#yy=np.linspace(ylim[0], ylim[1], 30)
+#YY, XX=np.meshgrid(yy,xx)
+#xy=np.vstack([XX.revel(), YY.ravel()]).T
+#Z=clf.decision_function(xy).reshape(XX.shape)
 
 xx=np.linspace(xlim[0], xlim[1], 30)
 yy=np.linspace(ylim[0], ylim[1], 30)
@@ -26,5 +31,3 @@ xy=np.vstack([XX.ravel(), YY.ravel()]).T
 Z=clf.decision_function(xy).reshape(XX.shape)
 
 ax.contour(XX,YY,Z,colors='k',levels=[-1,0,1],alpha=0.5)
-#ax.scatter(clf.support_vectors_[:,0]),clf.support_vectors_[:,1],s=100,linewidth=1,facecolors='none')
-plt.show()
