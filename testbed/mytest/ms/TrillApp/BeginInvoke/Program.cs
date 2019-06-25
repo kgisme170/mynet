581dcdbc25d304bc30f6d1546e53f0f5f6590d26
limgong <limgong@microsoft.com>
2019-06-25 22:24:05 +0800

    Add BeginInvoke

---
+++ testbed/mytest/ms/TrillApp/BeginInvoke/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace BeginInvoke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AsyncDemo ad = new AsyncDemo();
+            AsyncDelegate dlgt = new AsyncDelegate(ad.TestMethod);
+            object[] obj = { dlgt, 12 };
+            dlgt.BeginInvoke(3000, new AsyncCallback(CallbackMethod), obj);
+            Console.WriteLine("main thread begins sleep");
+            Thread.Sleep(1000);
+            Console.WriteLine("main thread ends sleep");
+            Console.ReadKey();
+        }
+        public delegate string AsyncDelegate(int callTime);
+        static void CallbackMethod(IAsyncResult ar)
+        {
+            object[] obj = (object[])ar.AsyncState;
+            AsyncDelegate dlgt = (AsyncDelegate)obj[0];
+            int iTest = (int)obj[1];
+            string result = dlgt.EndInvoke(ar);
+            Console.WriteLine("async finishes，{0}！", result);
+            Console.WriteLine("ar.AsyncState value:{0}", iTest);
+        }
+        class AsyncDemo
+        {
+            public string TestMethod(int callTime)
+            {
+                Console.WriteLine("async begins");
+                Thread.Sleep(callTime);
+                return "Method cost time" + callTime / 1000 + "s";
+            }
+        }
+    }
+}
--- testbed/mytest/ms/TrillApp/ConsoleApp1/Program.cs
+++ testbed/mytest/ms/TrillApp/ConsoleApp1/Program.cs
@@ -58,7 +58,6 @@ namespace ConsoleApp1
         }
         static void Main(string[] args)
         {
-
         }
     }
 }
