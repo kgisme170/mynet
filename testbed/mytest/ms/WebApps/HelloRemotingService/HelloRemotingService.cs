eb6f1ec04d92be327e50a4aedc2798b7ba6a5c39
limgong <limgong@microsoft.com>
2019-06-27 09:24:05 +0800

    Add web apps

---
+++ testbed/mytest/ms/WebApps/HelloRemotingService/HelloRemotingService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloRemotingService
+{
+    public class HelloRemotingService : MarshalByRefObject,
+        IHelloRemotingService.IHelloRemotingService
+    {
+        public string GetMessage(string name)
+        {
+            return "Hello " + name;
+        }
+    }
+}
---
+++ testbed/mytest/ms/WebApps/WebClient1/WebForm1.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WebClient1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            HelloService.WebService1SoapClient client = new HelloService.WebService1SoapClient();
+            Label1.Text = client.GetMessage(TextBox1.Text);
+        }
+    }
+}
