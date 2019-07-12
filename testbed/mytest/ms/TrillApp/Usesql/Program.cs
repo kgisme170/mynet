cb2e56594c8e18e0ae316daec09e66e085e831d9
limgong <limgong@microsoft.com>
2019-07-12 18:17:26 +0800

    sql

---
+++ testbed/mytest/ms/TrillApp/Usesql/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace Usesql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection conn = new SqlConnection("Data Source=MININT-EP12N1V;Initial Catalog=EmployeeDB;Integrated Security=True");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("select FirstName, LastName from Employees");
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.Write("{1}, {0}", reader.GetString(0), reader.GetString(1));
+            }
+            reader.Close();
+            cmd.Dispose();
+            conn.Close();
+
+            if (Debugger.IsAttached)
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
