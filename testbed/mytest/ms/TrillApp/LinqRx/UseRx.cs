f6c497037a368cac710e889906220e043c98ce88
limgong <limgong@microsoft.com>
2019-07-13 16:04:21 +0800

    push

--- testbed/mytest/ms/TrillApp/LinqRx/UseRx.cs
+++ testbed/mytest/ms/TrillApp/LinqRx/UseRx.cs
@@ -8,11 +8,24 @@ using System.Reactive.Subjects;
 using System.Reactive.Concurrency;
 using System.Reactive.Threading;
 using System.Threading;
+using System.Reactive;
+
 namespace LinqRx
 {
     class UseRx
     {
         public static void Test()
+        {
+            var observable = new MyRangeObservable(5, 8);
+            var observer = Observer.Create<int>(
+                x => Console.WriteLine($"value={x}"),
+                e => Console.WriteLine(e.Message),
+                () => Console.WriteLine("Completed")
+            );
+            var sub = observable.Subscribe(observer);
+            sub.Dispose();
+        }
+        public static void Test1()
         {
             var id = Thread.CurrentThread.ManagedThreadId.ToString();
             Console.WriteLine($"Thread id = {id}");
@@ -20,7 +33,7 @@ namespace LinqRx
             IObservable<int> input = Observable.Range(1, 15);
             input.Where(i => i % 2 == 0).Subscribe(Console.Write);
 
-            var ob = new Observer();
+            var ob = new MyObserver<int>();
             var subscripttion = input.Subscribe(ob);
             subscripttion.Dispose();
 
@@ -30,7 +43,7 @@ namespace LinqRx
         }
     }
 
-    class Observer : IObserver<int> // 消费
+    class Observer1 : IObserver<int> // 消费
     {
         public void OnCompleted()
         {
@@ -53,7 +66,30 @@ namespace LinqRx
         }
     }
 
-    class MyRangeObservable : IObservable<int>
+    class MyObserver<T> : IObserver<T> // 消费
+    {
+        public void OnCompleted()
+        {
+            var id = Thread.CurrentThread.ManagedThreadId.ToString();
+            Console.WriteLine($"Thread id = {id}");
+            Console.WriteLine("Observation complete");
+        }
+
+        public void OnError(Exception error)
+        {
+            var id = Thread.CurrentThread.ManagedThreadId.ToString();
+            Console.WriteLine($"Thread id = {id}");
+            Console.WriteLine($"Error: {error.Message}");
+        }
+
+        public void OnNext(T value)
+        {
+            var id = Thread.CurrentThread.ManagedThreadId.ToString();
+            Console.WriteLine($"Thread id = {id}, value = {value}");
+        }
+    }
+
+    class MyRangeObservable : IObservable<int> // 生产者
     {
         private int _start;
         private int _count;
