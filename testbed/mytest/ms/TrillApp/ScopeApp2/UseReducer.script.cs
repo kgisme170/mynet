616b0de2bf062a51535de3d2be1aa9c93f8cc59c
kgisme170 <kgisme170@163.com>
2019-07-14 21:40:14 +0800

    Use reducer

---
+++ testbed/mytest/ms/TrillApp/ScopeApp2/UseReducer.script.cs
@@ -0,0 +1,25 @@
+﻿using ScopeRuntime;
+using System.Collections.Generic;
+
+public class MyReducer : Reducer
+{
+    public override Schema Produces(string[] columns, string[] args, Schema input)
+    {
+        return new Schema("EmpNames:string,DeptId:int");
+    }
+
+    public override IEnumerable<Row> Reduce(RowSet input, Row output, string[] args)
+    {
+        string names = "";
+        int DeptId = 0;
+        foreach (Row row in input.Rows)
+        {
+            names += (string)row[0].Value;
+            names += ",";
+            DeptId = (int)row[1].Value;
+        }
+        output[0].Set(names);
+        output[1].Set(DeptId);
+        yield return output;
+    }
+}
