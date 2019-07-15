e4e47ee6a8ddf92561da304ea885fee17ec51316
limgong <limgong@microsoft.com>
2019-07-15 13:36:43 +0800

    streamingProcess

---
+++ testbed/mytest/ms/TrillApp/StreamProcessing/MyAverageAggregate.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.StreamProcessing;
+using Microsoft.StreamProcessing.Aggregates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamProcessing
+{
+    public struct AverageState
+    {
+        public long Sum;
+        public ulong Count;
+    }
+    public class MyAverageAggregate : IAggregate<int, AverageState, double>
+    {
+        public Expression<Func<AverageState>> InitialState()
+        {
+            return () => new AverageState();
+        }
+        public Expression<Func<AverageState, long, int, AverageState>> Accumulate()
+        {
+            return (oldState, timestamp, input) => new AverageState
+            { Count = oldState.Count + 1, Sum = oldState.Sum + input };
+        }
+        public Expression<Func<AverageState, long, int, AverageState>> Deaccumulate()
+        {
+            return (oldState, timestamp, input) => new AverageState
+            { Count = oldState.Count - 1, Sum = oldState.Sum - input };
+        }
+        public Expression<Func<AverageState, AverageState, AverageState>> Difference()
+        {
+            return (left, right) => new AverageState
+            { Count = left.Count - right.Count, Sum = left.Sum - right.Sum };
+        }
+        public Expression<Func<AverageState, double>> ComputeResult()
+        {
+            return state => (double)state.Sum / state.Count;
+        }
+    }
+
+    public static class MyExtensions
+    {
+        public static IAggregate<TSource, AverageState, double> MyAverage<TKey, TSource>(
+        this Window<TKey, TSource> window, Expression<Func<TSource, int>> selector)
+        {
+            var aggregate = new MyAverageAggregate();
+            return aggregate.Wrap(selector);
+        }
+    }
+    /*
+    public static class MyExtensions
+    {
+        public static IAggregate<TSource, NullOutputWrapper<AverageState>, double?>
+        MyAverage<TKey, TSource>(this Window<TKey, TSource> window,
+        Expression<Func<TSource, int?>> selector)
+        {
+            var aggregate = new MyAverageAggregate();
+            var aggregate1 = aggregate.MakeOutputNullableAndOutputNullsWhenEmpty();
+            var aggregate2 = aggregate1.MakeInputNullableAndSkipNulls();
+            return aggregate2.Wrap(selector);
+        }
+    }
+    */
+}
--- testbed/mytest/ms/TrillApp/StreamProcessing/Program.cs
+++ testbed/mytest/ms/TrillApp/StreamProcessing/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.StreamProcessing;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reactive.Linq;
 
 namespace StreamProcessing
 {
@@ -10,6 +9,41 @@ namespace StreamProcessing
     {
         static void Main(string[] args)
         {
+            /*
+            var values = new[]
+            {
+                new {Field1=1,Field2=3},
+                new {Field1=1,Field2=3},
+                new {Field1=1,Field2=3},
+                new {Field1=1,Field2=3},
+                new {Field1=1,Field2=3},
+            };
+            */
+
+            StreamEvent<int>[] values =
+            {
+                StreamEvent.CreateInterval(1, 10, 1),
+                StreamEvent.CreateInterval(2, 10, 2),
+                StreamEvent.CreateInterval(3, 10, 3),
+                StreamEvent.CreateInterval(4, 10, 4),
+                StreamEvent.CreateInterval(5, 10, 5),
+                StreamEvent.CreateInterval(6, 10, 6),
+                StreamEvent.CreateInterval(7, 10, 7),
+                StreamEvent.CreateInterval(8, 10, 8),
+                StreamEvent.CreateInterval(9, 10, 9),
+                StreamEvent.CreatePunctuation<int>(StreamEvent.InfinitySyncTime)
+            };
+
+            var input = values.ToObservable().ToStreamable();
+            Console.WriteLine("Input =");
+            input.ToStreamEventObservable().ForEachAsync(e => Console.WriteLine(e)).Wait();
+
+            Console.WriteLine();
+            var output = input.Where(r => r > 5);
+
+            Console.WriteLine();
+            Console.WriteLine("Output =");
+            output.ToStreamEventObservable().ForEachAsync(e => Console.WriteLine(e)).Wait();
         }
     }
 }
