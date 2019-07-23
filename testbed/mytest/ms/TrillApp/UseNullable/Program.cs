44eddabf784f82c9838bee9db45423960162bd45
limgong <limgong@microsoft.com>
2019-07-23 19:31:30 +0800

    UseNullable

---
+++ testbed/mytest/ms/TrillApp/UseNullable/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseNullable
+{
+    class Member
+    {
+        public string Name { get; set; }
+    }
+    class Inner
+    {
+        public Member mI { get; set; }
+        public Member mJ { get; set; }
+    }
+    class Outter
+    {
+        public Inner inner { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Outter outter = new Outter();
+            Console.WriteLine(outter?.inner?.mI?.Name);
+        }
+    }
+}
