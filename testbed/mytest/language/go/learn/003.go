5301f790aaa6d8df0025933b53f4777e3f837c43
limgong <limgong@microsoft.com>
2019-08-15 22:31:05 +0800

    Use reader

--- testbed/mytest/language/go/learn/003.go
+++ testbed/mytest/language/go/learn/003.go
@@ -1,8 +1,27 @@
 package main
 import (
+	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
+	"strings"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+var f = "config.yaml"
+
+func readConfig(content io.Reader) string {
+	config := viper.New()
+	config.SetConfigType("yaml")
+	err := config.ReadConfig(content)
+	if err != nil {
+		fmt.Printf("Error: ReadConfig: %s", err.Error())
+		return ""
+	}
+	v := config.GetString("param1")
+	//fmt.Println("param1:", v)
+	return v
+}
 func main() {
 	rootCmd := &cobra.Command{
 		Use: "003",
@@ -19,7 +38,29 @@ Message 003`,
 		},
 	}
 
-	//rootCmd.AddCommand(dumpOne.cobraCmd)
+	cobraCmd := &cobra.Command{
+		Use: "mycmd",
+		Short: "my usage",
+		Long: `Long term`,
+		Run: func(_ *cobra.Command, args [] string) {
+			fmt.Println("mycmd")
+			content, err := ioutil.ReadFile(f)
+			if err != nil {
+				fmt.Printf("Error: Config not found: %s\n", f)
+				os.Exit(1)
+			}
+			//fmt.Println(content)
+			reader := strings.NewReader(string(content))
+
+			c := readConfig(reader)
+			if err != nil {
+				fmt.Printf("Error: %s", err.Error())
+				os.Exit(2)
+			}
+			fmt.Println(c)
+		},
+	}
+	rootCmd.AddCommand(cobraCmd)
 	rootCmd.Execute()
 	os.Exit(0)
 }
