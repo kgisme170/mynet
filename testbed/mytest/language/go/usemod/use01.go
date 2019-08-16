69e95c7686791fd777503a5d23411940c3b9a1a2
limgong <limgong@microsoft.com>
2019-08-16 10:50:03 +0800

    use01

--- testbed/mytest/language/go/usemod/use01.go
+++ testbed/mytest/language/go/usemod/use01.go
@@ -1,7 +1,20 @@
-package usemod
+package main
 import (
 	"fmt"
+	"bytes"
 )
 func main(){
 	fmt.Println("hw")
+	var sa = "a好"
+	buf := bytes.NewBufferString(sa)
+	fmt.Println(buf)
+	fmt.Println("rune:", len([]rune(sa)))
+	first := "社区"
+	fmt.Println([]rune(first))
+	fmt.Println([]byte(first))
+	ss := "golangcaff"
+	fmt.Println(ss[:3])
+	s := "截取中文"
+	res := []rune(s)
+	fmt.Println(string(res[:2]))
 }
