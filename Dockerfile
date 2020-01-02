e8719ada610c1df70a753bc487b81e48d3d5517a
limgong <limgong@microsoft.com>
2020-01-02 19:31:29 +0800

    use x509

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
 # Base build image
-FROM ubuntu-18.04 AS build_base
+FROM ubuntu:19.04 AS build_base
 
 # Install some dependencies needed to build the project
-RUN apt-get install -y --fix-missing ca-certificates git gcc g++ libc-dev
+RUN apt-get upgrade \
+    && apt-get install -y --fix-missing autoconf automake bison ca-certificates cmake curl flex git gcc golang graphviz g++ libc6-dev lua5.3 make openssl python ruby scons wget
 
 # Force the go compiler to use modules
 ENV GO111MODULE=on
---
+++ practice/usex509/XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        static void print(string s)
+        {
+            Debug.WriteLine(s);
+        }
+        static string GetParent(string path, int level)
+        {
+            if (level <= 1)
+            {
+                throw new Exception("Level should be >=1");
+            }
+            var ret = path;
+            for(int i = 0; i < level; ++i)
+            {
+                ret = Directory.GetParent(ret).FullName;
+            }
+            return ret;
+        }
+        [Fact]
+        public void Test1()
+        {
+            try
+            {
+                // Get the current directory.
+                string path = Directory.GetCurrentDirectory();
+                print(path);
+                var p = Path.Join(GetParent(path, 5), "cert");
+                print(p);
+                if (!Directory.Exists(p))
+                {
+                    Directory.CreateDirectory(p);
+                }
+                Console.WriteLine("The current directory is {0}", path);
+
+                // Change the current directory.
+                Environment.CurrentDirectory = (p);
+                if (path.Equals(Directory.GetCurrentDirectory()))
+                {
+                    Console.WriteLine("You are in the temp directory.");
+                }/*
+                else
+                {
+                    Console.WriteLine("You are not in the temp directory.");
+                }*/
+                //CertInfo.main("client.key");
+                Console.WriteLine("------------------");
+                CertInfo.main("client.pem");
+                Console.WriteLine("------------------");
+                //CertInfo.main("server.key");
+                Console.WriteLine("------------------");
+                CertInfo.main("server.pem");
+                Console.WriteLine("------------------");
+
+            }
+            catch (Exception e)
+            {
+                print("The process failed: " + e.ToString());
+            }
+        }
+    }
+}
