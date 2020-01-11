d154ddd2a9757e87db5e3a886c576de29bd329f2
limgong <limgong@microsoft.com>
2020-01-11 22:42:13 +0800

    fix

--- testbed/cpp3p/build.sh
+++ testbed/cpp3p/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-source /etc/environment
 /mynet/testbed/cpp3p/install_grpc.sh && /mynet/testbed/cpp3p/install_openmpi.sh && /mynet/testbed/cpp3p/install_thrift.sh
 cd /mynet/testbed/cpp3p/misc      && cmake . && make -j8
 cd /mynet/testbed/cpp3p/protobuf  && cmake . && make -j8
