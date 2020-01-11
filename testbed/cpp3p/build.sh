9a5c68d11b829ec857da0a79abc5a11d108f1354
kgisme170 <kgisme170@163.com>
2020-01-11 19:45:19 +0800

    fix install.sh

--- testbed/cpp3p/build.sh
+++ testbed/cpp3p/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./install_grpc.sh && ./install_openmpi.sh && ./install_thrift.sh
+/mynet/testbed/cpp3p/install_grpc.sh && /mynet/testbed/cpp3p/install_openmpi.sh && /mynet/testbed/cpp3p/install_thrift.sh
 cd /mynet/testbed/cpp3p/misc      && cmake . && make -j8
 cd /mynet/testbed/cpp3p/protobuf  && cmake . && make -j8
 cd /mynet/testbed/cpp3p/usegrpc   && cmake . && make -j8
