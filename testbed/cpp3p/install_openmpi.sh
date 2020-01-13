41540a2208c5282edca500e3177437aedbbf7472
kgisme170 <kgisme170@163.com>
2020-01-13 15:08:58 +0800

    fix

--- testbed/cpp3p/install_openmpi.sh
+++ testbed/cpp3p/install_openmpi.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-#openmpi
-cd /
-wget https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-3.1.1.tar.gz
-tar xvf openmpi-3.1.1.tar.gz
-cd openmpi-3.1.1
-./configure
-make -j8
-make install
-ldconfig
+cd / && wget https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-3.1.1.tar.gz && tar xvf openmpi-3.1.1.tar.gz && cd openmpi-3.1.1 && ./configure && make -j8 && make install && ldconfig
