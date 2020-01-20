40bdc97bb572ccb01de8deda67536f3c8cb0bf6f
kgisme170 <kgisme170@163.com>
2020-01-20 05:36:55 +0000

    fix

--- testbed/language/go/gopl/recover_test.go
+++ testbed/language/go/gopl/recover_test.go
@@ -2,18 +2,35 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 	"testing"
 )
 
-func TestRecover(t *testing.T) {
-	defer func() {
-		if err := recover(); err != nil {
-			fmt.Println("Got error:")
-			fmt.Println(err)
-		}
-	}()
+func check() {
+	if err := recover(); err != nil {
+		fmt.Println("Got error")
+		fmt.Println(err)
+	}
+}
 
+func TestRecover(t *testing.T) {
+	defer check()
 	var c interface{} = []int{10}
 	var d interface{} = []int{20}
 	fmt.Println(c == d)
+}
+
+// my comments
+func TestTypeAssertion(t *testing.T) {
+	defer check()
+	var v interface{} = os.Stdout
+	v2 := v.(io.ReadWriter)
+	fmt.Println(v2)
+	// go internal package
+	// go map value *=
+	// go module
+	// go flag
+	// tcp Dial
+
 }
