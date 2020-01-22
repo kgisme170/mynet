0894c6a6e50cf4e0ca648e3c7d7fd49b5b0be3a5
kgisme170 <kgisme170@163.com>
2020-01-22 11:56:14 +0000

    fix

--- testbed/java3p/useThrift/src/main/java/AdditionServiceHandler.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author liming.gong
- */
-public class AdditionServiceHandler implements AdditionService.Iface {
-    @Override
-    public int add(int n1, int n2) {
-        return n1 + n2;
-    }
-}
