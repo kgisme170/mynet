fab1f80d52278418ed6ecbd3b3e1157ba4f652b6
kgisme170 <kgisme170@163.com>
2020-01-24 02:41:20 +0000

    fix

--- ms/dotnetcorelinux.sh
+++ ms/dotnetcorelinux.sh
@@ -4,8 +4,4 @@ wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-pr
 && dpkg -i packages-microsoft-prod.deb \
 && add-apt-repository universe \
 && apt-get update \
-&& apt-get install apt-transport-https\
-&& apt-get update \
-&& apt-get install dotnet-sdk-3.1 \
-&& apt-get install aspnetcore-runtime-3.1 \
-&& apt-get install dotnet-runtime-3.1
+&& apt-get install -y apt-transport-https dotnet-sdk-3.1 aspnetcore-runtime-3.1 dotnet-runtime-3.1
