fca470a68d4d65e1e98243e8070ece435fb90b79
kgisme170 <kgisme170@163.com>
2020-01-26 21:07:20 +0000

    fix

--- docker/prebuild.sh
+++ docker/prebuild.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+cp /mynet/docker/environment /etc/
 source /etc/environment
-cd /mynet/testbed/cpp3p && ./install_grpc.sh && ./install_openmpi.sh && ./install_thrift.sh
+cd /mynet/testbed/cpp3p && ./install_grpc.sh && ./install_openmpi.sh && ./install_thrift.sh && cd /mynet/testbed/cpp3p && ./build.sh
