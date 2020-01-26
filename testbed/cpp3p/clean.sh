532e8085a2c31edc9b6441beabcedec6e15792c7
kgisme170 <kgisme170@163.com>
2020-01-26 15:41:20 +0000

    fix

--- testbed/cpp3p/clean.sh
+++ testbed/cpp3p/clean.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /mynet/testbed/cpp3p/misc && rm -fr build && cd /mynet/testbed/cpp3p/protobuf && cmake . && rm -fr build && cd /mynet/testbed/cpp3p/usegrpc && rm -fr build && cd /mynet/testbed/cpp3p/usethrift && rm -fr build && cd /mynet/testbed/cpp3p/useXml2 && rm -fr build
+cd /mynet/testbed/cpp3p/misc && rm -fr build && cd /mynet/testbed/cpp3p/protobuf && rm -fr build && cd /mynet/testbed/cpp3p/usegrpc && rm -fr build && cd /mynet/testbed/cpp3p/usethrift && rm -fr build && cd /mynet/testbed/cpp3p/useXml2 && rm -fr build
