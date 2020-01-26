70d8ca226c80b13284c3717fc14db0afc76ded20
kgisme170 <kgisme170@163.com>
2020-01-26 08:22:17 +0000

    add doc

---
+++ testbed/cpp/cpp11concurrent/orderSequence.cpp
@@ -0,0 +1,25 @@
+#include <atomic>
+#include <chrono>
+#include <iostream>
+#include <thread>
+using namespace std;
+atomic<bool> y;
+void write()
+{
+    this_thread::sleep_for(chrono::seconds(2));
+    cout<<"end sleep\n";
+    y.store(true,memory_order_relaxed);
+}
+void read()
+{
+    while(!y.load(memory_order_relaxed));
+    cout<<"end load\n";
+}
+int main() {
+    y = false;
+    thread a(write);
+    thread b(read);
+    a.join();
+    b.join();
+    cout<<y.load()<<endl;
+}
