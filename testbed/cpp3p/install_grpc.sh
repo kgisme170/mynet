a48029502f232a59e9afe1866665eda1f0114916
kgisme170 <kgisme170@163.com>
2020-01-30 18:10:29 +0800

    fix

--- testbed/cpp3p/install_grpc.sh
+++ testbed/cpp3p/install_grpc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /grpc/third_party/protobuf && bazel build :all && make install -j8 && cd /grpc/ && bazel build :all && make install -j8
+cd /grpc/third_party/protobuf && ./autogen.sh && ./configure && make -j8 && make install -j8 && cd /grpc/ && bazel build :all && make install -j8
