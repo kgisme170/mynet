b5e9a2c50504bfd8e5baf20e07c271313f70ff60
kgisme170 <kgisme170@163.com>
2020-02-12 11:56:35 +0000

    fix

--- docker/prebuild.sh
+++ docker/prebuild.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+apt-get install -y libxml2-dev
+apt remove libprotobuf-dev libprotobuf*
+
 cp /mynet/docker/environment /etc/
 source /etc/environment
-cd /libevent && mkdir -p build && cd build && cmake .. && make -j8 && sudo make install && /mynet/docker/makegrpc.sh && cd /mynet/testbed/cpp3p && ./install_openmpi.sh && ./install_thrift.sh && cd /mynet/testbed/cpp3p && ./build.sh
+cd /libevent && mkdir -p build && cd build && cmake .. && make -j8 && make install && /mynet/docker/makegrpc.sh && cd /mynet/testbed/cpp3p && ./install_openmpi.sh && ./install_thrift.sh && cd /mynet/testbed/cpp3p && ./build.sh
