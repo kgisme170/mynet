3d07c0db0aba1d338081b90049472bfd64c8c283
kgisme170 <kgisme170@163.com>
2020-02-13 11:33:09 +0000

    fix

--- docker/prebuild.sh
+++ docker/prebuild.sh
@@ -4,6 +4,19 @@ apt-get remove -y libprotobuf-dev libprotobuf*
 
 cp /mynet/docker/environment /etc/
 source /etc/environment
+
+git clone https://github.com/kgisme170/mynet.git \
+cd /mynet \
+git config --global user.email kgisme170@163.com \
+git config --global user.name kgisme170 \
+git config credential.helper store
+
+cd / && rm -fr /libevent && git clone https://github.com/libevent/libevent.git \
+cd / && rm -fr /grpc && git clone https://github.com/grpc/grpc.git \
+cd /grpc/third_party/protobuf/ && git submodule update --init --recursive \
+cd / && wget https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-3.1.1.tar.gz && tar xvf openmpi-3.1.1.tar.gz \
+cd / && wget http://archive.apache.org/dist/thrift/0.10.0/thrift-0.10.0.tar.gz && tar xvf thrift-0.10.0.tar.gz
+
 cd /libevent && mkdir -p build && cd build && cmake .. && make -j8 && make install && /mynet/docker/makegrpc.sh && cd /mynet/testbed/cpp3p && ./install_openmpi.sh && ./install_thrift.sh && cd /mynet/testbed/cpp3p && ./build.sh
 
 echo "Begin to install kernel dev files"
