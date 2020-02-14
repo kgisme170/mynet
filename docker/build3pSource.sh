c3ddacac15193f5f1ca93458d756f0526408d3cb
kgisme170 <kgisme170@163.com>
2020-02-14 09:13:16 +0000

    fix

---
+++ docker/build3pSource.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /libevent && mkdir -p build && cd build && cmake .. && make -j8 && make install && /mynet/docker/makegrpc.sh && cd /mynet/testbed/cpp3p && ./install_openmpi.sh && ./install_thrift.sh
+
+cd /mynet/testbed/cpp3p && ./build.sh
+
+[ $? -ne 0 ] && echo "prebuild.sh failed" && exit 1
+echo "build3pSource.sh successful"
