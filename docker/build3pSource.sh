fb26d0f7e4f7542ab4f4da78bae433e3bdf724ef
kgisme170 <kgisme170@163.com>
2020-02-14 09:52:58 +0000

    fix

--- docker/build3pSource.sh
+++ docker/build3pSource.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /libevent && mkdir -p build && cd build && cmake .. && make -j8 && make install && /mynet/docker/makegrpc.sh && cd /mynet/testbed/cpp3p && ./install_openmpi.sh && ./install_thrift.sh
+cd /libevent && mkdir -p build && cd build && cmake .. && make -j8 && make install && cd /grpc && bazel build :all && cd /mynet/testbed/cpp3p && ./install_openmpi.sh && ./install_thrift.sh
 
 cd /mynet/testbed/cpp3p && ./build.sh
 
