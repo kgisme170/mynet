99c60a2370f76db3ef7b487d281e386cf968e671
limgong <limgong@microsoft.com>
2020-02-15 12:07:13 +0800

    ss

---
+++ spark/structuredStreaming/src/main/scala/FileSs/scala.scala
@@ -0,0 +1,28 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.StructType
+
+object FileSs {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder
+      .master("local")
+      .appName("StructuredNetworkWordCount")
+      .getOrCreate()
+
+    spark.sparkContext.setLogLevel("WARN")
+
+    import spark.implicits._
+    val userSchema = new StructType().add("name", "string").add("age", "integer")
+    val lines = spark.readStream
+      .option("sep", ";")
+      .schema(userSchema)
+      .csv("file:///my.csv")
+
+    val query = lines.writeStream
+      .outputMode("append")
+      .format("console")
+      .start()
+
+    query.awaitTermination()
+  }
+}
