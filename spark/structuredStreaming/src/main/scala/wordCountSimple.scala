fd04d3833dd46a5eee3ce2695ae0d1ba196b1df3
kgisme170 <kgisme170@163.com>
2020-02-21 04:05:26 +0000

    fix

--- spark/structuredStreaming/src/main/scala/wordCountSimple.scala
+++ spark/structuredStreaming/src/main/scala/wordCountSimple.scala
@@ -1,20 +1,28 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types
+import org.apache.spark.sql.functions._
 object wordCountSimple {
   def main(args: Array[String]): Unit = {
 	val spark = SparkSession
 	  .builder
 	  .master("local")
-	  .appName("wordCount9999")
+	  .appName("wordCountSimple")
 	  .getOrCreate()
 
 	spark.sparkContext.setLogLevel("WARN")
 	import spark.sqlContext.implicits._
-	val df = Seq(("2019-07-01 12:01:19.000"),
-	  ("2019-06-24 12:01:19.000"),
-	  ("2019-11-16 16:44:55.406"),
-	  ("2019-11-16 16:50:59.406")).toDF("input_timestamp")
-	df.withColumn("datetype_timestamp", to_timestamp(col("input_timestamp")))
-	  .printSchema()
+	val df = Seq(("abc", "2019-07-01 12:01:19.000"),
+	  ("xyz", "2019-06-24 12:01:19.000"),
+	  ("abc", "2019-11-16 16:44:55.406"),
+	  ("abc", "2019-11-16 16:50:59.406")).toDF("value", "date")
+	val res = df.select(
+	  $"value",
+	  $"date",
+	  unix_timestamp($"date", "yyyy/MM/dd HH:mm:ss").as("timestamp"))
+	res.printSchema
+	res.show(false)
+	println("step1")
 	spark.close()
+	println("step2")
   }
 }
