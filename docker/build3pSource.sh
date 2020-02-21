b9beb65e077b8dd57ec028ff6be73b40be14e80a
kgisme170 <kgisme170@163.com>
2020-02-21 10:56:35 +0000

    fix

--- docker/build3pSource.sh
+++ docker/build3pSource.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd /libevent && mkdir -p build && cd build && cmake .. && make -j8 && make install && /mynet/docker/makegrpc.sh
-cd /mynet/testbed/cpp3p && ./install_openmpi.sh && ./install_thrift.sh && ./build.sh
+cd /libevent && mkdir -p build && cd build && cmake .. && make -j8 && make install && ~/mynet/docker/makegrpc.sh
+cd ~/mynet/testbed/cpp3p && ./install_openmpi.sh && ./install_thrift.sh && ./build.sh
 
 [ $? -ne 0 ] && echo "build3pSource.sh failed" && exit 1
 echo "build3pSource.sh successful"
