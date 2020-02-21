154097de21df63baaba27a93e84647e0949c1bdd
kgisme170 <kgisme170@163.com>
2020-02-21 04:14:34 +0000

    fix

--- testbed/linux/characterDevice/.hello.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/mynet/testbed/linux/characterDevice/hello.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /mynet/testbed/linux/characterDevice/hello.ko /mynet/testbed/linux/characterDevice/hello.o /mynet/testbed/linux/characterDevice/hello.mod.o ;  true
--- testbed/linux/characterDevice/.tmp_versions/hello.mod
+++
@@ -1,3 +0,0 @@
-/mynet/testbed/linux/characterDevice/hello.ko
-/mynet/testbed/linux/characterDevice/hello.o
-
