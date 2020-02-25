b88c65d8108cb489ba0104abb267d751c53af655
limgong <limgong@microsoft.com>
2020-02-25 17:20:54 +0800

    fix

---
+++ testbed/csharp/NUnitTestProject_core/AbstractClass.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace NUnitTestProject_core
+{
+    abstract class Ac
+    {
+        private int _mi;
+
+        public int MI { get => _mi; set => _mi = value; }
+
+        public abstract void f();
+    }
+    class Derive : Ac
+    {
+        public override void f() { }
+    }
+
+    class AbstractClass
+    {
+        [Test]
+        public static void TestAbstractClass()
+        {
+            Ac o = new Derive
+            {
+                MI = 3
+            };
+            Assert.Equals(3, o.MI);
+        }
+    }
+}
---
+++ testbed/csharp/NUnitTestProject_core/UseIReadOnlyList.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace NUnitTestProject_core
+{
+    class UseIReadOnlyList
+    {
+        [Test]
+        public static void TestIReadOnlyList()
+        {
+            IReadOnlyList<string> list = null;
+        }
+    }
+}
