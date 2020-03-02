3d80c067a7d54ecd681eff212c7a153dbc3dfd41
limgong <limgong@microsoft.com>
2020-03-02 17:49:43 +0800

    Add netapp3.1

---
+++ testbed/csharp/NUnitTestProject_standard2/UseNetApp31/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UseNetApp31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var l = default(List<string>);
+            Console.WriteLine(l);
+        }
+    }
+}
