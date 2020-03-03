5c57f7b2df88003c3d5e194b89ff0776326e41f7
limgong <limgong@microsoft.com>
2020-03-03 15:51:04 +0800

    Test move function

---
+++ testbed/csharp/NUnitTestProject_standard2/UseNewtonSoftJson/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace UseNewtonSoftJson
+{
+    public class MyClass
+    {
+        public int MyNum { get; set; }
+
+        public string MyStr { get; set; } = string.Empty;
+    }
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MyClass myClass = new MyClass { MyNum = 10, MyStr = "Hello World" };
+
+            var s = JsonConvert.SerializeObject(myClass);
+            Console.WriteLine(s);
+
+            var info = JsonConvert.DeserializeObject<MyClass>(s);
+            Console.WriteLine(info);
+        }
+    }
+}
