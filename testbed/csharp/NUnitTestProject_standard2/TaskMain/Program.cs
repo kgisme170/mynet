53151f3d2a600c1a834d636a8ead3e6108e28dce
limgong <limgong@microsoft.com>
2020-03-05 17:20:37 +0800

    Add core31 task project

--- testbed/csharp/NUnitTestProject_standard2/TaskMain/Program.cs
+++ testbed/csharp/NUnitTestProject_standard2/TaskMain/Program.cs
@@ -8,6 +8,43 @@ namespace TaskMain
 {
     class Program
     {
+        static async Task<int> DelayAndReturnAsync(int val)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(val));
+            return val;
+        }
+
+        static async Task AwaitAndProcessAsync(Task<int> task)
+        {
+            var result = await task;
+            Console.WriteLine(result);
+        }
+
+        static async Task ProcessTaskAsync()
+        {
+            Task<int> taskA = DelayAndReturnAsync(4);
+            Task<int> taskB = DelayAndReturnAsync(6);
+            Task<int> taskC = DelayAndReturnAsync(2);
+            var tasks = new[] { taskA, taskB, taskC };
+
+            var processingTasks = tasks.Select(async t =>
+            {
+                var result = await t;
+                Console.WriteLine(result); // 按照完成的顺序
+                return result;
+            }).ToArray(); // or var processingTasks = (from t in tasks select AwaitAndProcessAsyc(t))
+            Console.WriteLine("----------");
+            var t1 = await Task.WhenAll(processingTasks);
+            Console.WriteLine("----------");
+            foreach (var i in t1)
+            {
+                // 按照声明的顺序
+                Console.WriteLine(i);
+            }
+
+            var tuple = new Tuple<List<int>, List<int>>(new List<int>() { 1 }, new List<int>() { 2 });
+        }
+
         public static async Task<int> CalcAsync(int wait, int x)
         {
             await Task.Run(() => Thread.Sleep(wait));
@@ -46,12 +83,6 @@ namespace TaskMain
             timer.Elapsed += delegate { timer.Dispose(); tcs.SetResult(42); };
             timer.Start();
             return tcs.Task;
-        }
-
-        public static void Test00()
-        {
-            var aw = GetAnswerToLife().GetAwaiter();
-            aw.OnCompleted(() => Console.WriteLine(awaiter.GetResult()));
         }
 
         public static void Test01()
---
+++ testbed/csharp/NUnitTestProject_standard2/UseNetCore31_task/Program.cs
@@ -0,0 +1,39 @@
+﻿using Nito.AsyncEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+
+namespace UseNetStandard2_task
+{
+    class Program
+    {
+
+        public static async Task MainAsync(string[] args)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(2));
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hi");
+            /*
+            try
+            {
+                AsyncContext.Run(() => MainAsync(args));
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e);
+            }*/
+
+            IObservable<DateTimeOffset> timestamps =
+                Observable.Interval(TimeSpan.FromSeconds(1))
+                .Timestamp()
+                .Where(x => x.Value % 2 == 0)
+                .Select(x => x.Timestamp);
+            timestamps.Subscribe(x => Console.WriteLine(x));
+        }
+    }
+}
