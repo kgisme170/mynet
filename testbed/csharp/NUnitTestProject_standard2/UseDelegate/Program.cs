d1e37032d3ba0969f13f5da5fb72d5f92fd2db54
limgong <limgong@microsoft.com>
2020-03-10 21:11:59 +0800

    Use delegate

---
+++ testbed/csharp/NUnitTestProject_standard2/UseDelegate/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UseDelegate
+{
+    public class Inner
+    {
+        public int M = 3;
+        public int F() { Console.WriteLine("f"); return 0; }
+        public void G() { Console.WriteLine("g"); }
+    }
+    class Program
+    {
+        public static void Caller(Action a)
+        {
+            a();
+        }
+        static void Main(string[] args)
+        {
+            var i = new Inner();
+            Func<int> f = i.F;
+            Action g = i.G;
+            f();
+            g();
+            Program.Caller(g);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
